043f7b880ddac3627ac3e8096299a6ae3e9378ff
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-09-28 05:43:23 +0800

    devfeat(annotation): Add @Nullable

---
+++ src/main/java/seedu/address/annotation/Nullable.java
@@ -0,0 +1,9 @@
+package seedu.address.annotation;
+
+
+
+/**
+ * Explicitly marks types as potentially being null, meaning appropriate null
+ * checking is in order.
+ */
+public @interface Nullable {}
