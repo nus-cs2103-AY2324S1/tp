a7501c7e179fdbafd51db331a6bba54d59915010
cyaoxuan <chuayaoxuan@gmail.com>
2023-09-28 15:08:18 +0800

    Add Javadocs

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param remark A remark of any format.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
