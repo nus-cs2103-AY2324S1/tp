b7cd03731c98d092cb6034dbe4f45866aef112cd
antonTan96 <antonhz2021@gmail.com>
2023-09-29 21:50:23 +0800

    Implement Remark Command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,46 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents the Remark of a Person in the Address book
+ *
+ * @author Anton Tan Hong Zhi
+ */
+public class Remark {
+    public final String value;
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherRemark = (Remark) other;
+        return value.equals(otherRemark.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
