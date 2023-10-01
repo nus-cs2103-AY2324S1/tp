09dd9d1a587c84fea29dda6ccfe5a34699bddda8
owenyeo <owen_nly@hotmail.com>
2023-10-01 11:01:03 +0800

    Add Remark Command
    Remark command is not present in addressbook, and would be useful for users to know
    remarks that provide important information about person.
    
    Add RemarkCommand
    
    Add RemarkCommandParser
    
    Add Person#getRemark()
    
    Modify PersonBuilder to support Remark.
    
    Modify AddCommand to create a Person with a blank Remark.
    
    Modify EditCommand to pass Remarks on unchanged.
    
    Modify SampleDataUtil to add remarks for the sample data.
    
    Modify JsonAaptedPerson to support Remark.
    
    Modify UI to support Remark.
    
    Add withRemark() to PersonBuilder.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -23,7 +23,21 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private final Remark remark;
     private final Set<Tag> tags = new HashSet<>();
+
+    /**
+     * Every field must be present and not null except remark.
+     */
+    public Person(Name name, Phone phone, Email email, Address address, Remark remark, Set<Tag> tags) {
+        requireAllNonNull(name, phone, email, address, tags);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.remark = remark;
+        this.tags.addAll(tags);
+    }
 
     /**
      * Every field must be present and not null.
@@ -34,6 +48,7 @@ public class Person {
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.remark = new Remark("");
         this.tags.addAll(tags);
     }
 
@@ -51,6 +66,10 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+
+    public Remark getRemark() {
+        return remark;
     }
 
     /**
