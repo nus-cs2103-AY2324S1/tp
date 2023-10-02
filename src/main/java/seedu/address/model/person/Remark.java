ca2c28ec83939869cdb565efcdd0555e172adedf
justin soong <91204066+juzzztinsoong@users.noreply.github.com>
2023-10-02 15:24:47 +0800

    Implement Remark Command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,49 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+public class Remark {
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+
+    public final String value;
+
+    /**
+     * Constructs an {@code Address}.
+     *
+     * @param address A valid address.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherRemark = (Remark) other;
+        return value.equals(otherRemark.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
