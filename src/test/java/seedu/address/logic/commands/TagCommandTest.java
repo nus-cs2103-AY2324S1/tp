bf3a2eb332cde030f0cc84060405530817cf5f35
Yeo Boh Shin <110957811+YeoBohShin@users.noreply.github.com>
2023-10-03 11:58:30 +0800

    Conform to checkstyle
    Fix order of import statements
    in TagCommandTest

--- src/test/java/seedu/address/logic/commands/TagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/TagCommandTest.java
@@ -1,6 +1,12 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
@@ -8,14 +14,8 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-import org.junit.jupiter.api.Test;
 import seedu.address.testutil.TypicalPersons;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for TagCommand.
@@ -28,7 +28,8 @@ public class TagCommandTest {
     public void execute_addTag_success() {
         TagCommand tagCommand = new TagCommand(INDEX_FIRST_PERSON, TypicalPersons.ALICE.getTags());
 
-        String expectedMessage = String.format(TagCommand.MESSAGE_ADD_TAG_SUCCESS, Messages.format(TypicalPersons.ALICE));
+        String expectedMessage = String.format(TagCommand.MESSAGE_ADD_TAG_SUCCESS,
+                Messages.format(TypicalPersons.ALICE));
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         expectedModel.setPerson(model.getFilteredPersonList().get(0), TypicalPersons.ALICE);
@@ -40,7 +41,8 @@ public class TagCommandTest {
     public void execute_deleteTag_success() {
         TagCommand tagCommand = new TagCommand(INDEX_THIRD_PERSON, TypicalPersons.CARL.getTags());
 
-        String expectedMessage = String.format(TagCommand.MESSAGE_DELETE_TAG_SUCCESS, Messages.format(TypicalPersons.CARL));
+        String expectedMessage = String.format(TagCommand.MESSAGE_DELETE_TAG_SUCCESS,
+                Messages.format(TypicalPersons.CARL));
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         expectedModel.setPerson(model.getFilteredPersonList().get(2), TypicalPersons.CARL);
