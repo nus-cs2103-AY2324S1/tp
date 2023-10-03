e20a4803d33cfbe7939e8ed5bf9b65591852d4a0
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-10-03 14:54:47 +0800

    feat(main): NoteCommand throw error, add usage msg

--- src/main/java/seedu/address/logic/commands/NoteCommand.java
+++ src/main/java/seedu/address/logic/commands/NoteCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 
@@ -14,8 +15,22 @@ public class NoteCommand extends Command {
      */
     public static final String COMMAND_WORD = "note";
 
+    /**
+     * Non-trivial Command subclasses usually expose a MESSAGE_USAGE for use in
+     * places like the parser.
+     */
+    public static final String MESSAGE_USAGE = String.format(
+        "%s: Sets the note for the contact of the specified number."
+        + "\nArguments: [number] o/[your note]."
+        + "\nExample: %s 1 o/CS2103 Prof.",
+        NoteCommand.COMMAND_WORD,
+        NoteCommand.COMMAND_WORD
+    );
+
+    public static final String MESSAGE_TODO = "NoteCommand not implemented yet.";
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Executed NoteCommand.");
+    public CommandResult execute(Model _model) throws CommandException {
+        throw new CommandException(MESSAGE_TODO);
     }
 }
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,6 +11,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_NOTE = new Prefix("o/");
 
     //TODO to be scrapped
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
