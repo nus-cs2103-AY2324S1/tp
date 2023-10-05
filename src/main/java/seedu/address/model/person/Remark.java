ee0dceac9c66e9f6d0653062af5a177dd51629a7
erohsikivar <ravikishore1809@gmail.com>
2023-10-05 22:01:10 +0800

    Update Portfolio

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Instantiates a new Remark.
+     *
+     * @param remark the remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
