578c0c326cceebfd33f6d3f79ac34a34a026424b
KamiliArsyad <arsyad.ik@gmail.com>
2023-10-05 23:37:50 +0800

    Add exception package and DuplicateClientException

--- src/main/java/seedu/address/logic/commands/MeowCommand.java
+++ src/main/java/seedu/address/logic/commands/MeowCommand.java
@@ -2,6 +2,9 @@ package seedu.address.logic.commands;
 
 import seedu.address.model.Model;
 
+/**
+ * Meow!
+ */
 public class MeowCommand extends Command {
     public static final String COMMAND_WORD = "meow";
 
