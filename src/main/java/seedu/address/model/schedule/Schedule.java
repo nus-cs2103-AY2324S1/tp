11b58e3431668b048487dc68961682d52750fc05
DS <yeodisheng@gmail.com>
2023-10-07 18:21:06 +0800

    Add Schedule Object
    Create Schedule object to present a schedule in the addressbook

---
+++ src/main/java/seedu/address/model/schedule/Schedule.java
@@ -0,0 +1,75 @@
+package seedu.address.model.schedule;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Objects;
+
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Represents a Schedule in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Schedule {
+    private final TutorIndex tutorIndex;
+    private final StartTime startTime;
+    private final EndTime endTime;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Schedule(TutorIndex tutorIndex, StartTime startTime, EndTime endTime) {
+        requireAllNonNull(tutorIndex, startTime, endTime);
+        this.tutorIndex = tutorIndex;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public TutorIndex getTutorIndex() {
+        return tutorIndex;
+    }
+
+    public StartTime getStartTime() {
+        return startTime;
+    }
+
+    public EndTime getEndTime() {
+        return endTime;
+    }
+
+    /**
+     * Returns true if both schedules have the same tutor index and time fields.
+     * This defines a stronger notion of equality between two schedules.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Schedule)) {
+            return false;
+        }
+
+        Schedule otherSchedule = (Schedule) other;
+        return tutorIndex.equals(otherSchedule.tutorIndex)
+                && startTime.equals(otherSchedule.startTime)
+                && endTime.equals(otherSchedule.endTime);
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(tutorIndex, startTime, endTime);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("tutorIndex", tutorIndex)
+                .add("startTime", startTime)
+                .add("endTime", endTime)
+                .toString();
+    }
+}
---
+++ src/main/java/seedu/address/model/schedule/StartTime.java
@@ -0,0 +1,63 @@
+package seedu.address.model.schedule;
+
+import static java.util.Objects.requireNonNull;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+
+/**
+ * Represents a Schedule's start time in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidStartTime(String)}
+ */
+public class StartTime {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "StartTime should only contain a valid date and time in the format 2023-09-15T09:00:00,"
+                    + " and it should not be blank";
+    public static final String VALIDATION_REGEX = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}";
+    private final LocalDateTime time;
+
+    /**
+     * Constructs a {@code StartTime}.
+     *
+     * @param time A valid start time.
+     */
+    public StartTime(LocalDateTime time) {
+        requireNonNull(time);
+
+        this.time = time;
+    }
+
+    public static boolean isValidStartTime(String test) {
+        if (!test.matches(VALIDATION_REGEX)) {
+            return false;
+        }
+
+        try {
+            LocalDateTime.parse(test);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof StartTime)) {
+            return false;
+        }
+
+        StartTime otherStartTime = (StartTime) other;
+        return time.equals(otherStartTime.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return time.hashCode();
+    }
+}
