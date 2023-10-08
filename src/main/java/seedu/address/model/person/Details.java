96f3355dc909a8fc2227fe6cb55694cd80810c20
KamiliArsyad <arsyad.ik@gmail.com>
2023-10-08 19:08:08 +0800

    More person details

---
+++ src/main/java/seedu/address/model/person/Details.java
@@ -0,0 +1,31 @@
+package seedu.address.model.person;
+
+public class Details {
+
+    public static final String MESSAGE_CONSTRAINTS = "Details should only contain alphanumeric characters "
+            + "and spaces, and it should not be blank";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String value;
+
+    /**
+    * Constructs a {@code Details}.
+    *
+    * @param details A valid details.
+    */
+    public Details(String details) {
+        value = details;
+    }
+
+    /**
+    * Returns true if a given string is a valid details.
+    */
+    public static boolean isValidDetails(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
