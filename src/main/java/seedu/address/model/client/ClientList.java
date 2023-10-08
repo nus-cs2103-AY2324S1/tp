a22fda4a6559a742347101dd24f894d0008b6ba1
KamiliArsyad <arsyad.ik@gmail.com>
2023-10-08 16:53:38 +0800

    Update ClientList

--- src/main/java/seedu/address/model/client/ClientList.java
+++ src/main/java/seedu/address/model/client/ClientList.java
@@ -1,10 +1,14 @@
 package seedu.address.model.client;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.address.model.client.exceptions.ClientNotFoundException;
 import seedu.address.model.client.exceptions.DuplicateClientException;
 
 import java.util.Iterator;
+import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
@@ -40,5 +44,71 @@ public class ClientList implements Iterable<Client> {
             throw new DuplicateClientException();
         }
         internalList.add(toAdd);
+    }
+
+
+    /**
+     * Replaces the contents of this list with {@code clients}.
+     * {@code clients} must not contain duplicate clients.
+     */
+    public void setClients(List<Client> clients) {
+        requireAllNonNull(clients);
+        if (!clientsAreUnique(clients)) {
+            throw new DuplicateClientException();
+        }
+
+        internalList.setAll(clients);
+    }
+
+    /**
+     * Returns true if {@code clients} contains only unique clients.
+     */
+    private boolean clientsAreUnique(List<Client> clients) {
+        for (int i = 0; i < clients.size() - 1; i++) {
+            for (int j = i + 1; j < clients.size(); j++) {
+                if (clients.get(i).equals(clients.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Replace the client {@code target} in the list with {@code editedClient}.
+     * {@code target} must exist in the list.
+     * The client identity of {@code editedClient} must not be the same as another existing client in the list.
+     */
+    public void setClient(Client target, Client editedClient) {
+        requireAllNonNull(target, editedClient);
+
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            throw new DuplicateClientException();
+        }
+
+        if (!target.equals(editedClient) && contains(editedClient)) {
+            throw new DuplicateClientException();
+        }
+
+        internalList.set(index, editedClient);
+    }
+
+    /**
+     * Removes the equivalent client from the list.
+     * The client must exist in the list.
+     */
+    public void remove(Client toRemove) {
+        requireNonNull(toRemove);
+        if (!internalList.remove(toRemove)) {
+            throw new ClientNotFoundException();
+        }
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<Client> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
     }
 }
