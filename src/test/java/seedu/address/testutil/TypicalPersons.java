3cb4c3d2a2657abab573eacdaffed578c2da05a6
markgcera <>
2023-10-11 00:54:32 +0800

    Fix non command tests

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -8,6 +8,8 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ROOM_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ROOM_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 
@@ -17,6 +19,8 @@ import java.util.List;
 
 import seedu.address.model.BookingsBook;
 import seedu.address.model.booking.Booking;
+
+//import static seedu.address.logic.commands.CommandTestUtil.*;
 
 /**
  * A utility class containing a list of {@code Person} objects to be used in tests.
@@ -33,27 +37,27 @@ public class TypicalPersons {
             .withTags("owesMoney", "friends").build();
     public static final Booking CARL = new BookingBuilder().withRoom("3").withName("Carl Kurz").withPhone("95352563")
             .withEmail("heinz@example.com").withAddress("wall street").build();
-    public static final Booking DANIEL = new BookingBuilder().withRoom("3").withName("Daniel Meier")
+    public static final Booking DANIEL = new BookingBuilder().withRoom("4").withName("Daniel Meier")
             .withPhone("87652533").withEmail("cornelia@example.com").withAddress("10th street")
             .withTags("friends").build();
-    public static final Booking ELLE = new BookingBuilder().withRoom("4").withName("Elle Meyer").withPhone("9482224")
+    public static final Booking ELLE = new BookingBuilder().withRoom("5").withName("Elle Meyer").withPhone("9482224")
             .withEmail("werner@example.com").withAddress("michegan ave").build();
-    public static final Booking FIONA = new BookingBuilder().withRoom("5").withName("Fiona Kunz").withPhone("9482427")
+    public static final Booking FIONA = new BookingBuilder().withRoom("6").withName("Fiona Kunz").withPhone("9482427")
             .withEmail("lydia@example.com").withAddress("little tokyo").build();
-    public static final Booking GEORGE = new BookingBuilder().withRoom("6").withName("George Best").withPhone("9482442")
+    public static final Booking GEORGE = new BookingBuilder().withRoom("7").withName("George Best").withPhone("9482442")
             .withEmail("anna@example.com").withAddress("4th street").build();
 
     // Manually added
-    public static final Booking HOON = new BookingBuilder().withRoom("7").withName("Hoon Meier").withPhone("8482424")
+    public static final Booking HOON = new BookingBuilder().withRoom("8").withName("Hoon Meier").withPhone("8482424")
             .withEmail("stefan@example.com").withAddress("little india").build();
-    public static final Booking IDA = new BookingBuilder().withRoom("8").withName("Ida Mueller").withPhone("8482131")
+    public static final Booking IDA = new BookingBuilder().withRoom("9").withName("Ida Mueller").withPhone("8482131")
             .withEmail("hans@example.com").withAddress("chicago ave").build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
-    public static final Booking AMY = new BookingBuilder().withRoom("9").withName(VALID_NAME_AMY)
+    public static final Booking AMY = new BookingBuilder().withRoom(VALID_ROOM_AMY).withName(VALID_NAME_AMY)
             .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
             .withTags(VALID_TAG_FRIEND).build();
-    public static final Booking BOB = new BookingBuilder().withRoom("10").withName(VALID_NAME_BOB)
+    public static final Booking BOB = new BookingBuilder().withRoom(VALID_ROOM_BOB).withName(VALID_NAME_BOB)
             .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
             .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
 
