d5e9284380ec0870e8617f1fd30c37c881ca1151
STEVEN\Steven <stevenliudw@gmail.com>
2023-10-10 17:31:55 +0800

    Add Test class for the new classes

---
+++ src/test/java/seedu/address/model/person/ProjectManagerTest.java
@@ -0,0 +1,19 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+
+import org.junit.jupiter.api.Test;
+
+public class ProjectManagerTest {
+
+    @Test
+    public void equals() {
+        ProjectManager aliceManager = new ProjectManager(ALICE.getName(), ALICE.getPhone(), ALICE.getEmail(), ALICE.getAddress(), ALICE.getRemark(), ALICE.getTags());
+
+        // same object -> returns true
+        assertTrue(aliceManager.equals(aliceManager));
+    }
+
+    // Other tests related to managing teams can be added here.
+}
---
+++ src/test/java/seedu/address/model/person/TeamTest.java
@@ -0,0 +1,20 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+
+import org.junit.jupiter.api.Test;
+
+public class TeamTest {
+
+    @Test
+    public void equals() {
+        TeamLeader aliceLeader = new TeamLeader(ALICE.getName(), ALICE.getPhone(), ALICE.getEmail(), ALICE.getAddress(), ALICE.getRemark(), ALICE.getTags());
+        Team aliceTeam = new Team(aliceLeader);
+
+        // same object -> returns true
+        assertTrue(aliceTeam.equals(aliceTeam));
+    }
+
+    // Tests related to adding developers can be added here.
+}
