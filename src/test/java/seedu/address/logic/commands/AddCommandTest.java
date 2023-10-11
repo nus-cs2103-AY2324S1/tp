6fb3ea790e7519e90de128d1f8ca33e8f7c55cbd
chonguschonguschongus <chongzhenxi@gmail.com>
2023-10-11 15:18:41 +0800

    Change find to search by NRIC

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -10,7 +10,6 @@ import static seedu.address.testutil.TypicalPersons.ALICE;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
@@ -154,7 +153,7 @@ public class AddCommandTest {
         }
 
         @Override
-        public void updateFilteredPersonList(Predicate<Person> predicate) {
+        public void updateFilteredPersonList(IcContainsKeywordsPredicate predicate) {
             throw new AssertionError("This method should not be called.");
         }
     }
