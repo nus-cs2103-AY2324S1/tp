9ad34134d7915ba5dba4bd2023e9564f6ddbcf1a
Wang Zihan <e1075515@u.nus.edu>
2023-10-12 04:18:37 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/logic/commands/ListEventCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListEventCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.BeforeEach;
