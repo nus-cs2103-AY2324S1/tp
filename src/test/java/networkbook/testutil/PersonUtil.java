e152270d2018a135c97bada96002924fb22b74e8
Singa-Pirate <E0969244@u.nus.edu>
2023-10-11 22:11:28 +0800

    Change command prefix format to " /prefix " - e.g. " n/jl" becomes " /name jl" - Add trailing whitespace for patterns in parser & tokenizer - Update tests, docs and error messages

--- src/test/java/networkbook/testutil/PersonUtil.java
+++ src/test/java/networkbook/testutil/PersonUtil.java
@@ -27,14 +27,14 @@ public class PersonUtil {
      */
     public static String getPersonDetails(Person person) {
         StringBuilder sb = new StringBuilder();
-        sb.append(CliSyntax.PREFIX_NAME + person.getName().fullName + " ");
-        sb.append(CliSyntax.PREFIX_PHONE + person.getPhone().value + " ");
+        sb.append(CliSyntax.PREFIX_NAME + " " + person.getName().fullName + " ");
+        sb.append(CliSyntax.PREFIX_PHONE + " " + person.getPhone().value + " ");
         person.getEmails().stream().forEach(
-                e -> sb.append(CliSyntax.PREFIX_EMAIL + e.toString() + " ")
+                e -> sb.append(CliSyntax.PREFIX_EMAIL + " " + e.toString() + " ")
         );
-        sb.append(CliSyntax.PREFIX_ADDRESS + person.getAddress().value + " ");
+        sb.append(CliSyntax.PREFIX_ADDRESS + " " + person.getAddress().value + " ");
         person.getTags().stream().forEach(
-            s -> sb.append(CliSyntax.PREFIX_TAG + s.tagName + " ")
+            s -> sb.append(CliSyntax.PREFIX_TAG + " " + s.tagName + " ")
         );
         return sb.toString();
     }
@@ -44,24 +44,28 @@ public class PersonUtil {
      */
     public static String getEditPersonDescriptorDetails(EditCommand.EditPersonDescriptor descriptor) {
         StringBuilder sb = new StringBuilder();
-        descriptor.getName().ifPresent(name -> sb.append(CliSyntax.PREFIX_NAME).append(name.fullName).append(" "));
-        descriptor.getPhone().ifPresent(phone -> sb.append(CliSyntax.PREFIX_PHONE).append(phone.value).append(" "));
+        descriptor.getName().ifPresent(name -> sb.append(CliSyntax.PREFIX_NAME).append(" ")
+                                                    .append(name.fullName).append(" "));
+        descriptor.getPhone().ifPresent(phone -> sb.append(CliSyntax.PREFIX_PHONE).append(" ")
+                                                    .append(phone.value).append(" "));
         if (descriptor.getEmails().isPresent()) {
             UniqueList<Email> emails = descriptor.getEmails().get();
             if (emails.isEmpty()) {
-                sb.append(CliSyntax.PREFIX_EMAIL);
+                sb.append(CliSyntax.PREFIX_EMAIL).append(" ");
             } else {
-                emails.forEach(e -> sb.append(CliSyntax.PREFIX_EMAIL).append(e.toString()).append(" "));
+                emails.forEach(e -> sb.append(CliSyntax.PREFIX_EMAIL).append(" ")
+                                                    .append(e.toString()).append(" "));
             }
         }
-        descriptor.getAddress().ifPresent(address -> sb.append(CliSyntax.PREFIX_ADDRESS)
-                .append(address.value).append(" "));
+        descriptor.getAddress().ifPresent(address -> sb.append(CliSyntax.PREFIX_ADDRESS).append(" ")
+                                                    .append(address.value).append(" "));
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
             if (tags.isEmpty()) {
-                sb.append(CliSyntax.PREFIX_TAG);
+                sb.append(CliSyntax.PREFIX_TAG).append(" ");
             } else {
-                tags.forEach(s -> sb.append(CliSyntax.PREFIX_TAG).append(s.tagName).append(" "));
+                tags.forEach(s -> sb.append(CliSyntax.PREFIX_TAG).append(" ")
+                                                    .append(s.tagName).append(" "));
             }
         }
         return sb.toString();
