5ffed4c70625eee0a5b28d71c7729c3256d3c7d5
kohkaijie <kohkaijie21@gmail.com>
2023-10-12 14:25:30 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -5,9 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIcs.FIRST_NRIC;
-import static seedu.address.testutil.TypicalIcs.SECOND_NRIC;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import java.util.Arrays;
 import java.util.List;
