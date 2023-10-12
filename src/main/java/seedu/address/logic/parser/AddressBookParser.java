7f7980e3ea9c7fda1393db64c27f68d62966047e
LWZ19 <e0985886@u.nus.edu>
2023-10-13 03:45:15 +0800

    Isolate original find command from class manager
    Moved the NameContainsKeywordsPredicate into the test package,
    since it is not needed anymore in the main program.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -14,7 +14,6 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.LookupCommand;
@@ -66,9 +65,6 @@ public class AddressBookParser {
 
         case ClearCommand.COMMAND_WORD:
             return new ClearCommand();
-
-        case FindCommand.COMMAND_WORD:
-            return new FindCommandParser().parse(arguments);
 
         case LookupCommand.COMMAND_WORD:
             return new LookupCommandParser().parse(arguments);
