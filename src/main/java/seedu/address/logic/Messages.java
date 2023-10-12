affec13d70fbaf73969d158b1b13993738a3f961
mamayuan <948665003my@gmail.com>
2023-10-12 22:34:33 +0800

    Fix checks

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -37,11 +37,13 @@ public class Messages {
     // Messages associated with Attributes constraints
     public static final String MESSAGE_NAME_CONSTRAINTS =
             "Names should only contain alphanumeric characters and spaces, and it should not be blank";
-    public static final String MESSAGE_PHONE_CONSTRAINTS = "Phone numbers should only contain numbers, and it should be at least 3 digits long";
+    public static final String MESSAGE_PHONE_CONSTRAINTS =
+            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
     public static final String MESSAGE_EMAIL_CONSTRAINTS = "Emails should be of the format local-part@domain "
             + "and adhere to the following constraints:\n"
             + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
-            + "the parentheses, (" + Email.SPECIAL_CHARACTERS + "). The local-part may not start or end with any special "
+            + "the parentheses, (" + Email.SPECIAL_CHARACTERS
+            + "). The local-part may not start or end with any special "
             + "characters.\n"
             + "2. This is followed by a '@' and then a domain name. The domain name is made up of domain labels "
             + "separated by periods.\n"
