27ffbd07ac177426d914c8308ed80b17a95c66cf
mfjkri <muhammadfikri6844@gmail.com>
2023-10-12 14:51:58 +0800

    Remove Address and Tag class

--- src/test/java/seedu/address/model/util/SampleDataUtilTest.java
+++ src/test/java/seedu/address/model/util/SampleDataUtilTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
@@ -16,18 +17,27 @@ public class SampleDataUtilTest {
     public void getSamplePersons_validData_returnsArrayOfPersons() {
         Person[] samplePersons = SampleDataUtil.getSamplePersons();
 
-        // Check if the array is not null
         assertNotNull(samplePersons);
-
-        // Check if the length of the array matches the number of sample persons
         assertEquals(6, samplePersons.length);
 
-        // Check if each element in the array is an instance of Person
         for (Person person : samplePersons) {
             assertNotNull(person);
             assertEquals(Name.class, person.getName().getClass());
             assertEquals(Phone.class, person.getPhone().getClass());
             assertEquals(Email.class, person.getEmail().getClass());
+        }
+    }
+
+    @Test
+    public void getSampleAddressBook_validData_returnsAddressBookWithSamplePersons() {
+        ReadOnlyAddressBook addressBook = SampleDataUtil.getSampleAddressBook();
+
+        assertNotNull(addressBook);
+        assertEquals(6, addressBook.getPersonList().size());
+
+        Person[] samplePersons = SampleDataUtil.getSamplePersons();
+        for (int i = 0; i < samplePersons.length; i++) {
+            assertEquals(samplePersons[i], addressBook.getPersonList().get(i));
         }
     }
 }
