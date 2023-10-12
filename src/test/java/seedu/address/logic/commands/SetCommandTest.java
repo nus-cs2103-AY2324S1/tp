a015ec6978f65de5b306305e38e7ab1769a9a206
scarletblanks <irisu.nita@gmail.com>
2023-10-13 00:22:16 +0800

    added tests

--- src/test/java/seedu/address/logic/commands/SetCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SetCommandTest.java
@@ -1,17 +1,17 @@
 package seedu.address.logic.commands;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.model.person.StatusTypes;
+import seedu.address.model.person.Status;
 
 public class SetCommandTest {
 
     @Test
-    public void execute_emptyAddressBook_success() {
-        assert (true);
+    public void execute_validIndexAndStatus_success() {
+        // Check if the status of the person in the model has been updated
+        assertEquals(new Status(), "Preliminary");
     }
 
-    @Test
-    public void execute_nonEmptyAddressBook_success() {
-        assert (true);
-    }
 
 }
