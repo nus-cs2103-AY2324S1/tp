cc9e4aa37d5c05698984550aa90bf10bf7cd2477
Xihua Zhu <xihua12370@gmail.com>
2023-10-13 04:12:10 +0800

    Modify SampleDataUtil to departments

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -24,19 +24,19 @@ public class SampleDataUtil {
                 getDepartmentSet("investment")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getDepartmentSet("colleagues", "investment")),
+                getDepartmentSet("R&D", "investment")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getDepartmentSet("neighbours")),
+                getDepartmentSet("logistics")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getDepartmentSet("family")),
+                getDepartmentSet("Production")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
-                getDepartmentSet("classmates")),
+                getDepartmentSet("Sales")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getDepartmentSet("colleagues"))
+                getDepartmentSet("Not-confirmed"))
         };
     }
 
