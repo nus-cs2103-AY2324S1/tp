58ae1b3dc77990cb96afd8890095a3fa7a26bb9d
mamayuan <948665003my@gmail.com>
2023-10-12 22:26:55 +0800

    Fix checks

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -13,9 +13,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format!\n%s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%d persons listed!";
-    public static final String MESSAGE_DUPLICATE_FIELDS =
-                "Multiple values specified for the following single-valued field(s): ";
-
+    public static final String MESSAGE_DUPLICATE_FIELDS = "Multiple values specified for the following single-valued field(s):";
     public static final String MESSAGE_EMPTY_WORD_PARAMETER = "Word parameter cannot be empty";
     public static final String MESSAGE_SINGLE_WORD_EXPECTED = "Word parameter should be a single word";
 
@@ -28,13 +26,15 @@ public class Messages {
     public static final String MESSAGE_EDIT_COMMAND_NOT_EDITED = "At least one field to edit must be provided.";
     public static final String MESSAGE_HELP_COMMAND_SHOW_HELP = "Opened help window.";
     public static final String MESSAGE_LIST_COMMAND_SUCCESS = "Listed all persons";
+
     // Exception message
     public static final String FILE_OPS_ERROR_FORMAT = "Could not save data due to the following error: %s";
     public static final String FILE_OPS_PERMISSION_ERROR_FORMAT =
             "Could not save data to file %s due to insufficient permissions to write to the file or the folder.";
     public static final String MESSAGE_Duplicate_Person_Exception = "Operation would result in duplicate persons";
-    // Messages associated with Attributes constraints 
-    public static final String MESSAGE_NAME_CONSTRAINTS = 
+
+    // Messages associated with Attributes constraints
+    public static final String MESSAGE_NAME_CONSTRAINTS =
             "Names should only contain alphanumeric characters and spaces, and it should not be blank";
     public static final String MESSAGE_PHONE_CONSTRAINTS = "Phone numbers should only contain numbers, and it should be at least 3 digits long";
     public static final String MESSAGE_EMAIL_CONSTRAINTS = "Emails should be of the format local-part@domain "
