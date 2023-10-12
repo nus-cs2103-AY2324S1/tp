5dfda464cf8b44ff79a9fa53d8300635e5708e0c
justin soong <91204066+juzzztinsoong@users.noreply.github.com>
2023-10-13 01:41:57 +0800

    fix trailing whitespace attempt 4

--- src/test/java/seedu/address/logic/parser/DeleteMeetingCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteMeetingCommandParserTest.java
@@ -27,7 +27,7 @@ public class DeleteMeetingCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", 
+        assertParseFailure(parser, "a",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteMeetingCommand.MESSAGE_USAGE));
     }
 }
