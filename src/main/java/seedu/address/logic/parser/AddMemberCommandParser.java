94da9c4c3e132f6d893e3341a09dd0932463a864
AlfredBeNoel <alfredbenoel@gmail.com>
2023-10-12 22:46:40 +0800

    Update fix checkstyle

--- src/main/java/seedu/address/logic/parser/AddMemberCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddMemberCommandParser.java
@@ -5,8 +5,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM;
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM;
 
 import java.util.Set;
 import java.util.stream.Stream;
