854613ff0bf6951a20d22236c43ffd520ea78865
Nam Do Hyun <brandonnam2020@gmail.com>
2023-10-14 00:29:05 +0800

    Fix styling issues

--- src/test/java/seedu/address/logic/parser/ViewModeParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewModeParserTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.commands.ViewExitCommand;
 
 public class ViewModeParserTest {
