120e7d0f580f7a41846f7fd259bf2d2a3629d272
Lim Win Sheng <e0968698@u.nus.edu>
2023-10-13 20:06:21 +0800

    Added newline to EOF

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -150,6 +150,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void deleteMember(Member member) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void setPerson(Person target, Person editedPerson) {
             throw new AssertionError("This method should not be called.");
         }
