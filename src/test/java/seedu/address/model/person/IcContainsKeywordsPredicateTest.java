c3c21eb4d0adcadb861a591ce48b232aa5d33645
chonguschonguschongus <chongzhenxi@gmail.com>
2023-10-13 16:29:48 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/model/person/IcContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/IcContainsKeywordsPredicateTest.java
@@ -1,12 +1,12 @@
 package seedu.address.model.person;
 
-import org.junit.jupiter.api.Test;
-
-import seedu.address.testutil.PersonBuilder;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.PersonBuilder;
 
 public class IcContainsKeywordsPredicateTest {
 
