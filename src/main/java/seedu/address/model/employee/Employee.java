a8bf170a00e6140c85a3b92ce1d56ad0435de01d
Joey Lee <joeylee@comp.nus.edu>
2023-10-13 13:45:06 +0800

    Add toString() method and hashCode() method for Employee class

--- src/main/java/seedu/address/model/employee/Employee.java
+++ src/main/java/seedu/address/model/employee/Employee.java
@@ -1,9 +1,12 @@
 package seedu.address.model.employee;
 
+import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
+
+import java.util.Objects;
 
 public class Employee {
 
@@ -18,4 +21,24 @@ public class Employee {
     private final JoinDate joinDate;
     private final Salary salary;
     private final AnnualLeave annualLeave;
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, phone, email, address, bankAccount, joinDate, salary, annualLeave);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("name", name)
+                .add("phone", phone)
+                .add("email", email)
+                .add("address", address)
+                .add("bank account", bankAccount)
+                .add("join date", joinDate)
+                .add("salary", salary)
+                .add("annual leave", annualLeave)
+                .toString();
+    }
 }
