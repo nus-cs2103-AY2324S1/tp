ac6cf62662a432e8ca248c3638debbad20a57a45
sk2001git <122198303+sk2001git@users.noreply.github.com>
2023-10-13 21:33:12 +0800

    bug fixing
    seems that implementing status.equals(otherPerson.status) causes errors

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -113,15 +113,14 @@ public class Person {
                 && email.equals(otherPerson.email)
                 && address.equals(otherPerson.address)
                 && remark.equals(otherPerson.remark)
-                && tags.equals(otherPerson.tags)
-                && currentStatus.equals(otherPerson.currentStatus);
+                && tags.equals(otherPerson.tags);
 
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, address, tags, remark);
     }
 
     @Override
