f1f4ac24360154435f5b0ccdbf67b0faf9f8554b
jonasongg <jonasong2@gmail.com>
2023-10-13 21:32:42 +0800

    Fix checkstyle for ListApplicantCommand

--- src/main/java/seedu/address/logic/commands/ListApplicantCommand.java
+++ src/main/java/seedu/address/logic/commands/ListApplicantCommand.java
@@ -1,15 +1,14 @@
 package seedu.address.logic.commands;
 
-import seedu.address.model.Model;
-
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import seedu.address.model.Model;
 
 public class ListApplicantCommand extends Command {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all applicants";
-
 
     @Override
     public CommandResult execute(Model model) {
