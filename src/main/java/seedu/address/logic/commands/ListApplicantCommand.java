a64b76d43e38c2a7db2686b88610ad8dbc50d09b
jonasongg <jonasong2@gmail.com>
2023-10-13 21:43:55 +0800

    Fix checkstyle for ListApplicantCommand again

--- src/main/java/seedu/address/logic/commands/ListApplicantCommand.java
+++ src/main/java/seedu/address/logic/commands/ListApplicantCommand.java
@@ -5,6 +5,9 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
 
+/**
+ * Lists all applicants in the address book to the user.
+ */
 public class ListApplicantCommand extends Command {
     public static final String COMMAND_WORD = "list";
 
