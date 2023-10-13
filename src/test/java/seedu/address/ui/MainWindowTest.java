955e878e4b9e8a7ec2e8c7a64c1c4e6e150e5e29
Zhetao <quzhetao2001@gmail.com>
2023-10-13 17:48:34 +0800

    Fix import format

--- src/test/java/seedu/address/ui/MainWindowTest.java
+++ src/test/java/seedu/address/ui/MainWindowTest.java
@@ -1,8 +1,6 @@
 package seedu.address.ui;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit.ApplicationTest;
@@ -10,8 +8,6 @@ import org.testfx.framework.junit.ApplicationTest;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import seedu.address.logic.Logic;
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.testutil.Assert;
 import seedu.address.testutil.LogicStub;
 
 public class MainWindowTest extends ApplicationTest {
@@ -34,7 +30,7 @@ public class MainWindowTest extends ApplicationTest {
 
     @Test
     public void testFillInnerParts() {
-        Assert.assertThrows(NullPointerException.class, () -> mainWindow.fillInnerParts());
+        assertThrows(NullPointerException.class, () -> mainWindow.fillInnerParts());
         // verifyThat("#personListPanel", isVisible());
 
         // verifyThat("#appointmentListPanelPlaceholder #personListPanel", isVisible());
