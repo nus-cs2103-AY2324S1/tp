e84fb55915534d8bdea2a18c744bbe3a791aa912
kokrui <kokruiwong@gmail.com>
2023-10-13 15:20:04 +0800

    Add multi-field find functionality

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,12 +1,18 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Arrays;
+import java.util.function.Predicate;
 
+import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -19,15 +25,35 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+
+        if (!argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        Predicate<Person> checkAllFields = checkAllFieldsPredicate(argMultimap);
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        return new FindCommand(checkAllFields);
+    }
+
+    private static Predicate<Person> checkAllFieldsPredicate(ArgumentMultimap argMultimap) {
+        Predicate<Person> checkNames = person -> (argMultimap.getAllValues(PREFIX_NAME).stream()
+                .allMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword)));
+        Predicate<Person> checkPhones = person -> (argMultimap.getAllValues(PREFIX_PHONE).stream()
+                .allMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getPhone().value, keyword)));
+        Predicate<Person> checkEmails = person -> (argMultimap.getAllValues(PREFIX_EMAIL).stream()
+                .allMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getEmail().value, keyword)));
+        Predicate<Person> checkAddresses = person -> (argMultimap.getAllValues(PREFIX_ADDRESS).stream()
+                .allMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getAddress().value, keyword)));
+
+        // tags are slightly more complicated -- here a person passes the predicate if their tags
+        // is a superset of the tags specified in the find command
+        Predicate<Person> checkTags = person -> (argMultimap.getAllValues(PREFIX_TAG).stream()
+                .allMatch(keyword -> person.getTags().stream()
+                        .anyMatch(tag -> StringUtil.containsWordIgnoreCase(tag.tagName, keyword))));
+
+        return checkNames.and(checkPhones).and(checkEmails).and(checkAddresses).and(checkTags);
     }
 
 }
