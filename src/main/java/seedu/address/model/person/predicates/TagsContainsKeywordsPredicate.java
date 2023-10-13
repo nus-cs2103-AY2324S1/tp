b746b054d18eb4b9f8eabf0ac9271f5fcce0413b
Gavino3o <gavinchiam@gmail.com>
2023-10-13 17:19:18 +0800

    Fix import order in TagContainsKeywordsPredicate

--- src/main/java/seedu/address/model/person/predicates/TagsContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/predicates/TagsContainsKeywordsPredicate.java
@@ -1,12 +1,11 @@
 package seedu.address.model.person.predicates;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.person.Person;
-
-import java.util.function.Predicate;
-import java.util.List;
-
 
 /**
  * Tests that a {@code Person}'s {@code Tags} matches any of the keywords given.
