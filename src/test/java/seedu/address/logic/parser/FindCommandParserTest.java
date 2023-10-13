65f5ca08046fb59551891e5b9902dc9fbe39bb0a
kokrui <kokruiwong@gmail.com>
2023-10-13 15:51:23 +0800

    Add more detailed find command parser tests

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
@@ -9,6 +11,7 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
@@ -17,18 +20,36 @@ public class FindCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+
+    @Test
+    public void parse_validOneArg_returnsFindCommand() throws ParseException {
+        // name arg
+        assertDoesNotThrow(() -> parser.parse(" n/Alice"));
+        assertTrue(parser.parse(" n/Alice") instanceof FindCommand);
+
+        // other arg
+        assertDoesNotThrow(() -> parser.parse(" t/friends"));
+        assertTrue(parser.parse(" t/friends") instanceof FindCommand);
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
-        // no leading and trailing whitespaces
-        FindCommand expectedFindCommand =
-                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
-        assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+    public void parse_validMultipleArgs_returnsFindCommand() throws ParseException {
+        // name args
+        assertDoesNotThrow(() -> parser.parse(" n/Alice n/Li"));
+        assertTrue(parser.parse(" n/Alice n/Li") instanceof FindCommand);
 
-        // multiple whitespaces between keywords
-        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+        // multiple distinct args
+        assertDoesNotThrow(() -> parser.parse(" n/Alice t/friends"));
+        assertTrue(parser.parse(" n/Alice t/friends") instanceof FindCommand);
+
+        // multiple distinct args with repeated fields
+        assertDoesNotThrow(() -> parser.parse(" n/Alice t/colleagues t/friends"));
+        assertTrue(parser.parse( " n/Alice t/colleagues t/friends") instanceof FindCommand);
+
     }
 
 }
