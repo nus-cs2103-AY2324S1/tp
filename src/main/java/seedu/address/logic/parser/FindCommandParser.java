cf051a332016b4b4579314ab521f2f2c4899f0e5
kokrui <kokruiwong@gmail.com>
2023-10-14 22:14:16 +0800

    Update FindCommandParser ]to use custom parser/tokenizer

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -7,10 +7,12 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.parser.FindFilterStringTokenizer.Token;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Person;
 
@@ -22,6 +24,7 @@ public class FindCommandParser implements Parser<FindCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns a FindCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
@@ -30,14 +33,13 @@ public class FindCommandParser implements Parser<FindCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
-                PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+        FindFilterStringTokenizer tokenizer = new FindFilterStringTokenizer(args);
+        List<Token> tokens = tokenizer.tokenize();
 
-        if (!argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-        }
+        FindExpressionParser parser = new FindExpressionParser();
+        FindExpressionParser.ExprNode filterStringAst = parser.parse(tokens);
 
-        Predicate<Person> checkAllFields = checkAllFieldsPredicate(argMultimap);
+        Predicate<Person> checkAllFields = filterStringAst.toPredicate();
 
         return new FindCommand(checkAllFields);
     }
