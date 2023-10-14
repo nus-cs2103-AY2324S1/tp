8f6ddcfb6aa5ee47e78d294ee8400932f8cd9dd5
kokrui <kokruiwong@gmail.com>
2023-10-14 23:52:46 +0800

    Add tests for find expression parser

---
+++ src/test/java/seedu/address/logic/parser/FindExpressionParserTest.java
@@ -0,0 +1,56 @@
+package seedu.address.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class FindExpressionParserTest {
+
+    private final FindExpressionParser parser = new FindExpressionParser();
+
+    @Test
+    public void fromPrefix_invalidPrefix_throwsParseException() {
+        assertThrows(ParseException.class, () -> FindExpressionParser.FindSupportedField.fromPrefix(" "));
+        assertThrows(ParseException.class, () -> FindExpressionParser.FindSupportedField.fromPrefix("asdf"));
+
+        // existent but unsupported fields
+        assertThrows(ParseException.class, () -> FindExpressionParser.FindSupportedField.fromPrefix("tg/"));
+        assertThrows(ParseException.class, () -> FindExpressionParser.FindSupportedField.fromPrefix("e2/"));
+        assertThrows(ParseException.class, () -> FindExpressionParser.FindSupportedField.fromPrefix("li/"));
+    }
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertThrows(ParseException.class, () -> parser.parse(new ArrayList<>()));
+    }
+
+    @Test
+    public void conditionNodeToPredicate_validField_returnsPredicate() throws ParseException {
+        assertTrue(new FindExpressionParser.ConditionNode(
+                FindExpressionParser.FindSupportedField.NAME, "Alice").toPredicate()
+                instanceof Predicate);
+
+        assertTrue(new FindExpressionParser.ConditionNode(
+                FindExpressionParser.FindSupportedField.PHONE, "12345").toPredicate()
+                instanceof Predicate);
+
+        assertTrue(new FindExpressionParser.ConditionNode(
+                FindExpressionParser.FindSupportedField.EMAIL, "haha@haha.com").toPredicate()
+                instanceof Predicate);
+
+        assertTrue(new FindExpressionParser.ConditionNode(
+                FindExpressionParser.FindSupportedField.ADDRESS, "123, haha street").toPredicate()
+                instanceof Predicate);
+
+        assertTrue(new FindExpressionParser.ConditionNode(
+                FindExpressionParser.FindSupportedField.TAG, "friends").toPredicate()
+                instanceof Predicate);
+
+    }
+}
