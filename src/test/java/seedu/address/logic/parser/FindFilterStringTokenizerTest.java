a67397d693b2732b0aa2ebcd945e34590c25a2d0
kokrui <kokruiwong@gmail.com>
2023-10-14 23:57:26 +0800

    Add tests for FindFilterStringTokenizer

---
+++ src/test/java/seedu/address/logic/parser/FindFilterStringTokenizerTest.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FindFilterStringTokenizerTest {
+
+    @Test
+    public void tokenToString_validInput_success() {
+        assertEquals(new FindFilterStringTokenizer.Token(
+                FindFilterStringTokenizer.Token.Type.AND, "Alice").toString(), "AND:Alice");
+        assertEquals(new FindFilterStringTokenizer.Token(
+                FindFilterStringTokenizer.Token.Type.OR, "Alice").toString(), "OR:Alice");
+        assertEquals(new FindFilterStringTokenizer.Token(
+                FindFilterStringTokenizer.Token.Type.NOT, "Alice").toString(), "NOT:Alice");
+        assertEquals(new FindFilterStringTokenizer.Token(
+                FindFilterStringTokenizer.Token.Type.LPAREN, "Alice").toString(), "LPAREN:Alice");
+        assertEquals(new FindFilterStringTokenizer.Token(
+                FindFilterStringTokenizer.Token.Type.RPAREN, "Alice").toString(), "RPAREN:Alice");
+    }
+}
