44739be9042e8302179787dde833c015b011df59
yytan25 <tan.yang.yi.2001@gmail.com>
2023-10-15 18:21:53 +0800

    Create class "Band"

---
+++ src/main/java/seedu/address/model/band/Band.java
@@ -0,0 +1,53 @@
+package seedu.address.model.band;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.musician.Musician;
+import seedu.address.model.musician.Name;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Represents a Band in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Band {
+
+    private final Name name;
+
+    private final Set<Tag> genres = new HashSet<>();
+
+    private final Set<Musician> musicians;
+
+    public Band(Name name, Set<Tag> genreTags, Set<Musician> musicians) {
+        requireAllNonNull(name, genreTags, musicians);
+        this.name = name;
+        this.genres.addAll(genreTags);
+        this.musicians = musicians;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public Set<Tag> getGenres() {
+        return Collections.unmodifiableSet(genres);
+    }
+
+    public Set<Musician> getMusicians() {
+        return Collections.unmodifiableSet(musicians);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .add("name", name)
+            .add("genres", genres)
+            .add("musicians", musicians)
+            .toString();
+    }
+}
