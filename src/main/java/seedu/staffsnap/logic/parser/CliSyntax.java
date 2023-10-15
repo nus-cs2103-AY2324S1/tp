73c74a541241ce521076ec444b7b1bf4e8035743
craigtonlian <108467437+craigtonlian@users.noreply.github.com>
2023-10-15 16:52:52 +0800

    Update command prefices

--- src/main/java/seedu/staffsnap/logic/parser/CliSyntax.java
+++ src/main/java/seedu/staffsnap/logic/parser/CliSyntax.java
@@ -7,9 +7,9 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_PHONE = new Prefix("hp/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_POSITION = new Prefix("a/");
+    public static final Prefix PREFIX_POSITION = new Prefix("p/");
     public static final Prefix PREFIX_INTERVIEW = new Prefix("t/");
 
 }
