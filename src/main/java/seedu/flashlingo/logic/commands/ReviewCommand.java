ad3149459e5003613e381849b127ce24d61670fe
Song-Mengfei <e1075530@u.nus.edu>
2023-10-16 21:49:34 +0800

    Add Review Command

---
+++ src/main/java/seedu/flashlingo/logic/commands/ReviewCommand.java
@@ -0,0 +1,32 @@
+package seedu.flashlingo.logic.commands;
+
+import seedu.flashlingo.logic.Messages;
+import seedu.flashlingo.model.Model;
+import seedu.flashlingo.model.flashcard.WordOverduePredicate;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Finds and lists all flashcards in flashlingo who is overdue.
+ */
+public class ReviewCommand extends Command {
+
+    public static final String COMMAND_WORD = "review";
+
+    public static final String MESSAGE_SUCCESS = "Listed all flashcards you need to review";
+
+    private final WordOverduePredicate predicate;
+
+    public ReviewCommand() {
+        this.predicate = new WordOverduePredicate();
+    }
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredFlashCardList(predicate);
+        return new CommandResult( MESSAGE_SUCCESS + "\n"
+                + String.format(Messages.MESSAGE_FLASHCARDS_LISTED_OVERVIEW + "\n"
+                                + model.getFilteredFlashCardList(),
+                        model.getFilteredFlashCardList().size()));
+    }
+}
