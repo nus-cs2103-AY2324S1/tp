c038bce261197c59900363b0a9c3d7b5b42010aa
winson8222 <winson8222@hotmail.com>
2023-10-17 00:48:17 +0800

    Fix error when data does not exist

--- src/main/java/seedu/address/storage/JsonDeckStorage.java
+++ src/main/java/seedu/address/storage/JsonDeckStorage.java
@@ -3,6 +3,7 @@ package seedu.address.storage;
 import static java.util.Objects.requireNonNull;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.logging.Logger;
@@ -35,6 +36,7 @@ public class JsonDeckStorage implements DeckStorage {
 
     @Override
     public Optional<ReadOnlyDeck> readDeck() throws DataLoadingException {
+
         return readDeck(filePath);
     }
 
@@ -46,6 +48,17 @@ public class JsonDeckStorage implements DeckStorage {
      */
     public Optional<ReadOnlyDeck> readDeck(Path filePath) throws DataLoadingException {
         requireNonNull(filePath);
+
+        if (!Files.exists(filePath)) {
+            try {
+                Files.createDirectories(filePath.getParent());
+                Files.createFile(filePath); // Create the 'deck.json' file
+                // You might want to initialize deck.json with some default content,
+                // but for the purpose of this example, I'm simply creating an empty file.
+            } catch (IOException e) {
+                throw new DataLoadingException(e);
+            }
+        }
 
         Optional<JsonSerializableDeck> jsonDeck = JsonUtil.readJsonFile(
                 filePath, JsonSerializableDeck.class);
@@ -60,6 +73,7 @@ public class JsonDeckStorage implements DeckStorage {
             throw new DataLoadingException(ive);
         }
     }
+
 
     @Override
     public void saveDeck(ReadOnlyDeck deck) throws IOException {
