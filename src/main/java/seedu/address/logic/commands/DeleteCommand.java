86c3519bc0e70a2358cc338c861d1b7d4dfc1900
nubnubyas <e0970089@u.nus.edu>
2023-10-16 16:49:44 +0800

    Add DeletePersonDescriptor

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -2,14 +2,27 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Appointment;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.MedicalHistory;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
 
 /**
  * Deletes a person identified using it's displayed index from the address book.
@@ -27,8 +40,16 @@ public class DeleteCommand extends Command {
 
     private final Index targetIndex;
 
+    private final DeletePersonDescriptor deletePersonDescriptor;
+
     public DeleteCommand(Index targetIndex) {
         this.targetIndex = targetIndex;
+        this.deletePersonDescriptor = null;
+    }
+
+    public DeleteCommand(DeletePersonDescriptor deletePersonDescriptor) {
+        this.targetIndex = null;
+        this.deletePersonDescriptor = deletePersonDescriptor;
     }
 
     @Override
@@ -65,5 +86,84 @@ public class DeleteCommand extends Command {
         return new ToStringBuilder(this)
                 .add("targetIndex", targetIndex)
                 .toString();
+    }
+
+    /**
+     * Stores the fields that is to be deleted from a person in the address book.
+     */
+    public static class DeletePersonDescriptor {
+        private boolean phone;
+        private boolean medicalHistory;
+        private boolean appointment;
+        private boolean email;
+        private boolean address;
+        private boolean tags;
+
+        public DeletePersonDescriptor() {
+        }
+
+        public void setPhone(Phone phone) {
+            this.phone = true;
+        }
+
+        public void setEmail(Email email) {
+            this.email = true;
+        }
+
+        public void setAddress(Address address) {
+            this.address = true;
+        }
+
+        public void setMedicalHistory(MedicalHistory medicalHistory) {
+            this.medicalHistory = true;
+        }
+
+        public void setAppointment(Appointment appointment) {
+            this.appointment = true;
+        }
+
+        /**
+         * Sets {@code tags} to this object's {@code tags}.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public void setTags(Set<Tag> tags) {
+            this.tags = true;
+        }
+
+        public boolean isAllFalse() {
+            return !(phone || email || address || tags || medicalHistory || appointment);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof DeletePersonDescriptor)) {
+                return false;
+            }
+
+            DeletePersonDescriptor otherDeletePersonDescriptor = (DeletePersonDescriptor) other;
+            return Objects.equals(phone, otherDeletePersonDescriptor.phone)
+                    && Objects.equals(email, otherDeletePersonDescriptor.email)
+                    && Objects.equals(address, otherDeletePersonDescriptor.address)
+                    && Objects.equals(tags, otherDeletePersonDescriptor.tags)
+                    && Objects.equals(medicalHistory, otherDeletePersonDescriptor.medicalHistory)
+                    && Objects.equals(appointment, otherDeletePersonDescriptor.appointment);
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this)
+                    .add("phone", phone)
+                    .add("email", email)
+                    .add("address", address)
+                    .add("tags", tags)
+                    .add("medicalHistory", medicalHistory)
+                    .add("appointment", appointment)
+                    .toString();
+        }
     }
 }
--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,10 +1,23 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEDICAL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_START;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.DeleteCommand.DeletePersonDescriptor;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Nric;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -12,13 +25,41 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class DeleteCommandParser implements Parser<DeleteCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
+     * Parses the given {@code String} of arguments in the context of the
+     * DeleteCommand
      * and returns a DeleteCommand object for execution.
+     * 
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
         try {
+            requireNonNull(args);
+            ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_NRIC, PREFIX_PHONE,
+                    PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_START, PREFIX_END, PREFIX_MEDICAL, PREFIX_TAG);
+
+            boolean hasNamePrefix = argMultimap.getValue(PREFIX_NAME).isPresent();
+            boolean hasNricPrefix = argMultimap.getValue(PREFIX_NRIC).isPresent();
+
+            if (!hasNamePrefix && !hasNricPrefix) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+            }
+
+            argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_NRIC, PREFIX_PHONE, PREFIX_EMAIL,
+                    PREFIX_ADDRESS, PREFIX_START, PREFIX_END, PREFIX_MEDICAL, PREFIX_TAG);
+
+            Name name = null;
+            Nric nric = null;
+
             Index index = ParserUtil.parseIndex(args);
+
+            if (hasNamePrefix) {
+                name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+            } else if (hasNricPrefix) {
+                nric = ParserUtil.parseNric(argMultimap.getValue(PREFIX_NRIC).get());
+            }
+
+            DeletePersonDescriptor deletePersonDescriptor = new DeletePersonDescriptor();
+
             return new DeleteCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
