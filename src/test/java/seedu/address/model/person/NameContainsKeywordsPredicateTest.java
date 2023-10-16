5b047e8d1310856780b584cd0d6c68357f7f5dc7
Saezenn <teng.hxa@gmail.com>
2023-10-17 03:55:15 +0800

    Remove old find tests

--- src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
@@ -1,4 +1,4 @@
-package seedu.address.model.person;
+/*package seedu.address.model.person;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -83,3 +83,4 @@ public class NameContainsKeywordsPredicateTest {
         assertEquals(expected, predicate.toString());
     }
 }
+*/
