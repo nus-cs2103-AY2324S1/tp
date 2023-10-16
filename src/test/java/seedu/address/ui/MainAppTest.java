94fa57733725532e57c68aa5ef9b6f32ee094bac
Zhetao <quzhetao2001@gmail.com>
2023-10-16 17:48:34 +0800

    Add proper set up for UI testing

---
+++ src/test/java/seedu/address/ui/MainAppTest.java
@@ -0,0 +1,53 @@
+package seedu.address.ui;
+
+import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+import seedu.address.MainApp;
+
+import java.util.concurrent.TimeoutException;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class MainAppTest extends ApplicationTest {
+
+    private MainApp mainApp;
+
+    @Before
+    public void setUp() throws Exception {
+        ApplicationTest.launch(MainApp.class);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        mainApp = new MainApp();
+        mainApp.start(stage);
+        stage.show();
+    }
+
+    @After
+    public void afterEachTest() throws TimeoutException {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
+
+    public <T extends Node> T find (String query) {
+        return (T) lookup(query).queryAll().iterator().next();
+    }
+
+    @Test
+    public void fillInnerPartsTest() {
+        verifyThat("#personListPanel", NodeMatchers.isVisible());
+        verifyThat("#resultDisplay", NodeMatchers.isVisible());
+        verifyThat("#statusBarFooter", NodeMatchers.isVisible());
+        verifyThat("#commandBox", NodeMatchers.isVisible());
+    }
+}
--- src/test/java/seedu/address/ui/MainWindowTest.java
+++ src/test/java/seedu/address/ui/MainWindowTest.java
@@ -1,42 +1,26 @@
 package seedu.address.ui;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.testfx.api.FxAssert.verifyThat;
 
 import org.junit.jupiter.api.Test;
-import org.testfx.framework.junit.ApplicationTest;
 
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import seedu.address.logic.Logic;
-import seedu.address.testutil.LogicStub;
+import org.testfx.matcher.base.NodeMatchers;
 
-public class MainWindowTest extends ApplicationTest {
-
-    private MainWindow mainWindow;
-    private Logic logic;
-
-
-    @Override
-    public void start(Stage stage) {
-        logic = new LogicStub();
-        mainWindow = new MainWindow(stage, logic);
-        mainWindow.fillInnerParts();
-        Scene scene = mainWindow.getRoot().getScene();
-        stage.setScene(scene);
-        stage.show();
-
-    }
+public class MainWindowTest extends MainAppTest {
 
 
     @Test
     public void testFillInnerParts() {
-        assertThrows(NullPointerException.class, () -> mainWindow.fillInnerParts());
         // verifyThat("#personListPanel", isVisible());
 
         // verifyThat("#appointmentListPanelPlaceholder #personListPanel", isVisible());
 
         // verifyThat("#statusbarPlaceholder #statusBarFooter", isVisible());
 
-        // verifyThat("#commandBoxPlaceholder #commandBox", isVisible());
+
+
+//        verifyThat("#commandBoxPlaceholder", NodeMatchers.isVisible());
+
+        verifyThat("#menuBar", NodeMatchers.isNotNull());
     }
 }
