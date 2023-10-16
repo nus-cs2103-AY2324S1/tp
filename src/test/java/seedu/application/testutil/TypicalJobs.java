66885deaaa5ed10589ac5953dd43f12cba163490
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2023-10-16 22:34:25 +0800

    Config build.gradle for shadowJar

--- src/test/java/seedu/application/testutil/TypicalJobs.java
+++ src/test/java/seedu/application/testutil/TypicalJobs.java
@@ -2,8 +2,6 @@ package seedu.application.testutil;
 
 import static seedu.application.logic.commands.CommandTestUtil.VALID_COMPANY_CHEF;
 import static seedu.application.logic.commands.CommandTestUtil.VALID_COMPANY_CLEANER;
-import static seedu.application.logic.commands.CommandTestUtil.VALID_DEADLINE_CHEF;
-import static seedu.application.logic.commands.CommandTestUtil.VALID_DEADLINE_CLEANER;
 import static seedu.application.logic.commands.CommandTestUtil.VALID_ROLE_CHEF;
 import static seedu.application.logic.commands.CommandTestUtil.VALID_ROLE_CLEANER;
 
@@ -21,31 +19,31 @@ import seedu.application.model.job.Job;
 public class TypicalJobs {
 
     public static final Job SOFTWARE_ENGINEER = new JobBuilder().withRole("Software Engineer")
-            .withCompany("Google").withDeadline(Deadline.TO_ADD_DEADLINE).build();
+        .withCompany("Google").withDeadline(Deadline.TO_ADD_DEADLINE).build();
     public static final Job TEACHER = new JobBuilder().withRole("Teacher")
-            .withCompany("NUS").withDeadline(Deadline.TO_ADD_DEADLINE).build();
+        .withCompany("NUS").withDeadline(Deadline.TO_ADD_DEADLINE).build();
     public static final Job WAITER = new JobBuilder().withRole("Waiter")
-            .withCompany("McDonalds").withDeadline(Deadline.TO_ADD_DEADLINE).build();
+        .withCompany("McDonalds").withDeadline(Deadline.TO_ADD_DEADLINE).build();
     public static final Job PILOT = new JobBuilder().withRole("Pilot")
-            .withCompany("Singapore Airlines").withDeadline(Deadline.TO_ADD_DEADLINE).build();
+        .withCompany("Singapore Airlines").withDeadline(Deadline.TO_ADD_DEADLINE).build();
     public static final Job POLICE_OFFICER = new JobBuilder().withRole("Police Officer")
-            .withCompany("SPF").withDeadline(Deadline.TO_ADD_DEADLINE).build();
+        .withCompany("SPF").withDeadline(Deadline.TO_ADD_DEADLINE).build();
     public static final Job FRUIT_SELLER = new JobBuilder().withRole("Fruit Seller")
-            .withCompany("ABC Fruits").withDeadline(Deadline.TO_ADD_DEADLINE).build();
+        .withCompany("ABC Fruits").withDeadline(Deadline.TO_ADD_DEADLINE).build();
     public static final Job GRASS_CUTTER = new JobBuilder().withRole("Grass Cutter")
-            .withCompany("XYZ Gardening").withDeadline(Deadline.TO_ADD_DEADLINE).build();
+        .withCompany("XYZ Gardening").withDeadline(Deadline.TO_ADD_DEADLINE).build();
 
     // Manually added
     public static final Job ARTIST = new JobBuilder().withRole("Artist")
-            .withCompany("Van Gogh Paintings").build();
+        .withCompany("Van Gogh Paintings").build();
     public static final Job SPRINTER = new JobBuilder().withRole("Sprinter")
-            .withCompany("Nike").build();
+        .withCompany("Nike").build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
     public static final Job CHEF = new JobBuilder().withRole(VALID_ROLE_CHEF)
-            .withCompany(VALID_COMPANY_CHEF).withDeadline(VALID_DEADLINE_CHEF).build();
+        .withCompany(VALID_COMPANY_CHEF).withDeadline(Deadline.TO_ADD_DEADLINE).build();
     public static final Job CLEANER = new JobBuilder().withRole(VALID_ROLE_CLEANER)
-            .withCompany(VALID_COMPANY_CLEANER).withDeadline(VALID_DEADLINE_CLEANER).build();
+        .withCompany(VALID_COMPANY_CLEANER).withDeadline(Deadline.TO_ADD_DEADLINE).build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
@@ -65,6 +63,6 @@ public class TypicalJobs {
 
     public static List<Job> getTypicalJobs() {
         return new ArrayList<>(Arrays.asList(SOFTWARE_ENGINEER, TEACHER, WAITER, PILOT,
-                POLICE_OFFICER, FRUIT_SELLER, GRASS_CUTTER));
+            POLICE_OFFICER, FRUIT_SELLER, GRASS_CUTTER));
     }
 }
