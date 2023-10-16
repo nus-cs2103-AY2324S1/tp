7dfa192ad0ddfdcaccdbebb2fea603dae891b393
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2023-10-16 13:56:24 +0800

    Add FilterCommand skeleton

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.commands;
+
+import javax.xml.crypto.dsig.spec.XPathType.Filter;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+public class FilterCommand extends Command {
+    public static final String COMMAND_WORD = "filter";
+
+    // TODO: Implement usage message
+    public static final String MESSAGE_USAGE = COMMAND_WORD;
+
+    public final String operation;
+    public final String type;
+    public final String value;
+
+    public FilterCommand(String operation, String type, String value) {
+        this.operation = operation;
+        this.type = type;
+        this.value = value;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(
+                String.format("%s, %s, %s", operation, type, value));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FilterCommand)) {
+            return false;
+        }
+
+        FilterCommand e = (FilterCommand) other;
+        return operation.equals(e.operation)
+                && type.equals(e.type)
+                && value.equals(e.value);
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class FilterCommandParser implements Parser<FilterCommand> {
+    
+    public FilterCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
+        }
+
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        // TODO: Implement parsing
+        return new FilterCommand("operation", "type", "value");
+    }
+
+}
