116d4ae2cf395af17ef241694abcd9aff1af4a8b
Zhetao <quzhetao2001@gmail.com>
2023-10-16 18:21:50 +0800

    Fix styling in MainAppTest

--- src/test/java/seedu/address/ui/MainAppTest.java
+++ src/test/java/seedu/address/ui/MainAppTest.java
@@ -1,21 +1,16 @@
 package seedu.address.ui;
 
-import static org.testfx.api.FxAssert.verifyThat;
-
 import java.util.concurrent.TimeoutException;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
-import org.testfx.matcher.base.NodeMatchers;
 
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 import seedu.address.MainApp;
-
 
 public class MainAppTest extends ApplicationTest {
 
