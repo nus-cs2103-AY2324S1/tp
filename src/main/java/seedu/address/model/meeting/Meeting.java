0f9363d456af6fdafe04ceb9cfa8ce3e1058980e
Jason <jiacan.h01@gmail.com>
2023-10-16 16:16:22 +0800

    Add Tags to Meeting model

--- src/main/java/seedu/address/model/meeting/Meeting.java
+++ src/main/java/seedu/address/model/meeting/Meeting.java
@@ -3,12 +3,15 @@ package seedu.address.model.meeting;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.tag.Tag;
 
 /**
  * Represents a Meeting in the address book.
@@ -20,15 +23,17 @@ public class Meeting {
     private final Location location;
     private final MeetingTime meetingTime;
     private final Set<Attendee> attendees;
+    private final Set<Tag> tags;
 
     /**
      * Every field must be present and not null.
      */
-    public Meeting(Title title, Location location, LocalDateTime start, LocalDateTime end, Set<Attendee> attendees) {
+    public Meeting(Title title, Location location, LocalDateTime start, LocalDateTime end, Set<Attendee> attendees, Set<Tag> tags) {
         this.title = title;
         this.location = location;
         this.meetingTime = new MeetingTime(start, end);
         this.attendees = new LinkedHashSet<>(attendees);
+        this.tags = new HashSet<>(tags);
     }
 
     public Title getTitle() {
@@ -64,8 +69,15 @@ public class Meeting {
      * Returns an attendee at the given {@code index}
      */
     public Attendee getAttendee(Index index) {
-        // TODO: catch index out of bounds
         return new ArrayList<>(attendees).get(index.getZeroBased());
+    }
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
     }
 
     /**
@@ -101,7 +113,8 @@ public class Meeting {
         return title.equals(otherMeeting.title)
                 && location.equals(otherMeeting.location)
                 && meetingTime.equals(otherMeeting.meetingTime)
-                && attendees.equals(otherMeeting.attendees);
+                && attendees.equals(otherMeeting.attendees)
+                && tags.equals(otherMeeting.tags);
     }
 
     @Override
@@ -118,6 +131,7 @@ public class Meeting {
                 .add("start", meetingTime.getStart())
                 .add("end", meetingTime.getEnd())
                 .add("attendees", attendees)
+                .add("tags", tags)
                 .toString();
     }
 }
