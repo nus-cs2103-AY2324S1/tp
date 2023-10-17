d139817876b8d5febb95cf078f129465216c92af
nicrandomlee <leejpnicholas@gmail.com>
2023-10-18 01:17:37 +0800

    Remove RemarkCommand tutorial

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -8,76 +8,62 @@ import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.model.event.Event;
 import seedu.address.model.person.Person;
-
 /**
  * The API of the Model component.
  */
 public interface Model {
     /** {@code Predicate} that always evaluate to true */
     Predicate<Person> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
-
     /**
      * Replaces user prefs data with the data in {@code userPrefs}.
      */
     void setUserPrefs(ReadOnlyUserPrefs userPrefs);
-
     /**
      * Returns the user prefs.
      */
     ReadOnlyUserPrefs getUserPrefs();
-
     /**
      * Returns the user prefs' GUI settings.
      */
     GuiSettings getGuiSettings();
-
     /**
      * Sets the user prefs' GUI settings.
      */
     void setGuiSettings(GuiSettings guiSettings);
-
     /**
      * Returns the user prefs' address book file path.
      */
     Path getAddressBookFilePath();
-
     /**
      * Sets the user prefs' address book file path.
      */
     void setAddressBookFilePath(Path addressBookFilePath);
-
     /**
      * Replaces address book data with the data in {@code addressBook}.
      */
     void setAddressBook(ReadOnlyAddressBook addressBook);
-
     /** Returns the AddressBook */
     ReadOnlyAddressBook getAddressBook();
-
     /**
      * Returns true if a person with the same identity as {@code person} exists in the address book.
      */
     boolean hasPerson(Person person);
-
     /**
      * Deletes the given person.
      * The person must exist in the address book.
      */
     void deletePerson(Person target);
-
     /**
      * Adds the given person.
      * {@code person} must not already exist in the address book.
      */
     void addPerson(Person person);
-
     /**
      * Replaces the given person {@code target} with {@code editedPerson}.
      * {@code target} must exist in the address book.
      * The person identity of {@code editedPerson} must not be the same as another existing person in the address book.
      */
     void setPerson(Person target, Person editedPerson);
-
     /**
      * Check if calendar can add event
      *
@@ -85,17 +71,14 @@ public interface Model {
      * @return true if possible, false otherwise
      */
     boolean canAddEvent(Event event);
-
     /**
      * Adds an event into the calendar
      *
      * @param event event to be added
      */
     void addEvent(Event event);
-
     /** Returns an unmodifiable view of the filtered person list */
     ObservableList<Person> getFilteredPersonList();
-
     /**
      * Updates the filter of the filtered person list to filter by the given {@code predicate}.
      * @throws NullPointerException if {@code predicate} is null.
@@ -106,5 +89,5 @@ public interface Model {
      * Sorts the filtered person list by the given {@code predicate}.
      * @throws NullPointerException if {@code predicate} is null.
      */
-    void sortPersonList(Comparator<Person> expenseComparator);
+    void sortPersonList(Comparator<Person> personComparator);
 }
