39b6d618f47841b4cdeca8fcc85f92f9f816956d
kiwibang <>
2023-10-17 20:51:24 +0800

    Fix CheckStyle issues in the TimeParser test class

--- src/test/java/seedu/address/logic/parser/TimeParserTest.java
+++ src/test/java/seedu/address/logic/parser/TimeParserTest.java
@@ -1,10 +1,11 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class TimeParserTest {
     @Test
