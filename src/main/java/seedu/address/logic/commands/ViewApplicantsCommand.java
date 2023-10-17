59c0cd1d6b39a259860ddb369e7f4982cb58a245
matochichap <ruijia.chan@gmail.com>
2023-10-17 22:14:31 +0800

    Change back to PREDICATE_SHOW_ALL_PERSONS

--- src/main/java/seedu/address/logic/commands/ViewApplicantsCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewApplicantsCommand.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_APPLICANTS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
 
@@ -16,7 +16,7 @@ public class ViewApplicantsCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredApplicantList(PREDICATE_SHOW_ALL_APPLICANTS);
+        model.updateFilteredApplicantList(PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
