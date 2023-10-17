963c44de69eeeaccd3cfa3f266193c054ec27561
SimWPEric <e0968924@u.nus.edu>
2023-10-17 14:25:06 +0800

    Implement deleteAppointment

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -111,6 +111,11 @@ public class ModelManager implements Model {
         addressBook.setPerson(target, editedPerson);
     }
 
+    @Override
+    public void deleteAppointment(Appointment target) {
+        addressBook.removeAppointment(target);
+    }
+
     //=========== Filtered Person List Accessors =============================================================
 
     /**
