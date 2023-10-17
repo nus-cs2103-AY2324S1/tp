020a9dcab2ed83d1da740d8134387d7bbb51440c
lambraydon <braydonlam2001@gmail.com>
2023-10-17 17:30:45 +0800

    Add Day field to Person

---
+++ src/main/java/seedu/address/model/person/Day.java
@@ -0,0 +1,55 @@
+package seedu.address.model.person;
+
+import java.time.DayOfWeek;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents the day where the Tutee has tuition every week.
+ * Guarantees: immutable; is valid as declared in
+ */
+public class Day {
+    public final DayOfWeek value;
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Days should be written using their full names or their first three letters, and it should not be blank";
+
+    /**
+     * Constructs a {@code Day}.
+     *
+     * @param day A valid day.
+     */
+    public Day(String day) {
+        requireNonNull(day);
+        value = parse(day);
+    }
+
+    public DayOfWeek parse(String test) {
+        String lowerCaseTest = test.toLowerCase();
+        switch (lowerCaseTest) {
+        case "mon":
+        case "monday":
+            return DayOfWeek.MONDAY;
+        case "tue":
+        case "tuesday":
+            return DayOfWeek.TUESDAY;
+        case "wed":
+        case "wednesday":
+            return DayOfWeek.WEDNESDAY;
+        case "thu":
+        case "thursday":
+            return DayOfWeek.THURSDAY;
+        case "fri":
+        case "friday":
+            return DayOfWeek.FRIDAY;
+        case "sat":
+        case "saturday":
+            return DayOfWeek.SATURDAY;
+        case "sun":
+        case "sunday":
+            return DayOfWeek.SUNDAY;
+        default:
+            throw new IllegalArgumentException(MESSAGE_CONSTRAINTS);
+        }
+    }
+}
