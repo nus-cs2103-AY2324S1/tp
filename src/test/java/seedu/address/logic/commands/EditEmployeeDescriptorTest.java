98f8a96da9c8f922c361e05ff6cde852a8754a82
He Yifan <e0968838@u.nus.edu>
2023-10-17 16:26:41 +0800

    Optimise code style

--- src/test/java/seedu/address/logic/commands/EditEmployeeDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditEmployeeDescriptorTest.java
@@ -1,9 +1,8 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
