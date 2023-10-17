55b5e2a04ba48560a90a570cfa431cbc3f6cea7a
Gavino3o <gavinchiam@gmail.com>
2023-10-17 17:21:46 +0800

    Remove unused imports in tests

--- src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
+++ src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
@@ -3,8 +3,6 @@ package seedu.address.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.FindPredicateMap;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.PersonType;
 
