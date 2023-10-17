2c73ee75663004692bdffee179b525654cf0a605
awhb <awhangbin@gmail.com>
2023-10-18 04:31:48 +0800

    Fix checkstyle issues

--- src/test/java/networkbook/logic/commands/AddCommandTest.java
+++ src/test/java/networkbook/logic/commands/AddCommandTest.java
@@ -3,7 +3,6 @@ package networkbook.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
