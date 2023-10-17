26fd0648a8c5f15904da50328f62f4f445160f1b
matochichap <ruijia.chan@gmail.com>
2023-10-17 21:58:33 +0800

    Edit Logic

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -9,7 +9,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Member;
-import seedu.address.model.person.Person;
 
 /**
  * API of the Logic component
@@ -30,9 +29,6 @@ public interface Logic {
      * @see seedu.address.model.Model#getAddressBook()
      */
     ReadOnlyAddressBook getAddressBook();
-
-    /** Returns an unmodifiable view of the filtered list of persons */
-    ObservableList<Person> getFilteredPersonList();
 
     /** Returns an unmodifiable view of the filtered list of member */
     ObservableList<Member> getFilteredMemberList();
