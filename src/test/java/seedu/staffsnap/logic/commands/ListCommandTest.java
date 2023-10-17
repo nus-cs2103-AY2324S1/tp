56a1ef3647eaef522b864fb4d67a6d40b340f136
craigtonlian <108467437+craigtonlian@users.noreply.github.com>
2023-10-17 21:37:54 +0800

    Update testcases for ListCommand

--- src/test/java/seedu/staffsnap/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/staffsnap/logic/commands/ListCommandTest.java
@@ -1,5 +1,6 @@
 package seedu.staffsnap.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.staffsnap.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.staffsnap.logic.commands.CommandTestUtil.showApplicantAtIndex;
 import static seedu.staffsnap.testutil.TypicalApplicants.getTypicalApplicantBook;
@@ -8,9 +9,11 @@ import static seedu.staffsnap.testutil.TypicalIndexes.INDEX_FIRST_APPLICANT;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.staffsnap.logic.commands.exceptions.CommandException;
 import seedu.staffsnap.model.Model;
 import seedu.staffsnap.model.ModelManager;
 import seedu.staffsnap.model.UserPrefs;
+import seedu.staffsnap.model.applicant.Descriptor;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
@@ -35,5 +38,23 @@ public class ListCommandTest {
     public void execute_listIsFiltered_showsEverything() {
         showApplicantAtIndex(model, INDEX_FIRST_APPLICANT);
         assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_sortAndList_success() throws CommandException {
+        // Execute the SortCommand
+        SortCommand sortCommand = new SortCommand(Descriptor.NAME);
+        CommandResult sortResult = sortCommand.execute(model);
+
+        // Create a ListCommand
+        ListCommand listCommand = new ListCommand();
+        CommandResult listResult = listCommand.execute(model);
+
+        // Assert that both commands were successful
+        assertEquals(SortCommand.MESSAGE_SUCCESS, sortResult.getFeedbackToUser());
+        assertEquals(ListCommand.MESSAGE_SUCCESS, listResult.getFeedbackToUser());
+
+        // Assert that the actual and expected models are the same
+        assertEquals(expectedModel, model);
     }
 }
