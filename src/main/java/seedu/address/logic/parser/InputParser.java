3b73a56cff964e6c33d10e3198a4c9c633ec8d3c
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-10-19 07:03:44 +0800

    ref(src): Reduce some logging

--- src/main/java/seedu/address/logic/parser/InputParser.java
+++ src/main/java/seedu/address/logic/parser/InputParser.java
@@ -3,12 +3,10 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.Messages.MESSAGE_UNKNOWN_COMMAND;
 
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.annotation.Nullable;
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
@@ -20,16 +18,16 @@ import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+
+
 /**
  * Parses user input.
  */
 public class InputParser {
-
     /**
      * Used for initial separation of command word and args.
      */
     private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
-    private static final Logger logger = LogsCenter.getLogger(InputParser.class);
 
     /**
      * Returns a parsed Command based on the specified user input.
@@ -53,19 +51,6 @@ public class InputParser {
 
         final @Nullable String commandWord = matcher.group("commandWord");
         final @Nullable String arguments = matcher.group("arguments");
-        /* NOTE
-         * Change the log level in your dev workspace's config.json file to
-         * enable the displaying of lower level log messages such as this one.
-         * Eg FINE, FINER or lower. Lower level logging is done sparingly to
-         * avoid bloating the codebase.
-        */
-        logger.fine(
-            String.format(
-                "BASIC_COMMAND_FORMAT successfully matched commandWord (%s) arguments (%s)",
-                commandWord,
-                arguments
-            )
-        );
 
         switch (commandWord) {
         case AddCommand.COMMAND_WORD:
@@ -85,14 +70,7 @@ public class InputParser {
         case ExitCommand.COMMAND_WORD:
             return new ExitCommand();
         default:
-            logger.finer(
-                String.format(
-                    "Throwing ParseException for userInput (%s)",
-                    userInput
-                )
-            );
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
     }
-
 }
