84d0c3692c2c37bc532e57bf8632abda2fd04e80
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-10-19 06:15:18 +0800

    ref(Logic): Draft initial simplification

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -7,44 +7,46 @@ import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.ContactList;
+import seedu.address.model.ReadOnlyContacts;
 import seedu.address.model.contact.Contact;
+
+
 
 /**
  * API of the Logic component.
  */
 public interface Logic {
     /**
-     * Executes the command and returns the result.
-     * @param commandText The command as entered by the user.
-     * @return the result of the command execution.
-     * @throws CommandException If an error occurs during command execution.
+     * Returns the {@link CommandResult} of executing the specified command
+     * text.
+     *
      * @throws ParseException If an error occurs during parsing.
+     * @throws CommandException If an error occurs during command execution.
      */
-    CommandResult execute(String commandText) throws CommandException, ParseException;
+    CommandResult execute(String commandText) throws ParseException, CommandException;
 
     /**
      * Returns the ContactList.
      *
-     * @see seedu.address.model.Model#getContactList()
+     * @see seedu.address.model.Model#getContacts()
      */
-    ContactList getContactList();
+    // ReadOnlyContacts getContactList();
 
     /** Returns an unmodifiable view of the filtered list of contacts */
-    ObservableList<Contact> getFilteredContactList();
+    // ObservableList<Contact> getFilteredContactList();
 
     /**
      * Returns the user prefs' address book file path.
      */
-    Path getConTextFilePath();
+    // Path getConTextFilePath();
 
     /**
      * Returns the user prefs' GUI settings.
      */
-    GuiSettings getGuiSettings();
+    // GuiSettings getGuiSettings();
 
     /**
      * Set the user prefs' GUI settings.
      */
-    void setGuiSettings(GuiSettings guiSettings);
+    // void setGuiSettings(GuiSettings guiSettings);
 }
