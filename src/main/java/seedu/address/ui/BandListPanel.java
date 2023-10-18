c1fe6ca4960593e0711b260c99e54341d0a3d036
hejin <hj827458@gmail.com>
2023-10-18 16:37:56 +0800

    Add fxml files for Band

---
+++ src/main/java/seedu/address/ui/BandListPanel.java
@@ -0,0 +1,50 @@
+package seedu.address.ui;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.band.Band;
+import seedu.address.model.musician.Musician;
+
+import java.util.logging.Logger;
+
+/**
+ * Panel containing the list of persons.
+ */
+public class BandListPanel extends UiPart<Region> {
+    private static final String FXML = "PersonListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(BandListPanel.class);
+
+    @FXML
+    private ListView<Band> bandListView;
+
+    /**
+     * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
+     */
+    public BandListPanel(ObservableList<Band> bandList) {
+        super(FXML);
+        bandListView.setItems(bandList);
+        bandListView.setCellFactory(listView -> new BandListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Musician} using a {@code PersonCard}.
+     */
+    class BandListViewCell extends ListCell<Band> {
+        @Override
+        protected void updateItem(Band band, boolean empty) {
+            super.updateItem(band, empty);
+
+            if (empty || band == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new BandCard(band, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
