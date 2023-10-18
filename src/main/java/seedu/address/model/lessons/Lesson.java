5259770bfd1409084bd07b57ce4a21de7f5b1869
elaineshijie <lohelaineshijie.183@gmail.com>
2023-10-18 20:53:59 +0800

    Add schedule class similar to UniquePersonList

--- src/main/java/seedu/address/model/lessons/Lesson.java
+++ src/main/java/seedu/address/model/lessons/Lesson.java
@@ -1,24 +1,30 @@
 package seedu.address.model.lessons;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.person.Subject;
 
 /**
- * Represents a student's lesson
+ * Represents a student's lesson in the schedule.
+ * Guarantees: details are present and not null, field values are validated, immutable.
  */
 public class Lesson {
+    // Lesson fields
     private LocalDateTime start;
     private LocalDateTime end;
+    // Data fields
     private Subject subject;
-
     private ArrayList<String> students;
 
     /**
-     * Constructor for a Lesson Object
+     * Constructor for a Lesson Object with one student.
      * Note: parse the string before giving it to the constructor.
      *
      * @param start The start time of the lesson
@@ -28,6 +34,7 @@ public class Lesson {
      * @see seedu.address.logic.parser.ParserUtil
      */
     public Lesson(LocalDateTime start, LocalDateTime end, Subject subject, String studentName) {
+        requireAllNonNull(start, end, subject, studentName);
         this.start = start;
         this.end = end;
         this.subject = subject;
@@ -48,6 +55,7 @@ public class Lesson {
      * @see seedu.address.logic.parser.ParserUtil
      */
     public Lesson(LocalDateTime start, LocalDateTime end, Subject subject, ArrayList<String> students) {
+        requireAllNonNull(start, end, subject, students);
         this.start = start;
         this.end = end;
         this.subject = subject;
@@ -57,6 +65,12 @@ public class Lesson {
     // TODO: Tasks
     // private TaskContainer tasks;
 
+    public LocalDateTime getStart() {
+        return start;
+    }
+    public LocalDateTime getEnd() {
+        return end;
+    }
     /**
      * Serializes the start date to a String
      * @return stringified version of start date
@@ -109,5 +123,50 @@ public class Lesson {
     public static ArrayList<String> deserializeStudents(String students) throws IllegalValueException {
         // comma delimited
         return new ArrayList<>(Arrays.asList(students.split(",")));
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Lesson)) {
+            return false;
+        }
+
+        Lesson otherLesson = (Lesson) other;
+        return start.equals(otherLesson.start)
+                && end.equals(otherLesson.end)
+                && subject.equals(otherLesson.subject)
+                && students.equals(otherLesson.students);
+
+    }
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(start, end, subject, students);
+    }
+    /**
+     * Returns true if both lessons have the same start and end time.
+     * This defines a weaker notion of equality between two lessons.
+     */
+    public boolean isSameLesson(Lesson otherLesson) {
+        if (otherLesson == this) {
+            return true;
+        }
+
+        return otherLesson != null
+                && otherLesson.getStart().equals(getStart())
+                && otherLesson.getEnd().equals(getEnd());
+    }
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("start", start)
+                .add("end", end)
+                .add("subject", subject)
+                .add("students", students)
+                .toString();
     }
 }
