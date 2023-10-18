330a29eb21e00eb8a83d508d0c47dfd34d8bb132
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-10-19 01:06:51 +0800

    feat(Messages): Replace address book, diff casing

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -28,8 +28,8 @@ public final class Messages {
 
     // Messages associated with various Commands
     public static final String MESSAGE_ADD_COMMAND_SUCCESS = "New contact added: %1$s";
-    public static final String MESSAGE_COMMAND_DUPLICATE_CONTACT = "This contact already exists in Context";
-    public static final String MESSAGE_CLEAR_COMMAND_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_COMMAND_DUPLICATE_CONTACT = "This contact is already in your contact list.";
+    public static final String MESSAGE_CLEAR_COMMAND_SUCCESS = "All contacts have been deleted!";
     public static final String MESSAGE_DELETE_COMMAND_SUCCESS = "Deleted Contact: %1$s";
     public static final String MESSAGE_EDIT_COMMAND_SUCCESS = "Edited Contact: %1$s";
     public static final String MESSAGE_EDIT_COMMAND_NOT_EDITED = "At least one field to edit must be provided.";
