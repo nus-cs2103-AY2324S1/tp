542d68544a8cb5a02334c9e672b6e708d5e59483
Zhetao <quzhetao2001@gmail.com>
2023-10-18 23:25:44 +0800

    Add test cases

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -1,14 +1,18 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.ViewCommandParser.APPOINTMENT_CATEGORY;
+import static seedu.address.logic.parser.ViewCommandParser.STUDENT_CATEGORY;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_APPOINTMENTS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists all appointments or students in WellNus to the user.
  */
 public class ViewCommand extends Command {
 
@@ -20,7 +24,10 @@ public class ViewCommand extends Command {
             + "g/ [DATA_CATEGORY]\n"
             + "Example: " + COMMAND_WORD + " g/appointments ";
 
-    public static final String MESSAGE_SUCCESS = "Listed all appointments";
+    public static final String MESSAGE_SUCCESS_APPOINTMENT = "Listed all appointments";
+
+    public static final String MESSAGE_SUCCESS_STUDENT = "Listed all students";
+
     public static final String MESSAGE_ARGUMENTS = "Data chosen: %1$s";
 
     private final String category;
@@ -32,9 +39,15 @@ public class ViewCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        model.updateFilteredAppointmentList(PREDICATE_SHOW_ALL_APPOINTMENTS);
-        return new CommandResult(MESSAGE_SUCCESS);
-        // throw new CommandException(String.format(MESSAGE_ARGUMENTS, category));
+        if (this.category.equals(STUDENT_CATEGORY)) {
+            model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+            return new CommandResult(MESSAGE_SUCCESS_STUDENT);
+        } else if (this.category.equals(APPOINTMENT_CATEGORY)) {
+            model.updateFilteredAppointmentList(PREDICATE_SHOW_ALL_APPOINTMENTS);
+            return new CommandResult(MESSAGE_SUCCESS_APPOINTMENT);
+        } else {
+            throw new CommandException(String.format(MESSAGE_ARGUMENTS, category));
+        }
     }
 
     @Override
@@ -49,5 +62,12 @@ public class ViewCommand extends Command {
 
         ViewCommand e = (ViewCommand) other;
         return category.equals(e.category);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("category", category)
+                .toString();
     }
 }
