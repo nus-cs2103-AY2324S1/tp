9b859e194997f99acc73f8a61b594f333f37112c
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-10-19 03:28:45 +0800

    ref(src): Refactor ContactList
    This aims to disambiguate from UniqueContactList,
    and other forms of filtered/observable contact lists.
    
    This was less of an issue when UniquePersonList
    was inside an AddressBook.
    
    Going back to the ReadOnly naming
    as the interfaces are designed to only have getters.

---
+++ src/main/java/seedu/address/model/ReadOnlyContacts.java
@@ -0,0 +1,16 @@
+package seedu.address.model;
+
+import javafx.collections.ObservableList;
+import seedu.address.model.contact.Contact;
+
+
+
+/**
+ * Read-only view of all {@link Contact}s.
+ */
+public interface ReadOnlyContacts {
+    /**
+     * Returns an unmodifiable {@link ObservableList} of all {@link Contact}s.
+     */
+    public ObservableList<Contact> getContactList();
+}
