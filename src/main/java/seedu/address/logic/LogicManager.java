c53c4851a089987a604c43404061fa51d71eb354
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-10-19 06:24:19 +0800

    ref(LogicManager): Draft initial simplification

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -3,81 +3,88 @@ package seedu.address.logic;
 import java.io.IOException;
 import java.nio.file.AccessDeniedException;
 import java.nio.file.Path;
-import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.InputParser;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.ContactList;
 import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyContacts;
 import seedu.address.model.contact.Contact;
 import seedu.address.storage.Storage;
 
+
+
 /**
- * The main LogicManager of the app.
+ * Implementation of the Logic component.
  */
 public class LogicManager implements Logic {
-    private final Logger logger = LogsCenter.getLogger(LogicManager.class);
+    private Model model;
+    private Storage storage;
 
-    private final Model model;
-    private final Storage storage;
-    private final InputParser inputParser;
+    //TODO can the entire parser be static?
+    private InputParser inputParser = new InputParser();
 
-    /**
-     * Constructs a {@code LogicManager} with the given {@code Model} and {@code Storage}.
-     */
     public LogicManager(Model model, Storage storage) {
         this.model = model;
         this.storage = storage;
-        inputParser = new InputParser();
     }
 
     @Override
-    public CommandResult execute(String commandText) throws CommandException, ParseException {
-        logger.info("----------------[USER COMMAND][" + commandText + "]");
-
-        CommandResult commandResult;
+    public CommandResult execute(
+        String commandText
+    ) throws ParseException, CommandException {
         Command command = inputParser.parseCommand(commandText);
-        commandResult = command.execute(model);
+        CommandResult result = command.execute(model);
 
         try {
-            storage.saveContactsManager(model.getContactList());
+            storage.saveContacts(model.getContacts());
         } catch (AccessDeniedException e) {
-            throw new CommandException(String.format(Messages.FILE_OPS_PERMISSION_ERROR_FORMAT, e.getMessage()), e);
-        } catch (IOException ioe) {
-            throw new CommandException(String.format(Messages.FILE_OPS_ERROR_FORMAT, ioe.getMessage()), ioe);
+            throw new CommandException(
+                String.format(
+                    Messages.FILE_OPS_PERMISSION_ERROR_FORMAT,
+                    e.getMessage()
+                ),
+                e
+            );
+        } catch (IOException e) {
+            throw new CommandException(
+                String.format(
+                    Messages.FILE_OPS_ERROR_FORMAT,
+                    e.getMessage()
+                ),
+                e
+            );
         }
 
-        return commandResult;
+        return result;
     }
 
-    @Override
-    public ContactList getContactList() {
-        return model.getContactList();
-    }
+    // @Override
+    // public ReadOnlyContacts getContactList() {
+    //     return model.getContacts();
+    // }
 
-    @Override
-    public ObservableList<Contact> getFilteredContactList() {
-        return model.getFilteredContactList();
-    }
+    // @Override
+    // public ObservableList<Contact> getFilteredContactList() {
+    //     return model.getFilteredContactList();
+    // }
 
-    @Override
-    public Path getConTextFilePath() {
-        return model.getConTextFilePath();
-    }
+    // @Override
+    // public Path getConTextFilePath() {
+    //     return model.getContactsPath();
+    // }
 
-    @Override
-    public GuiSettings getGuiSettings() {
-        return model.getGuiSettings();
-    }
+    // @Override
+    // public GuiSettings getGuiSettings() {
+    //     return model.getGuiSettings();
+    // }
 
-    @Override
-    public void setGuiSettings(GuiSettings guiSettings) {
-        model.setGuiSettings(guiSettings);
-    }
+    // @Override
+    // public void setGuiSettings(GuiSettings guiSettings) {
+    //     model.setGuiSettings(guiSettings);
+    // }
 }
