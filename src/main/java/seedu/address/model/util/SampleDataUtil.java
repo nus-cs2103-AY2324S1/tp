7c9bfe2299fb91bb626006d27906289cc08bf2e3
LuoZYi <luozhongyi2002@gmail.com>
2023-10-18 10:09:18 +0800

    update checkstyle

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -7,10 +7,15 @@ import java.util.stream.Collectors;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.*;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.FreeTime;
+import seedu.address.model.person.Hour;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Telegram;
 import seedu.address.model.tag.Mod;
 import seedu.address.model.tag.Tag;
-
 /**
  * Contains utility methods for populating {@code AddressBook} with sample data.
  * @formatter:off
