c97905a2eb738da3e99338c810237b6c3aaafdc0
JizhuoChen <88676760+JizhuoChen@users.noreply.github.com>
2023-10-18 11:48:17 +0800

    Make the code compilable

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javafx.collections.ObservableList;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Team;
 import seedu.address.model.person.UniquePersonList;
 
 /**
@@ -104,6 +105,11 @@ public class AddressBook implements ReadOnlyAddressBook {
     @Override
     public ObservableList<Person> getPersonList() {
         return persons.asUnmodifiableObservableList();
+    }
+
+    @Override
+    public ObservableList<Team> getTeamList() {
+        return null;
     }
 
     @Override
