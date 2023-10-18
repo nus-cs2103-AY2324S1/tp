25ef4607ad6d89d63d70f8a33367ca6625683228
Tan Yiming <tanyiming.nus@gmail.com>
2023-10-18 18:19:24 +0800

    Add tests for Instrument, Genre, JsonAdaptedMusician classes

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -28,6 +28,7 @@ public class EditCommandParser implements Parser<EditCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the EditCommand
      * and returns an EditCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public EditCommand parse(String args) throws ParseException {
@@ -57,9 +58,12 @@ public class EditCommandParser implements Parser<EditCommand> {
         if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
             editMusicianDescriptor.setEmail(ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get()));
         }
-        parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editMusicianDescriptor::setTags);
-        parseInstrumentsForEdit(argMultimap.getAllValues(PREFIX_INSTRUMENT)).ifPresent(editMusicianDescriptor::setInstruments);
-        parseGenresForEdit(argMultimap.getAllValues(PREFIX_GENRE)).ifPresent(editMusicianDescriptor::setGenres);
+        parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG))
+                .ifPresent(editMusicianDescriptor::setTags);
+        parseInstrumentsForEdit(argMultimap.getAllValues(PREFIX_INSTRUMENT))
+                .ifPresent(editMusicianDescriptor::setInstruments);
+        parseGenresForEdit(argMultimap.getAllValues(PREFIX_GENRE))
+                .ifPresent(editMusicianDescriptor::setGenres);
 
         if (!editMusicianDescriptor.isAnyFieldEdited()) {
             throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
