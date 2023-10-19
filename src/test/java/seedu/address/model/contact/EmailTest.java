3f60bbde4038e52685115d9f9f675c3c9f45099c
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-10-20 00:27:15 +0800

    fix(EmailTest): No null, no assert, require dot

--- src/test/java/seedu/address/model/contact/EmailTest.java
+++ src/test/java/seedu/address/model/contact/EmailTest.java
@@ -2,28 +2,12 @@ package seedu.address.model.contact;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class EmailTest {
-
     @Test
-    public void constructor_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new Email(null));
-    }
-
-    @Test
-    public void constructor_invalidEmail_throwsIllegalArgumentException() {
-        String invalidEmail = "";
-        assertThrows(IllegalArgumentException.class, () -> new Email(invalidEmail));
-    }
-
-    @Test
-    public void isValidEmail() {
-        // null email
-        assertThrows(NullPointerException.class, () -> Email.isValid(null));
-
+    public void isValid() {
         // blank email
         assertFalse(Email.isValid("")); // empty string
         assertFalse(Email.isValid(" ")); // spaces only
@@ -57,32 +41,9 @@ public class EmailTest {
         assertTrue(Email.isValid("PeterJack.1190@example.com")); // period in local part
         assertTrue(Email.isValid("PeterJack+1190@example.com")); // '+' symbol in local part
         assertTrue(Email.isValid("PeterJack-1190@example.com")); // hyphen in local part
-        assertTrue(Email.isValid("a@bc")); // minimal
-        assertTrue(Email.isValid("test@localhost")); // alphabets only
-        assertTrue(Email.isValid("123@145")); // numeric local part and domain name
         assertTrue(Email.isValid("a1+be.d@example1.com")); // mixture of alphanumeric and special characters
         assertTrue(Email.isValid("peter_jack@very-very-very-long-example.com")); // long domain name
         assertTrue(Email.isValid("if.you.dream.it_you.can.do.it@example.com")); // long local part
         assertTrue(Email.isValid("e1234567@u.nus.edu")); // more than one period in domain
-    }
-
-    @Test
-    public void equals() {
-        Email email = new Email("valid@email");
-
-        // same values -> returns true
-        assertTrue(email.equals(new Email("valid@email")));
-
-        // same object -> returns true
-        assertTrue(email.equals(email));
-
-        // null -> returns false
-        assertFalse(email.equals(null));
-
-        // different types -> returns false
-        assertFalse(email.equals(5.0f));
-
-        // different values -> returns false
-        assertFalse(email.equals(new Email("other.valid@email")));
     }
 }
