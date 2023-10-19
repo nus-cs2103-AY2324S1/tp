e40a4b21b22113d0d8bdb4f380bb2c8881dd7d38
Nicholas <nicholaschiazhijie2001@yahoo.com>
2023-10-20 01:43:45 +0800

    Remove unused imports

--- src/main/java/seedu/address/logic/parser/ScheduleCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ScheduleCommandParser.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT_DATE;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ScheduleCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.appointment.Appointment;
