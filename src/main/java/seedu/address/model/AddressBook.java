207a1b34c7f8a7f9b2d90ec979aff81e90b3f478
sk2001git <122198303+sk2001git@users.noreply.github.com>
2023-10-19 21:55:02 +0800

    Checkstyle fix
    for ci

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -2,7 +2,6 @@ package seedu.address.model;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.Comparator;
 import java.util.List;
 
 import javafx.collections.ObservableList;
@@ -97,12 +96,6 @@ public class AddressBook implements ReadOnlyAddressBook {
 
 
     //// util methods
-
-    public void sortAddressBook(Comparator<Person> comparator) {
-        ObservableList<Person> personList = getPersonList();
-        List<Person> sortedList = personList.sorted(comparator);
-        setPersons(sortedList);
-    }
 
     @Override
     public String toString() {
