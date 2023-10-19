8c335d0ca1ee100d2ea6ec793c747a8c1a55ddae
A1WAYSD <yujl2004@163.com>
2023-10-19 17:32:33 +0800

    Add tests for storage

---
+++ src/test/java/seedu/flashlingo/testutil/TypicalFlashCards.java
@@ -0,0 +1,35 @@
+package seedu.flashlingo.testutil;
+
+import seedu.flashlingo.model.Flashlingo;
+import seedu.flashlingo.model.flashcard.FlashCard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A utility class containing a list of {@code FlashCard} objects to be used in tests.
+ */
+public class TypicalFlashCards {
+    public static final FlashCard WORD = new FlashCardBuilder().withOriginalWord("你好", "Chinese").
+            withTranslatedWord("hi","English").withLevel(2).
+            withWhenToReview("2023-12-12T23:59:59Z").build();
+
+    private TypicalFlashCards() {} // prevents instantiation
+
+    /**
+     * Returns an {@code Flashlingo} with all the typical flash cards.
+     */
+    public static Flashlingo getTypicalFlashlingo() {
+        Flashlingo fl = new Flashlingo();
+        for (FlashCard flashCard : getTypicalFlashCards()) {
+            fl.addFlashCard(flashCard);
+        }
+        return fl;
+    }
+
+    public static List<FlashCard> getTypicalFlashCards() {
+        return new ArrayList<>(Arrays.asList(WORD));
+    }
+
+}
