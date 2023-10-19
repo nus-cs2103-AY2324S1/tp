a30e59da7c1ed1cdd07ea3cca94a26521c2d11f6
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-10-19 09:38:50 +0800

    feat(src): Simplify email help text, combine regex

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -44,18 +44,6 @@ public final class Messages {
             "Names should only contain alphanumeric characters and spaces, and it should not be blank";
     public static final String MESSAGE_PHONE_CONSTRAINTS =
             "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String MESSAGE_EMAIL_CONSTRAINTS = "Emails should be of the format local-part@domain "
-            + "and adhere to the following constraints:\n"
-            + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
-            + "the parentheses, (" + Email.SPECIAL_CHARACTERS
-            + "). The local-part may not start or end with any special "
-            + "characters.\n"
-            + "2. This is followed by a '@' and then a domain name. The domain name is made up of domain labels "
-            + "separated by periods.\n"
-            + "The domain name must:\n"
-            + "    - end with a domain label at least 2 characters long\n"
-            + "    - have each domain label start and end with alphanumeric characters\n"
-            + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.";
 
     // Messages associated with Storage
     public static final String MESSAGE_FIELD_MISSING = "Contact's %s field is missing.";
@@ -72,8 +60,12 @@ public final class Messages {
     public static final String COMMAND_CLEAR_SUCCESS = "All contacts have been removed!";
     public static final String COMMAND_EXIT_SUCCESS = "Exiting app...";
 
-    // Tag
-    private static final String UNFORMATTED_TAG_INVALID = "\"%s\" is not a valid tag. Tags must be alphanumeric (spaces allowed).";
+    // Contacts
+    private static final String UNFORMATTED_TAG_INVALID =
+            "\"%s\" is not a valid tag. Tags must be alphanumeric (spaces allowed).";
+
+    public static final String EMAIL_INVALID =
+            "Emails must roughly be of the form \"example_email@foo-domain.sg.\"";
 
     /**
      * Returns a formatted message about the command format being invalid, with
