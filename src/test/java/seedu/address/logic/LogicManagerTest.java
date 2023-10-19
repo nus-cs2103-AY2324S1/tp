accc452aaa3f965d811c93677fb0727fd49fd1d7
LuoZYi <luozhongyi2002@gmail.com>
2023-10-19 10:12:02 +0800

    Fix logic manager test

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
 import static seedu.address.logic.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.HOUR_DESC_FIVE;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.PHONE_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.TELEGRAM_DESC_AMY;
@@ -166,7 +167,7 @@ public class LogicManagerTest {
 
         // Triggers the saveAddressBook method by executing an add command
         String addCommand = AddCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY
-                + EMAIL_DESC_AMY + TELEGRAM_DESC_AMY;
+                + EMAIL_DESC_AMY + TELEGRAM_DESC_AMY + HOUR_DESC_FIVE;
         Person expectedPerson = new PersonBuilder(AMY).withTags().withMods().build();
         ModelManager expectedModel = new ModelManager();
         expectedModel.addPerson(expectedPerson);
