030e79d1dbef33328ee52d92db085303834cc0a2
scarletblanks <irisu.nita@gmail.com>
2023-10-19 21:13:55 +0800

    For CodeCov Checks

---
+++ src/test/java/seedu/address/model/person/StatusTest.java
@@ -0,0 +1,16 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class StatusTest {
+    
+
+    @Test
+    public void toStringMethod() {
+        assertEquals(new Status().toString(), "Preliminary");
+    }
+}
