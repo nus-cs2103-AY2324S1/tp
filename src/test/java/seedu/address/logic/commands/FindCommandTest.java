0f026d7e2c6ee969a36d63429602a8a90fc49fa5
Tan Yiming <tanyiming.nus@gmail.com>
2023-10-19 12:41:56 +0800

    Add tests for FindCommand

--- src/test/java/seedu/address/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.Messages.MESSAGE_MUSICIANS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalMusicians.BENSON;
 import static seedu.address.testutil.TypicalMusicians.CARL;
 import static seedu.address.testutil.TypicalMusicians.ELLE;
 import static seedu.address.testutil.TypicalMusicians.FIONA;
@@ -22,6 +23,7 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.musician.Musician;
 import seedu.address.model.musician.NameContainsKeywordsPredicate;
+import seedu.address.model.tag.TagMatchesPredicate;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code FindCommand}.
@@ -43,15 +45,16 @@ public class FindCommandTest {
         FindCommand findFirstCommand = new FindCommand(firstPredicates);
         FindCommand findSecondCommand = new FindCommand(secondPredicates);
 
-        NameContainsKeywordsPredicate firstPredicateCopy =
-                new NameContainsKeywordsPredicate(Collections.singletonList("first"));
-        HashSet<Predicate<Musician>> firstPredicatesCopy = new HashSet<>(Arrays.asList(firstPredicateCopy));
-
         // same object -> returns true
         assertTrue(findFirstCommand.equals(findFirstCommand));
 
         // same values -> returns true
-        FindCommand findFirstCommandCopy = new FindCommand(firstPredicatesCopy);
+        NameContainsKeywordsPredicate firstPredicateCopy =
+                new NameContainsKeywordsPredicate(Collections.singletonList("first"));
+        HashSet<Predicate<Musician>> firstPredicatesCopy = new HashSet<>(Arrays.asList(firstPredicateCopy));
+        FindCommand findFirstCommandDeepCopy = new FindCommand(firstPredicatesCopy);
+        FindCommand findFirstCommandCopy = new FindCommand(firstPredicates);
+        assertTrue(findFirstCommand.equals(findFirstCommandDeepCopy));
         assertTrue(findFirstCommand.equals(findFirstCommandCopy));
 
         // different types -> returns false
@@ -67,7 +70,7 @@ public class FindCommandTest {
     @Test
     public void execute_zeroKeywords_noMusicianFound() {
         String expectedMessage = String.format(MESSAGE_MUSICIANS_LISTED_OVERVIEW, 0);
-        NameContainsKeywordsPredicate predicate = preparePredicate(" ");
+        NameContainsKeywordsPredicate predicate = prepareNameKeywordsPredicate(" ");
         HashSet<Predicate<Musician>> predicates = new HashSet<>(Arrays.asList(predicate));
         FindCommand command = new FindCommand(predicates);
         expectedModel.updateFilteredMusicianList(predicate);
@@ -76,14 +79,41 @@ public class FindCommandTest {
     }
 
     @Test
-    public void execute_multipleKeywords_multipleMusiciansFound() {
+    public void execute_multipleNameKeywords_multipleMusiciansFound() {
         String expectedMessage = String.format(MESSAGE_MUSICIANS_LISTED_OVERVIEW, 3);
-        NameContainsKeywordsPredicate predicate = preparePredicate("Kurz Elle Kunz");
+        NameContainsKeywordsPredicate predicate = prepareNameKeywordsPredicate("Kurz Elle Kunz");
+
         HashSet<Predicate<Musician>> predicates = new HashSet<>(Arrays.asList(predicate));
         FindCommand command = new FindCommand(predicates);
         expectedModel.updateFilteredMusicianList(predicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
         assertEquals(Arrays.asList(CARL, ELLE, FIONA), model.getFilteredMusicianList());
+    }
+
+    @Test
+    public void execute_mixedPredicates_singleMusicianFound() {
+        String expectedMessage = String.format(MESSAGE_MUSICIANS_LISTED_OVERVIEW, 1);
+        NameContainsKeywordsPredicate namePredicate = prepareNameKeywordsPredicate("Benson");
+        TagMatchesPredicate tagPredicate = prepareTagMatchesPredicate("friends");
+
+        HashSet<Predicate<Musician>> predicates = new HashSet<>(Arrays.asList(namePredicate, tagPredicate));
+        FindCommand command = new FindCommand(predicates);
+        expectedModel.updateFilteredMusicianList(namePredicate.and(tagPredicate));
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(BENSON), model.getFilteredMusicianList());
+    }
+
+    @Test
+    public void execute_mixedPredicates_noMusicianFound() {
+        String expectedMessage = String.format(MESSAGE_MUSICIANS_LISTED_OVERVIEW, 0);
+        NameContainsKeywordsPredicate namePredicate = prepareNameKeywordsPredicate("Kunz");
+        TagMatchesPredicate tagPredicate = prepareTagMatchesPredicate("friends");
+
+        HashSet<Predicate<Musician>> predicates = new HashSet<>(Arrays.asList(namePredicate, tagPredicate));
+        FindCommand command = new FindCommand(predicates);
+        expectedModel.updateFilteredMusicianList(namePredicate.and(tagPredicate));
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredMusicianList());
     }
 
     @Test
@@ -96,9 +126,18 @@ public class FindCommandTest {
     }
 
     /**
-     * Parses {@code userInput} into a {@code NameContainsKeywordsPredicate}.
+     * Parses {@code userInput} into a {@code NameContainsKeywordsPredicate}. This parsing method is used
+     * for the convenience of testing.
      */
-    private NameContainsKeywordsPredicate preparePredicate(String userInput) {
+    private NameContainsKeywordsPredicate prepareNameKeywordsPredicate(String userInput) {
         return new NameContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code TagMatchesPredicate}. This parsing method is used for the
+     * convenience of testing.
+     */
+    private TagMatchesPredicate prepareTagMatchesPredicate(String userInput) {
+        return new TagMatchesPredicate(Arrays.asList(userInput.split("\\s+")));
     }
 }
