4934946e3173b7e853dd11dd690d5192168f9a43
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-10-19 19:49:06 +0800

    dev(AppUtilTest): Update names, remove null test

--- src/test/java/seedu/address/commons/util/AppUtilTest.java
+++ src/test/java/seedu/address/commons/util/AppUtilTest.java
@@ -11,13 +11,8 @@ import seedu.address.ui.UiManager;
 
 public class AppUtilTest {
     @Test
-    public void getImage_appIcon_notNull() {
+    public void getImage_appIcon_imageReturned() {
         assertNotNull(AppUtil.getImage(UiManager.PATH_APP_ICON));
-    }
-
-    @Test
-    public void getImage_nullGiven_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> AppUtil.getImage(null));
     }
 
     @Test
@@ -27,12 +22,12 @@ public class AppUtilTest {
     }
 
     @Test
-    public void checkArgument_falseWithoutErrorMessage_throwsIllegalArgumentException() {
+    public void checkArgument_falseWithoutErrorMessage_exceptionThrown() {
         assertThrows(IllegalArgumentException.class, () -> AppUtil.checkArgument(false));
     }
 
     @Test
-    public void checkArgument_falseWithErrorMessage_throwsIllegalArgumentException() {
+    public void checkArgument_falseWithErrorMessage_exceptionThrown() {
         String errorMessage = "error message";
         assertThrows(IllegalArgumentException.class, errorMessage, () -> AppUtil.checkArgument(false, errorMessage));
     }
