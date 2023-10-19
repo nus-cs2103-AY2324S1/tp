08c88b8d7b11720815482e432b1e6316dd9e9991
Jun Hong <junhongloww@gmail.com>
2023-10-19 14:17:51 +0800

    Add test util classes for event population and calendar building

---
+++ src/test/java/seedu/address/testutil/CalendarBuilder.java
@@ -0,0 +1,37 @@
+package seedu.address.testutil;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.calendar.Calendar;
+import seedu.address.model.event.Event;
+import seedu.address.model.person.Person;
+
+/**
+ * A utility class to help with building Calendar objects.
+ * Example usage: <br>
+ *     {@code Calendar c = new CalendarBuilder().withEvent("Description", "2000-01-01 00:00", "2000-01-01 12:00")
+ *     .build();}
+ */
+public class CalendarBuilder {
+
+    private Calendar calendar;
+
+    public CalendarBuilder() {
+        calendar = new Calendar();
+    }
+
+    public CalendarBuilder(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
+    /**
+     * Adds a new {@code Person} to the {@code AddressBook} that we are building.
+     */
+    public CalendarBuilder withEvent(Event event) {
+        calendar.addEvent(event);
+        return this;
+    }
+
+    public Calendar build() {
+        return calendar;
+    }
+}
