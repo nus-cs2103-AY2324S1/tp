20645897e4c5ff7facce40ea3e0a7575c496a52e
jonasongg <jonasong2@gmail.com>
2023-10-20 19:32:51 +0800

    Finish ClearCommandTest

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -1,11 +1,14 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBookWithMembersApplicants;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 public class ClearCommandTest {
 
@@ -19,12 +22,11 @@ public class ClearCommandTest {
 
     @Test
     public void execute_nonEmptyAddressBook_success() {
-        // TODO: implement after implementing member
-        //        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        //        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        //        expectedModel.setAddressBook(new AddressBook());
-        //
-        //        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+        Model model = new ModelManager(getTypicalAddressBookWithMembersApplicants(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalAddressBookWithMembersApplicants(), new UserPrefs());
+        expectedModel.setAddressBook(new AddressBook());
+
+        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
 }
