dbed4a8294e110d1e2b832b8581c5a44df7f8ef2
Yu Han Zhang <yuhan.zhang@u.nus.edu>
2023-10-20 11:44:00 +0800

    Add some basic tests

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -47,5 +48,27 @@ public class ViewCommand extends Command {
 
         model.getSelectedPerson().setValue(personToView);
         return new CommandResult(String.format(MESSAGE_VIEW_CLIENT_SUCCESS, Messages.format(personToView)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ViewCommand)) {
+            return false;
+        }
+
+        ViewCommand otherViewCommand = (ViewCommand) other;
+        return index.equals(otherViewCommand.index);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("index", index)
+                .toString();
     }
 }
