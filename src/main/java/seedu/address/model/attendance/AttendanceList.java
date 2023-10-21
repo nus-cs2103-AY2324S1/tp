a260da54943a2d9a13c308f16fa5d0355fe49267
Andre Chua <110965035+andrechuakj@users.noreply.github.com>
2023-10-22 03:45:34 +0800

    Create AttendanceList class

--- src/main/java/seedu/address/model/attendance/AttendanceList.java
+++ src/main/java/seedu/address/model/attendance/AttendanceList.java
@@ -1,6 +1,42 @@
 package seedu.address.model.attendance;
 
+import seedu.address.model.event.Event;
+import seedu.address.model.member.Member;
+
 import java.util.ArrayList;
 
 public class AttendanceList extends ArrayList<Attendance> {
+    public void addAttendance(Attendance attendance) {
+        this.add(attendance);
+    }
+
+    public void removeAttendance(Attendance attendance) {
+        this.remove(attendance);
+    }
+
+    public ArrayList<Attendance> getFilteredListByMember(Member member) {
+        ArrayList<Attendance> filteredList = new ArrayList<>();
+
+        for (int i = 0; i < this.size(); i++) {
+            Attendance attendance = this.get(i);
+            if (attendance.getMemberName().equals(member.getName())) {
+                filteredList.add(attendance);
+            }
+        }
+
+        return filteredList;
+    }
+
+    public ArrayList<Attendance> getFilteredListByEvent(Event event) {
+        ArrayList<Attendance> filteredList = new ArrayList<>();
+
+        for (int i = 0; i < this.size(); i++) {
+            Attendance attendance = this.get(i);
+            if (attendance.getEventName().equals(event.getName())) {
+                filteredList.add(attendance);
+            }
+        }
+
+        return filteredList;
+    }
 }
