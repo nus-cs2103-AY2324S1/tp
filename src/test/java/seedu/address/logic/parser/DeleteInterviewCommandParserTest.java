27dd4c9ee175f7b2a4b1b4e6b269b80fd6b03436
JasonLCY-Temp <e0407197@u.nus.edu>
2023-10-22 01:52:18 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/parser/DeleteInterviewCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteInterviewCommandParserTest.java
@@ -1,12 +1,13 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.DeleteInterviewCommand;
-
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.DeleteInterviewCommand;
 
 /**
  * Adapted from AB3 DeleteCommandParserTest class
