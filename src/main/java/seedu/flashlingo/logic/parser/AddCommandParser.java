81866ba88883e3312edea39ec6c1fd342a7bdffa
Song-Mengfei <e1075530@u.nus.edu>
2023-10-21 22:00:25 +0800

    Fix JAVA CI issue

--- src/main/java/seedu/flashlingo/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/flashlingo/logic/parser/AddCommandParser.java
@@ -1,7 +1,7 @@
 package seedu.flashlingo.logic.parser;
 
-import static seedu.flashlingo.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.flashlingo.logic.Messages.MESSAGE_EMPTY_VALUE;
+import static seedu.flashlingo.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.flashlingo.logic.parser.CliSyntax.PREFIX_ORIGINAL_WORD;
 import static seedu.flashlingo.logic.parser.CliSyntax.PREFIX_ORIGINAL_WORD_LANGUAGE;
 import static seedu.flashlingo.logic.parser.CliSyntax.PREFIX_TRANSLATED_WORD;
