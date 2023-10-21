49a7070594e6f56344929da6a4382787bb35dddb
Delton Cheng <chengxuzhi@gmail.com>
2023-10-22 01:02:42 +0800

    Update tests due to 1-indexing and changes to AddMusiciantoBandCommand
    Add get(int index) functions for UniqueBandList and UniqueMusicianList needed for the tests

--- src/test/java/seedu/address/logic/commands/AddMusiciantoBandCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddMusiciantoBandCommandTest.java
@@ -5,13 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import seedu.address.commons.core.GuiSettings;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
@@ -19,7 +20,9 @@ import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.band.Band;
+import seedu.address.model.band.UniqueBandList;
 import seedu.address.model.musician.Musician;
+import seedu.address.model.musician.UniqueMusicianList;
 import seedu.address.testutil.BandBuilder;
 import seedu.address.testutil.MusicianBuilder;
 
@@ -27,11 +30,13 @@ public class AddMusiciantoBandCommandTest {
 
     @Test
     public void constructor_nullMusician_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new AddMusiciantoBandCommand(1, null));
+        Index bandIndex = Index.fromOneBased(1);
+        assertThrows(NullPointerException.class, () -> new AddMusiciantoBandCommand(bandIndex, null));
     }
     @Test
     public void constructor_nullBand_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new AddMusiciantoBandCommand(null, 1));
+        Index musicianIndex = Index.fromOneBased(1);
+        assertThrows(NullPointerException.class, () -> new AddMusiciantoBandCommand(null, musicianIndex));
     }
     @Test
     public void constructor_nullBandAndMusician_throwsNullPointerException() {
@@ -46,12 +51,16 @@ public class AddMusiciantoBandCommandTest {
         Band validBand = new BandBuilder().build();
         CommandResult addCommandResult = new AddCommand(validMusician).execute(modelBandStub);
         CommandResult addBandCommandResult = new AddBandCommand(validBand).execute(modelBandStub);
-        CommandResult commandResult = new AddMusiciantoBandCommand(0, 0).execute(modelBandStub);
+        Index bandIndex = Index.fromOneBased(1);
+        Index musicianIndex = Index.fromOneBased(1);
+        CommandResult commandResult = new AddMusiciantoBandCommand(bandIndex, musicianIndex).execute(modelBandStub);
 
-        assertEquals(String.format(AddMusiciantoBandCommand.MESSAGE_SUCCESS, Messages.format(0, 0)),
+        assertEquals(String.format(AddMusiciantoBandCommand.MESSAGE_SUCCESS,
+                Messages.format(modelBandStub.bandsAdded.get(bandIndex),
+                        modelBandStub.musiciansAdded.get(musicianIndex))),
                 commandResult.getFeedbackToUser());
-        assertEquals(validBand, modelBandStub.bandsAdded.get(0));
-        assertEquals(validBand.getMusicians(), modelBandStub.bandsAdded.get(0).getMusicians());
+        assertEquals(validBand, modelBandStub.bandsAdded.get(bandIndex));
+        assertEquals(validBand.getMusicians(), modelBandStub.bandsAdded.get(bandIndex).getMusicians());
     }
     @Test
     public void execute_duplicateMusicianAddedtoBand_throwsCommandException() throws Exception {
@@ -62,11 +71,14 @@ public class AddMusiciantoBandCommandTest {
         Band validBand = new BandBuilder().build();
         CommandResult addCommandResult = new AddCommand(validMusician).execute(modelBandStub);
         CommandResult addBandCommandResult = new AddBandCommand(validBand).execute(modelBandStub);
-        CommandResult firstAddMusiciancommandResult = new AddMusiciantoBandCommand(0, 0).execute(modelBandStub);
+        Index bandIndex = Index.fromOneBased(1);
+        Index musicianIndex = Index.fromOneBased(1);
+        CommandResult firstAddMusiciancommandResult =
+                new AddMusiciantoBandCommand(bandIndex, musicianIndex).execute(modelBandStub);
 
         assertThrows(CommandException.class,
                 AddMusiciantoBandCommand.MESSAGE_DUPLICATE_MUSICIAN, () ->
-                        new AddMusiciantoBandCommand(0, 0).execute(modelBandStub));
+                        new AddMusiciantoBandCommand(bandIndex, musicianIndex).execute(modelBandStub));
     }
     /**
      * A default model stub that have all of the methods failing.
@@ -186,19 +198,23 @@ public class AddMusiciantoBandCommandTest {
      * A Model stub that always accept the musician being added into the band.
      */
     private class ModelStubAcceptingMusicianAddedToBand extends AddMusiciantoBandCommandTest.ModelStub {
-        final ArrayList<Band> bandsAdded = new ArrayList<>();
-        final ArrayList<Musician> musiciansAdded = new ArrayList<>();
+        final UniqueBandList bandsAdded = new UniqueBandList();
+        final UniqueMusicianList musiciansAdded = new UniqueMusicianList();
         @Override
         public boolean hasMusicianInBand(int bandToAddInto, int musicianToAdd) {
             requireNonNull(bandToAddInto);
             requireNonNull(musicianToAdd);
-            return bandsAdded.get(bandToAddInto)
-                    .hasMusician(musiciansAdded.get(musicianToAdd));
+            Index bandIndex = Index.fromZeroBased(bandToAddInto);
+            Index musicianIndex = Index.fromZeroBased(musicianToAdd);
+            return bandsAdded.get(bandIndex)
+                    .hasMusician(musiciansAdded.get(musicianIndex));
         }
         @Override
         public void addMusicianToBand(int bandToAddInto, int musicianToAdd) {
-            bandsAdded.get(bandToAddInto).getModifiableMusicianList()
-                    .add(musiciansAdded.get(musicianToAdd));
+            Index bandIndex = Index.fromZeroBased(bandToAddInto);
+            Index musicianIndex = Index.fromZeroBased(musicianToAdd);
+            bandsAdded.get(bandIndex).getModifiableMusicianList()
+                    .add(musiciansAdded.get(musicianIndex));
         }
         @Override
         public void addMusician(Musician musician) {
@@ -219,6 +235,14 @@ public class AddMusiciantoBandCommandTest {
         public boolean hasMusician(Musician musician) {
             requireNonNull(musician);
             return musiciansAdded.contains(musician);
+        }
+        @Override
+        public ObservableList<Band> getFilteredBandList() {
+            return new FilteredList<>(bandsAdded.asUnmodifiableObservableList());
+        }
+        @Override
+        public ObservableList<Musician> getFilteredMusicianList() {
+            return new FilteredList<>(musiciansAdded.asUnmodifiableObservableList());
         }
         @Override
         public ReadOnlyAddressBook getAddressBook() {
