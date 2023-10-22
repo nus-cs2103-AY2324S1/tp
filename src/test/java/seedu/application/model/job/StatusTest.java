987ca5f78a9ad1e2c067d143cd888fbbe0474d7d
peiran18 <e0968798@u.nus.edu>
2023-10-22 20:23:08 +0800

    Add StatusTest to test Status class

---
+++ src/test/java/seedu/application/model/job/StatusTest.java
@@ -0,0 +1,60 @@
+package seedu.application.model.job;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.application.model.job.Status.IN_PROGRESS;
+import static seedu.application.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class StatusTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Status(null));
+    }
+
+    @Test
+    public void constructor_invalidStatus_throwsIllegalArgumentException() {
+        String invalidStatus = "";
+        assertThrows(IllegalArgumentException.class, () -> new Status(invalidStatus));
+    }
+
+    @Test
+    public void isValidStatus() {
+        // null status
+        assertThrows(NullPointerException.class, () -> Status.isValidStatus(null));
+
+        // invalid status
+        assertFalse(Status.isValidStatus("")); // empty string
+        assertFalse(Status.isValidStatus(" ")); // spaces only
+        assertFalse(Status.isValidStatus("^")); // only non-alphanumeric characters
+        assertFalse(Status.isValidStatus("peter")); // does not match anything in enum JobStatus
+
+        // valid status
+        assertTrue(Status.isValidStatus("to_be_submitted")); // all lowercase
+        assertTrue(Status.isValidStatus("Pending")); // first letter capitalised
+        assertTrue(Status.isValidStatus("appROVed")); // some lower case, some upper case
+        assertTrue(Status.isValidStatus("REJECTED")); // all uppercase
+    }
+
+    @Test
+    public void equals() {
+        Status status = new Status(IN_PROGRESS);
+
+        // same values -> returns true
+        assertTrue(status.equals(new Status(IN_PROGRESS)));
+
+        // same object -> returns true
+        assertTrue(status.equals(status));
+
+        // null -> returns false
+        assertFalse(status.equals(null));
+
+        // different types -> returns false
+        assertFalse(status.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(status.equals(new Status(Status.JobStatus.PENDING.toString())));
+    }
+}
