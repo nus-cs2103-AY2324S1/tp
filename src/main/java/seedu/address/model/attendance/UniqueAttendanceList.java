8b76c409e5d9152bf8a6d76b33c36f243c2dde77
Andre Chua <110965035+andrechuakj@users.noreply.github.com>
2023-10-22 16:57:05 +0800

    Update attendance list

--- src/main/java/seedu/address/model/attendance/UniqueAttendanceList.java
+++ src/main/java/seedu/address/model/attendance/UniqueAttendanceList.java
@@ -1,45 +1,149 @@
 package seedu.address.model.attendance;
 
-import java.util.ArrayList;
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import seedu.address.model.event.Event;
-import seedu.address.model.member.Member;
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.attendance.exceptions.DuplicateAttendanceException;
+import seedu.address.model.attendance.exceptions.AttendanceNotFoundException;
 
 /**
- * Represents a list of Attendance.
+ * A list of attendance that enforces uniqueness between its elements and does not allow nulls.
+ * An attendance is considered unique by comparing using {@code Attendance#equals(Object)}.
+ *
+ * Supports a minimal set of list operations.
+ *
  */
-public class UniqueAttendanceList extends ArrayList<Attendance> {
-    public void addAttendance(Attendance attendance) {
-        this.add(attendance);
+public class UniqueAttendanceList implements Iterable<Attendance> {
+
+    private final ObservableList<Attendance> internalList = FXCollections.observableArrayList();
+    private final ObservableList<Attendance> internalUnmodifiableList =
+            FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Returns true if the list contains an equivalent attendance as the given argument.
+     */
+    public boolean contains(Attendance attendanceToCheck) {
+        requireNonNull(attendanceToCheck);
+        return internalList.stream().anyMatch(attendanceToCheck::equals);
     }
 
-    public void removeAttendance(Attendance attendance) {
-        this.remove(attendance);
+    /**
+     * Adds an attendance to the list.
+     * The attendance must not already exist in the list.
+     */
+    public void createAttendance(Attendance attendanceToCreate) {
+        requireNonNull(attendanceToCreate);
+        if (contains(attendanceToCreate)) {
+            throw new DuplicateAttendanceException();
+        }
+        internalList.add(attendanceToCreate);
     }
 
-    public ArrayList<Attendance> getFilteredListByMember(Member member) {
-        ArrayList<Attendance> filteredList = new ArrayList<>();
+    /**
+     * Replaces the attendance {@code target} in the list with {@code editedAttendance}.
+     * {@code target} must exist in the list.
+     * The {@code editedAttendance} must not be the same as another attendance existing  in the list.
+     */
+    public void setAttendance(Attendance targetAttendance, Attendance editedAttendance) {
+        requireAllNonNull(targetAttendance, editedAttendance);
 
-        for (int i = 0; i < this.size(); i++) {
-            Attendance attendance = this.get(i);
-            if (attendance.getMemberName().equals(member.getName())) {
-                filteredList.add(attendance);
-            }
+        int index = internalList.indexOf(targetAttendance);
+        if (index == -1) {
+            throw new AttendanceNotFoundException();
         }
 
-        return filteredList;
-    }
-
-    public ArrayList<Attendance> getFilteredListByEvent(Event event) {
-        ArrayList<Attendance> filteredList = new ArrayList<>();
-
-        for (int i = 0; i < this.size(); i++) {
-            Attendance attendance = this.get(i);
-            if (attendance.getEventName().equals(event.getName())) {
-                filteredList.add(attendance);
-            }
+        if (!targetAttendance.equals(editedAttendance) && contains(editedAttendance)) {
+            throw new DuplicateAttendanceException();
         }
 
-        return filteredList;
+        internalList.set(index, editedAttendance);
+    }
+
+    /**
+     * Removes the equivalent attendance from the list.
+     * The attendance must exist in the list.
+     */
+    public void remove(Attendance toRemove) {
+        requireNonNull(toRemove);
+        if (!internalList.remove(toRemove)) {
+            throw new AttendanceNotFoundException();
+        }
+    }
+
+    /**
+     * Replaces the contents of this list with {@code replacement}.
+     */
+    public void setAttendances(UniqueAttendanceList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    /**
+     * Replaces the contents of this list with {@code attendances}.
+     * {@code attendances} must not contain duplicate attendances.
+     */
+    public void setAttendances(List<Attendance> attendances) {
+        requireAllNonNull(attendances);
+        if (!attendancesAreUnique(attendances)) {
+            throw new DuplicateAttendanceException();
+        }
+
+        internalList.setAll(attendances);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<Attendance> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    @Override
+    public Iterator<Attendance> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof UniqueAttendanceList)) {
+            return false;
+        }
+
+        UniqueAttendanceList otherUniqueAttendanceList = (UniqueAttendanceList) other;
+        return internalList.equals(otherUniqueAttendanceList.internalList);
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return internalList.toString();
+    }
+
+    /**
+     * Returns true if {@code attendances} contains only unique attendances.
+     */
+    private boolean attendancesAreUnique(List<Attendance> attendances) {
+        for (int i = 0; i < attendances.size() - 1; i++) {
+            for (int j = i + 1; j < attendances.size(); j++) {
+                if (attendances.get(i).equals(attendances.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
