8ebaa0f1ad1f8c2277fd916b8052f30fad469d15
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-10-23 14:42:38 +0800

    Add member and event prefixes

--- src/main/java/seedu/ccacommander/logic/parser/CliSyntax.java
+++ src/main/java/seedu/ccacommander/logic/parser/CliSyntax.java
@@ -23,4 +23,8 @@ public class CliSyntax {
     // Attendance prefixes
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
     public static final Prefix PREFIX_HOURS = new Prefix("h/");
+
+    // Index prefixes
+    public static final Prefix PREFIX_MEMBER = new Prefix("m/");
+    public static final Prefix PREFIX_EVENT = new Prefix("e/");
 }
