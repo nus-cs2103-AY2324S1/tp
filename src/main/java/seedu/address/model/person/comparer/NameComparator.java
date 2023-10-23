07955cb529806f85d328c6402c74535a28b6f9d5
nicrandomlee <leejpnicholas@gmail.com>
2023-10-24 02:00:34 +0800

    Sort Comparator Checkpoint
    Added Sort Comparator and Address, Email, Name and Phone comparator to
    compare between two persons.

---
+++ src/main/java/seedu/address/model/person/comparer/NameComparator.java
@@ -0,0 +1,41 @@
+package seedu.address.model.person.comparer;
+
+import seedu.address.model.person.Person;
+
+/**
+ * A comparator that sorts Person objects by name.
+ */
+public class NameComparator extends SortComparator {
+
+    /**
+     * The sort type for this comparator.
+     */
+    public static final String SORT_TYPE = "name";
+
+    /**
+     * Creates a new NameComparator with the given parameters.
+     *
+     * @param isActive Whether this comparator is currently active.
+     * @param isReverse Whether to sort in reverse order.
+     * @param priority The priority of this comparator.
+     */
+    public NameComparator(boolean isActive, boolean isReverse, int priority) {
+        super(isActive, isReverse, priority);
+    }
+
+    /**
+     * Compares two Person objects by name.
+     *
+     * @param p1 The first Person to compare.
+     * @param p2 The second Person to compare.
+     * @return A negative integer, zero, or a positive integer as the first argument is less than,
+     *     equal to, or greater than the second.
+     */
+    @Override
+    public int compare(Person p1, Person p2) {
+        if (p1.equals(p2)) {
+            return 0;
+        }
+        return p1.getName().fullName.compareTo(p2.getName().fullName);
+    }
+}
---
+++ src/main/java/seedu/address/model/person/comparer/SortComparator.java
@@ -0,0 +1,75 @@
+package seedu.address.model.person.comparer;
+
+import seedu.address.model.person.Person;
+
+import java.util.Comparator;
+
+/**
+ * An abstract class that defines a comparator for sorting Person objects.
+ */
+public abstract class SortComparator implements Comparator<Person> {
+
+    /**
+     * Whether this comparator is currently active.
+     */
+    protected boolean isActive;
+
+    /**
+     * Whether to sort in reverse order.
+     */
+    protected boolean isReverse;
+
+    /**
+     * The priority of this comparator.
+     */
+    protected int priority;
+
+    /**
+     * Creates a new SortComparator with the given parameters.
+     *
+     * @param isActive Whether this comparator is currently active.
+     * @param isReverse Whether to sort in reverse order.
+     * @param priority The priority of this comparator.
+     */
+    public SortComparator(boolean isActive, boolean isReverse, int priority) {
+        this.isActive = isActive;
+        this.isReverse = isReverse;
+        this.priority = isActive ? -1 : priority;
+    }
+
+    /**
+     * Returns whether this comparator is currently active.
+     *
+     * @return Whether this comparator is currently active.
+     */
+    public boolean getIsActive() {
+        return this.isActive;
+    }
+
+    /**
+     * Returns whether to sort in reverse order.
+     *
+     * @return Whether to sort in reverse order.
+     */
+    public boolean getIsReverse() {
+        return this.isReverse;
+    }
+
+    /**
+     * Returns the priority of this comparator.
+     *
+     * @return The priority of this comparator.
+     */
+    public int getPriority() {
+        return this.priority;
+    }
+
+    /**
+     * Compares two Person objects using this comparator's sorting criteria.
+     *
+     * @param p1 The first Person to compare.
+     * @param p2 The second Person to compare.
+     * @return A negative integer, zero, or a positive integer as the first argument is less than, equal to, or greater than the second.
+     */
+    public abstract int compare(Person p1, Person p2);
+}
