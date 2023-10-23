e716d0ac8bed7e7496dae6bbb40fe94aecb598f3
SelwynAng <selwynang.01@gmail.com>
2023-10-23 23:27:01 +0800

    Add more test cases for UndoCommand

---
+++ src/test/java/seedu/ccacommander/logic/commands/UndoCommandTest.java
@@ -0,0 +1,41 @@
+package seedu.ccacommander.logic.commands;
+
+import static seedu.ccacommander.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.ccacommander.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.ccacommander.testutil.TypicalCcaCommander.getTypicalCcaCommander;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.ccacommander.model.Model;
+import seedu.ccacommander.model.ModelManager;
+import seedu.ccacommander.model.UserPrefs;
+
+public class UndoCommandTest {
+    private Model model;
+    private Model expectedModel;
+
+    public static final String COMMIT_MESSAGE = "Commit Message";
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalCcaCommander(), new UserPrefs());
+        expectedModel = new ModelManager(getTypicalCcaCommander(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_hasPreviousState_success() {
+        model.commit(COMMIT_MESSAGE);
+
+        String expectedMessage = String.format(UndoCommand.MESSAGE_SUCCESS, COMMIT_MESSAGE);
+        expectedModel.commit(COMMIT_MESSAGE);
+        expectedModel.undo();
+
+        assertCommandSuccess(new UndoCommand(), model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_noPreviousState_throwsCommandException() {
+        assertCommandFailure(new UndoCommand(), model, UndoCommand.MESSAGE_NO_PREVIOUS_COMMAND);
+    }
+}
