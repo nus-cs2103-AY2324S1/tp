f74a9ddc9ffbc3e5590322e69b8c02aa7e5d4a25
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-10-23 17:42:53 +0800

    Enable storage and loading of Attendance

--- src/main/java/seedu/ccacommander/storage/JsonSerializableCcaCommander.java
+++ src/main/java/seedu/ccacommander/storage/JsonSerializableCcaCommander.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 import seedu.ccacommander.commons.exceptions.IllegalValueException;
 import seedu.ccacommander.model.CcaCommander;
 import seedu.ccacommander.model.ReadOnlyCcaCommander;
+import seedu.ccacommander.model.attendance.Attendance;
 import seedu.ccacommander.model.event.Event;
 import seedu.ccacommander.model.member.Member;
 
@@ -22,18 +23,22 @@ class JsonSerializableCcaCommander {
 
     public static final String MESSAGE_DUPLICATE_MEMBER = "Members list contains duplicate member(s).";
     public static final String MESSAGE_DUPLICATE_EVENT = "Events list contains duplicate event(s).";
+    public static final String MESSAGE_DUPLICATE_ATTENDANCE = "Attendances list contains duplicate attendance(s).";
 
     private final List<JsonAdaptedMember> members = new ArrayList<>();
     private final List<JsonAdaptedEvent> events = new ArrayList<>();
+    private final List<JsonAdaptedAttendance> attendances = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonSerializableCcaCommander} with the given members and events.
      */
     @JsonCreator
     public JsonSerializableCcaCommander(@JsonProperty("members") List<JsonAdaptedMember> members,
-                                        @JsonProperty("events") List<JsonAdaptedEvent> events) {
+                                        @JsonProperty("events") List<JsonAdaptedEvent> events,
+                                        @JsonProperty("attendances") List<JsonAdaptedAttendance> attendances) {
         this.members.addAll(members);
         this.events.addAll(events);
+        this.attendances.addAll(attendances);
     }
 
     /**
@@ -44,6 +49,8 @@ class JsonSerializableCcaCommander {
     public JsonSerializableCcaCommander(ReadOnlyCcaCommander source) {
         members.addAll(source.getMemberList().stream().map(JsonAdaptedMember::new).collect(Collectors.toList()));
         events.addAll(source.getEventList().stream().map(JsonAdaptedEvent::new).collect(Collectors.toList()));
+        attendances.addAll(source.getAttendanceList().stream().map(JsonAdaptedAttendance::new)
+                .collect(Collectors.toList()));
     }
 
     /**
@@ -66,6 +73,13 @@ class JsonSerializableCcaCommander {
                 throw new IllegalValueException(MESSAGE_DUPLICATE_EVENT);
             }
             ccaCommander.createEvent(event);
+        }
+        for (JsonAdaptedAttendance jsonAdaptedAttendance : attendances) {
+            Attendance attendance = jsonAdaptedAttendance.toModelType();
+            if (ccaCommander.hasAttendance(attendance)) {
+                throw new IllegalValueException(MESSAGE_DUPLICATE_ATTENDANCE);
+            }
+            ccaCommander.createAttendance(attendance);
         }
         return ccaCommander;
     }
