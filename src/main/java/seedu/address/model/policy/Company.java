314b6bf46b01dbefa46400f72b8fc1bf66ead29e
suryanshkushwaha <e0985892@u.nus.edu>
2023-10-23 16:39:23 +0800

    Add Company class

---
+++ src/main/java/seedu/address/model/policy/Company.java
@@ -0,0 +1,61 @@
+package seedu.address.model.policy;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class Company {
+
+    public static final String MESSAGE_CONSTRAINTS = "Should not contain unrecognizable characters";
+
+    /*
+     * Company name can be anything understandable to human being.
+     */
+    public static final String VALIDATION_REGEX = "^[a-zA-Z0-9.]+$";
+    public static final String DEFAULT_VALUE = "NOCOMPANY";
+
+    public final String value;
+
+    /**
+     * Constructs a {@code Company}.
+     *
+     * @param company A valid policy number.
+     */
+    public Company(String company) {
+        requireNonNull(company);
+        checkArgument(isValidCompany(company), MESSAGE_CONSTRAINTS);
+        value = company;
+    }
+
+    /**
+     * Returns true if a given string is a valid company.
+     */
+    public static boolean isValidCompany(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Company)) {
+            return false;
+        }
+
+        Company otherCompany = (Company) other;
+        return value.equals(otherCompany.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
