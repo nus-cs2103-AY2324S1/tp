47edd90f86e7582f2ca78f22cfaf4d34bb8d4e66
trgao <112098096+trgao@users.noreply.github.com>
2023-10-23 19:39:42 +0800

    Add additional tests

--- src/test/java/seedu/address/testutil/TypicalAppointments.java
+++ src/test/java/seedu/address/testutil/TypicalAppointments.java
@@ -4,6 +4,8 @@ import seedu.address.model.appointment.Appointment;
 
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.HOON;
+import static seedu.address.testutil.TypicalPersons.IDA;
 
 public class TypicalAppointments {
 
@@ -13,6 +15,15 @@ public class TypicalAppointments {
 
     public static final Appointment APPOINTMENT2 = new AppointmentBuilder()
             .withPatient(BENSON).withAppointmentTime("2023/01/29 21:00", "2023/01/29 23:30")
-            .withDescription("Checkup").build();
+            .withDescription("X-ray Checkup").build();
+
+    // Manually added
+    public static final Appointment APPOINTMENT3 = new AppointmentBuilder()
+            .withPatient(HOON).withAppointmentTime("2021/04/18 15:00", "2023/04/18 17:30")
+            .withDescription("Follow-up").build();
+
+    public static final Appointment APPOINTMENT4 = new AppointmentBuilder()
+            .withPatient(IDA).withAppointmentTime("2022/06/13 15:00", "2022/06/13 16:30")
+            .withDescription("Follow-up").build();
 
 }
