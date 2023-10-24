a9d5123f82edc6a3fe6519e1d6b4c87e00b44546
scarletblanks <irisu.nita@gmail.com>
2023-10-24 22:05:14 +0800

    Ensured further consistency in coding style

--- src/test/java/seedu/address/model/person/StatusTest.java
+++ src/test/java/seedu/address/model/person/StatusTest.java
@@ -13,34 +13,49 @@ public class StatusTest {
     }
 
     @Test
-    public void constructorTestingOffered() { assertEquals(new Status("offered"), new Status(StatusTypes.OFFERED));}
+    public void constructorTestingOffered() {
+        assertEquals(new Status("offered"), new Status(StatusTypes.OFFERED));
+    }
 
     @Test
-    public void constructorTestingPrelim() { assertEquals(new Status("preliminary"),
-            new Status(StatusTypes.PRELIMINARY));}
+    public void constructorTestingPrelim() {
+        assertEquals(new Status("preliminary"), new Status(StatusTypes.PRELIMINARY));
+    }
     @Test
-    public void constructorTestingInt() { assertEquals(new Status("interviewed"),
-            new Status(StatusTypes.INTERVIEWED));}
+    public void constructorTestingInt() {
+        assertEquals(new Status("interviewed"),
+            new Status(StatusTypes.INTERVIEWED));
+    }
     @Test
-    public void constructorTestingRej() { assertEquals(new Status("rejected"),
-            new Status(StatusTypes.REJECTED));}
+    public void constructorTestingRej() {
+        assertEquals(new Status("rejected"),
+            new Status(StatusTypes.REJECTED));
+    }
 
     @Test
-    public void getStatusTypeTestR() { assertEquals(new Status("rejected").getStatusType(),
-            StatusTypes.REJECTED);}
+    public void getStatusTypeTestR() {
+        assertEquals(new Status("rejected").getStatusType(),
+            StatusTypes.REJECTED);
+    }
     @Test
-    public void getStatusTypeTestI() { assertEquals(new Status("interviewed").getStatusType(),
-            StatusTypes.INTERVIEWED);}
+    public void getStatusTypeTestI() {
+        assertEquals(new Status("interviewed").getStatusType(),
+            StatusTypes.INTERVIEWED);
+    }
     @Test
-    public void getStatusTypeTestP() { assertEquals(new Status("preliminary").getStatusType(),
-            StatusTypes.REJECTED);}
+    public void getStatusTypeTestP() {
+        assertEquals(new Status("preliminary").getStatusType(),
+            StatusTypes.REJECTED);
+    }
     @Test
-    public void getStatusTypeTestO() { assertEquals(new Status("offered").getStatusType(),
-            StatusTypes.OFFERED);}
+    public void getStatusTypeTestO() {
+        assertEquals(new Status("offered").getStatusType(),
+            StatusTypes.OFFERED);
+    }
     @Test
     public void setStatusTypeTest() {
         Status testStatus = new Status();
         testStatus.setStatusType(StatusTypes.INTERVIEWED);
-        assertEquals(testStatus.getStatusType(), StatusTypes.INTERVIEWED);}
-
+        assertEquals(testStatus.getStatusType(), StatusTypes.INTERVIEWED);
+    }
 }
