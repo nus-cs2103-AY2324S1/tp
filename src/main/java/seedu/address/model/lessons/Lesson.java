f9a6d0e0eb982553a49f9ee92b06fbc6dc9b1445
Marcus Soh <macussoh38@gmail.com>
2023-10-24 16:56:51 +0800

    Fixed checkstyle issues

--- src/main/java/seedu/address/model/lessons/Lesson.java
+++ src/main/java/seedu/address/model/lessons/Lesson.java
@@ -7,7 +7,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.Optional;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.StringUtil;
@@ -223,7 +222,9 @@ public class Lesson {
      * @return stringified version of subject
      */
     public String serializeSubject() {
-        return this.subject != null ? this.subject.subjectName.toString() : Subject.Subjects.NONE.toString(); // TODO public access
+        return this.subject != null
+                ? this.subject.subjectName.toString()
+                : Subject.Subjects.NONE.toString(); // TODO public access
     }
     /**
      * Serializes the students to a String
