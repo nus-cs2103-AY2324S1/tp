5193db62222bde1430aa0ffdb47af10848bf6d38
licongshen12 <spectrashen@gmail.com>
2023-10-24 22:23:31 +0800

    Add redo

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -3,14 +3,42 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
+/**
+ * Represents a redo command to restore the application state to before the last undo action.
+ *
+ * The redo command allows users to reapply changes that were undone by the previous undo command.
+ * If there is no command available to redo, an exception is thrown.
+ */
 public class RedoCommand extends Command {
     public static final String COMMAND_WORD = "redo";
 
+    /**
+     * Success message displayed when the redo command is executed successfully.
+     */
     public static final String MESSAGE_SUCCESS = "Redo Successful. ";
+
+    /**
+     * Error message displayed when there is no command available to redo.
+     */
     public static final String MESSAGE_NO_COMMAND_TO_REDO_ERROR = "No command to redo.";
+
+    /**
+     * Usage message to guide the user on how to use the redo command.
+     */
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Restores state to before the last undo command. "
             + "\n"
             + "Example: " + COMMAND_WORD;
+
+    /**
+     * Executes the redo command.
+     *
+     * If the UserHistoryManager indicates that a redo is possible, the application state is updated.
+     * Otherwise, a CommandException is thrown.
+     *
+     * @param model The current application model.
+     * @return A CommandResult indicating the result of the redo operation.
+     * @throws CommandException if there is no command available to redo.
+     */
     @Override
     public CommandResult execute(Model model) throws CommandException {
         if (!model.getUserHistoryManager().canRedo()) {
@@ -21,6 +49,12 @@ public class RedoCommand extends Command {
         }
     }
 
+    /**
+     * Compares this RedoCommand object to another object.
+     *
+     * @param other The object to compare with.
+     * @return true if the other object is the same instance or is also an instance of RedoCommand, false otherwise.
+     */
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
--- src/main/java/seedu/address/logic/parser/RedoCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RedoCommandParser.java
@@ -5,7 +5,20 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new {@code RedoCommand} object.
+ *
+ * This parser ensures that the redo command provided by the user is valid.
+ * If additional arguments are provided with the redo command, a ParseException is thrown.
+ */
 public class RedoCommandParser implements Parser<RedoCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the RedoCommand.
+     *
+     * @param args Input arguments provided by the user.
+     * @return A RedoCommand object for execution.
+     * @throws ParseException if the user input does not conform to the expected format.
+     */
     public RedoCommand parse(String args) throws ParseException {
         if (!args.isEmpty()) {
             throw new ParseException(
