5c1b6e07dec1820114f1d8767cb54c9587c2309f
kiwibang <>
2023-10-24 23:39:29 +0800

    Add additional tests for TimeParser - This adds more tests for the TimeParser class - Add more tests to check that the API works as intended

--- src/test/java/seedu/address/logic/parser/TimeParserTest.java
+++ src/test/java/seedu/address/logic/parser/TimeParserTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.sql.Time;
 import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
@@ -35,6 +36,97 @@ class TimeParserTest {
             hasError = true;
         }
         assertTrue(hasError);
+    }
+
+    @Test
+    void testDayAnd24hTime() throws ParseException {
+        TimeParser.parseDate("Tue 1630");
+    }
+
+    @Test
+    void testDayShortForm() throws ParseException {
+        TimeParser.parseDate("Tue");
+    }
+
+    @Test
+    void testDayLongForm() throws ParseException {
+        TimeParser.parseDate("Tuesday");
+    }
+
+    @Test
+    void testDayIncompleteValid() throws ParseException {
+        TimeParser.parseDate("Tues");
+        TimeParser.parseDate("Tuesd");
+        TimeParser.parseDate("Tuesda");
+    }
+
+    @Test
+    void testDayIncompleteInvalid() {
+        boolean hasError = false;
+        try {
+            System.out.println(TimeParser.parseDate("T"));
+        } catch (ParseException ignored) {
+            hasError = true;
+        }
+        assertTrue(hasError);
+    }
+
+    @Test
+    void testDayIncompleteInvalid2() {
+        boolean hasError = false;
+        try {
+            System.out.println(TimeParser.parseDate("Tu"));
+        } catch (ParseException ignored) {
+            hasError = true;
+        }
+        assertTrue(hasError);
+    }
+
+    @Test
+    void testDayAnd12hTimeWithMinutes() throws ParseException {
+        TimeParser.parseDate("Tue 4.30pm");
+    }
+
+    // TODO: FIX THIS FROM PASSING
+    @Test
+    void testDayAnd12hTimeWithoutMinutes() throws ParseException {
+        TimeParser.parseDate("Tue 4pmdfjs");
+    }
+
+    @Test
+    void testDayAnd12hTimeWithoutMinutes2() throws ParseException {
+        TimeParser.parseDate("Tue 4pm");
+    }
+
+    @Test
+    void testYearMonthDayTime() throws ParseException {
+        TimeParser.parseDate("21/12/2024 5pm");
+    }
+
+    @Test
+    void testYearMonthDayTime2() throws ParseException {
+        TimeParser.parseDate("21-12-2024 5pm");
+    }
+
+    @Test
+    void testYearMonthDayTime3() throws ParseException {
+        System.out.println(TimeParser.parseDate("21-12-2024 1730"));
+    }
+
+    @Test
+    void testYearMonthDayTime4() throws ParseException {
+        System.out.println(TimeParser.parseDate("21-12-2024 1730pm"));
+    }
+
+    // TODO: FIX THIS FROM BECOMING 1.30AM
+    @Test
+    void testYearMonthDayTime5() throws ParseException {
+        System.out.println(TimeParser.parseDate("nov 12 1.30pm 2023"));
+    }
+
+    @Test
+    void testYearMonthDayTime6() throws ParseException {
+        System.out.println(TimeParser.parseDate("2023-12-12 1647"));
     }
 
     @Test
