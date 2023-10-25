f903342a35859094931f94ca5e34bf331df1a002
Wee Hung <cwhung1804@gmail.com>
2023-10-26 00:21:48 +0800

    update GUI to have logger tab and admin menu tab

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -38,8 +38,8 @@ public class MainWindow extends UiPart<Stage> {
     @FXML
     private StackPane commandBoxPlaceholder;
 
-    @FXML
-    private MenuItem helpMenuItem;
+    // @FXML
+    // private MenuItem helpMenuItem;
 
     @FXML
     private StackPane personListPanelPlaceholder;
@@ -63,7 +63,7 @@ public class MainWindow extends UiPart<Stage> {
         // Configure the UI
         setWindowDefaultSize(logic.getGuiSettings());
 
-        setAccelerators();
+        // setAccelerators();
 
         helpWindow = new HelpWindow();
     }
@@ -72,39 +72,39 @@ public class MainWindow extends UiPart<Stage> {
         return primaryStage;
     }
 
-    private void setAccelerators() {
-        setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
-    }
+    // private void setAccelerators() {
+    //     setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
+    // }
 
     /**
      * Sets the accelerator of a MenuItem.
      * @param keyCombination the KeyCombination value of the accelerator
      */
-    private void setAccelerator(MenuItem menuItem, KeyCombination keyCombination) {
-        menuItem.setAccelerator(keyCombination);
+    // private void setAccelerator(MenuItem menuItem, KeyCombination keyCombination) {
+    //     menuItem.setAccelerator(keyCombination);
 
-        /*
-         * TODO: the code below can be removed once the bug reported here
-         * https://bugs.openjdk.java.net/browse/JDK-8131666
-         * is fixed in later version of SDK.
-         *
-         * According to the bug report, TextInputControl (TextField, TextArea) will
-         * consume function-key events. Because CommandBox contains a TextField, and
-         * ResultDisplay contains a TextArea, thus some accelerators (e.g F1) will
-         * not work when the focus is in them because the key event is consumed by
-         * the TextInputControl(s).
-         *
-         * For now, we add following event filter to capture such key events and open
-         * help window purposely so to support accelerators even when focus is
-         * in CommandBox or ResultDisplay.
-         */
-        getRoot().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
-            if (event.getTarget() instanceof TextInputControl && keyCombination.match(event)) {
-                menuItem.getOnAction().handle(new ActionEvent());
-                event.consume();
-            }
-        });
-    }
+    //     /*
+    //      * TODO: the code below can be removed once the bug reported here
+    //      * https://bugs.openjdk.java.net/browse/JDK-8131666
+    //      * is fixed in later version of SDK.
+    //      *
+    //      * According to the bug report, TextInputControl (TextField, TextArea) will
+    //      * consume function-key events. Because CommandBox contains a TextField, and
+    //      * ResultDisplay contains a TextArea, thus some accelerators (e.g F1) will
+    //      * not work when the focus is in them because the key event is consumed by
+    //      * the TextInputControl(s).
+    //      *
+    //      * For now, we add following event filter to capture such key events and open
+    //      * help window purposely so to support accelerators even when focus is
+    //      * in CommandBox or ResultDisplay.
+    //      */
+    //     getRoot().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+    //         if (event.getTarget() instanceof TextInputControl && keyCombination.match(event)) {
+    //             menuItem.getOnAction().handle(new ActionEvent());
+    //             event.consume();
+    //         }
+    //     });
+    // }
 
     /**
      * Fills up all the placeholders of this window.
