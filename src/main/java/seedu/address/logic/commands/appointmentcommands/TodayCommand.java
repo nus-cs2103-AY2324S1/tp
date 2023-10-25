fa3167bed497fab3527d5b25a3144e0c72b40721
trgao <112098096+trgao@users.noreply.github.com>
2023-10-25 15:01:21 +0800

    Add today command

---
+++ src/main/java/seedu/address/logic/commands/appointmentcommands/TodayCommand.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.commands.appointmentcommands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.model.Model;
+
+import java.time.LocalDate;
+
+/**
+ * Lists all appointments occurring on the current date in the address book to the user.
+ */
+public class TodayCommand extends Command {
+    public static final String COMMAND_WORD = "today";
+
+    public static final String MESSAGE_SUCCESS = "Listed all appointments occurring today";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredAppointmentList(appointment -> appointment.getStartTime().toLocalDate().isEqual(LocalDate.now()));
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
