1564340edf7226d75bd3a8fe99472aed004d48a1
h1410101 <h1410101@gmail.com>
2023-10-25 20:41:15 +0800

    Remove unused imports

--- src/test/java/seedu/address/logic/search/FindCommandArgumentParserTest.java
+++ src/test/java/seedu/address/logic/search/FindCommandArgumentParserTest.java
@@ -1,9 +1,7 @@
 package seedu.address.logic.search;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
