70bf54de4b728c30b6fe3d79328ec28889f5f8c3
DS <yeodisheng@gmail.com>
2023-10-25 16:51:23 +0800

    Add separator for past schedules in Ui

--- src/main/java/seedu/address/ui/ScheduleListPanel.java
+++ src/main/java/seedu/address/ui/ScheduleListPanel.java
@@ -1,19 +1,26 @@
 package seedu.address.ui;
 
+import java.time.LocalDateTime;
 import java.util.logging.Logger;
 
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.schedule.Schedule;
+import seedu.address.model.schedule.StartTime;
 
 /**
  * Panel containing the list of schedules.
  */
 public class ScheduleListPanel extends UiPart<Region> {
+    private static int dividerIndex = -1;
     private static final String FXML = "ScheduleListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(ScheduleListPanel.class);
 
@@ -27,6 +34,20 @@ public class ScheduleListPanel extends UiPart<Region> {
         super(FXML);
         scheduleListView.setItems(scheduleList);
         scheduleListView.setCellFactory(listView -> new ScheduleListViewCell());
+
+        getDividerIndex(scheduleList);
+        scheduleList.addListener((ListChangeListener<Schedule>) c -> getDividerIndex(scheduleList));
+    }
+
+    private void getDividerIndex(ObservableList<Schedule> scheduleList) {
+        LocalDateTime now = LocalDateTime.now();
+        for (int i = 0; i < scheduleList.size(); i++) {
+            if (scheduleList.get(i).getStartTime().compareDays(new StartTime(now)) < 0) {
+                dividerIndex = i;
+                break;
+            }
+        }
+
     }
 
     /**
@@ -40,10 +61,16 @@ public class ScheduleListPanel extends UiPart<Region> {
             if (empty || schedule == null) {
                 setGraphic(null);
                 setText(null);
+            } else if (getIndex() == dividerIndex) {
+                VBox container = new VBox();
+                Label dividerLabel = new Label("Past Schedules");
+                Separator separator = new Separator();
+                container.getChildren()
+                        .addAll(dividerLabel, separator, new ScheduleCard(schedule, getIndex() + 1).getRoot());
+                setGraphic(container);
             } else {
                 setGraphic(new ScheduleCard(schedule, getIndex() + 1).getRoot());
             }
         }
     }
-
 }
