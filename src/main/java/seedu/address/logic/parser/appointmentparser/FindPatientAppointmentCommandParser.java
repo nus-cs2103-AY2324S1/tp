7f28df1437175dafe94a941dcaeb70ee0d40c40c
billieboy7 <smkbillkirankumar@gmail.com>
2023-10-25 16:07:27 +0800

    Set up FindPatientAppointmentCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/appointmentparser/FindPatientAppointmentCommandParser.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.parser.appointmentparser;
+
+import seedu.address.logic.commands.appointmentcommands.FindPatientAppointmentCommand;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class FindPatientAppointmentCommandParser implements Parser<FindPatientAppointmentCommand> {
+    /**
+     * Parses {@code userInput} and returns a FindPatientAppointmentCommand object to be executed.
+     *
+     * @param userInput user's input which is of type String.
+     * @throws ParseException if {@code userInput} does not conform to the expected format.
+     */
+    @Override
+    public FindPatientAppointmentCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
