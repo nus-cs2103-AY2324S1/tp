63e27e41544fad10e2f61c028c691b5f089db7ef
anthonytamzil <anthony.tamzil@gmail.com>
2023-10-26 23:41:26 +0800

    Add ViewCommand and ViewCommandParser

---
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Attendance;
+import seedu.address.model.person.Person;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Format full help instructions for every command for display.
+ */
+public class ViewCommand extends Command {
+
+    public static final String COMMAND_WORD = "view";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows student's attendance record in detail.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_NO_ATTENDANCE_RECORD = " has no attendance records.";
+
+    private final Index targetIndex;
+
+    /**
+     * @param targetIndex Index number used in the displayed person list of the target
+     */
+    public ViewCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        StringBuilder attendanceRecordString = new StringBuilder();
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex == null || (targetIndex.getZeroBased() >= lastShownList.size()
+                || targetIndex.getZeroBased() < 0)) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToView = lastShownList.get(targetIndex.getZeroBased());
+        List<Attendance> personAttendanceRecord = personToView.getAttendanceRecords();
+
+        if (personAttendanceRecord.isEmpty()) {
+            return new CommandResult(personToView.getName().toString() + MESSAGE_NO_ATTENDANCE_RECORD);
+        }
+
+        for (Attendance record : personAttendanceRecord) {
+            String attendanceStatus = record.isPresent()
+                    ? "Present\n"
+                    : "Absent\n";
+            attendanceRecordString.append("Week " + record.getWeek() + " : " + attendanceStatus);
+        }
+
+        return new CommandResult(attendanceRecordString.toString());
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/ViewCommandParser.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.ViewCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+public class ViewCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ViewCommand
+     * and returns a ViewCommand object for execution.
+     * @throws ParseException if the user input does not conform to the expected format
+     */
+    public ViewCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(trimmedArgs);
+        } catch (ParseException p) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE), p
+            );
+        }
+
+        return new ViewCommand(index);
+    }
+}
