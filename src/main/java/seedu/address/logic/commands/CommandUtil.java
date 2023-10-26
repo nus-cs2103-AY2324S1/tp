c2c8ea36eeb4f0068a7ff93c1cf1f2b824f45e4c
WZWren <115881359+WZWren@users.noreply.github.com>
2023-10-26 17:15:43 +0800

    Add JavaDoc for CommandUtil

--- src/main/java/seedu/address/logic/commands/CommandUtil.java
+++ src/main/java/seedu/address/logic/commands/CommandUtil.java
@@ -7,7 +7,19 @@ import seedu.address.model.person.Name;
 import seedu.address.model.person.Nric;
 import seedu.address.model.person.Person;
 
+/**
+ * Contains shared utility methods used in the various *Command classes.
+ */
 public class CommandUtil {
+    /**
+     * Finds a {@code Person} from {@code List<Person> persons} using the given {@code Name name}
+     * and {@code Nric nric}, if able.
+     *
+     * @param name The {@code Name} identifier field to search with.
+     * @param nric The {@code Nric} identifier field to search with.
+     * @param persons The collection of {@code Person} to search within.
+     * @return A person that was found, wrapped as an {@code Optional<Person>}.
+     */
     public static Optional<Person> findPersonByIdentifier(Name name, Nric nric, List<Person> persons) {
         Optional<Person> personByName = persons.stream()
                 .filter(p -> p.getName().equals(name))
