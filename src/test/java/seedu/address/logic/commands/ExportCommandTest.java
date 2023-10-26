9924085ffa68138ba07cfee12e9048f6a071429a
scarletblanks <irisu.nita@gmail.com>
2023-10-26 08:55:26 +0800

    Added more JUnit Tests for ExportCommand class

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -12,9 +14,13 @@ import seedu.address.model.UserPrefs;
 public class ExportCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     @Test
-    public void exportStringSafe() {
+    public void equalsMethod() {
         assert(new ExportCommand().equals(new ExportCommand()));
     }
 
+    @Test
+    public void toStringMethod() {
+        assertEquals(new ExportCommand().toString(), "ExportCommand");
+    }
 
 }
