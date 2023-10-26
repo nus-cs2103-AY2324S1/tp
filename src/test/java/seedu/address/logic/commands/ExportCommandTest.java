e019648d2f207c94cfc7c8a3e871543a61c347c9
scarletblanks <irisu.nita@gmail.com>
2023-10-26 09:30:21 +0800

    Added a direct test for the ExportCommand's execute function

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -1,10 +1,11 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
@@ -12,6 +13,7 @@ import seedu.address.model.ModelManager;
 public class ExportCommandTest {
     private Model model = new ModelManager();
     private Model expectedModel = new ModelManager();
+    private ExportCommand exportCommand = new ExportCommand();
     @Test
     public void equalsMethod() {
         assert(new ExportCommand().equals(new ExportCommand()));
@@ -23,9 +25,24 @@ public class ExportCommandTest {
     }
 
     @Test
-    public void execute_success() {
-        CommandResult expectedCommandResult = new CommandResult("Sucessfully Exported", false, false, false);
-        assertCommandSuccess(new ExportCommand(), model, expectedCommandResult, expectedModel);
+    public void execute_validExport_successful() {
+        try {
+            CommandResult result = exportCommand.execute(model);
+            assertEquals(ExportCommand.MESSAGE_SUCCESS, result.getFeedbackToUser());
+        } catch (CommandException e) {
+            fail("This test has failed because of a failed export");
+        }
     }
+
+    @Test
+    public void equals_sameObject_true() {
+        assertEquals(exportCommand, exportCommand);
+    }
+
+    @Test
+    public void equals_null_false() {
+        assertEquals(false, exportCommand.equals(null));
+    }
+
 
 }
