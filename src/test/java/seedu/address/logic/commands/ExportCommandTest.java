5e8ee53ffefda03c431631abcb3c697a313c996c
scarletblanks <irisu.nita@gmail.com>
2023-10-26 09:20:53 +0800

    Added JUnit Tests

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -2,18 +2,16 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
 
 
 public class ExportCommandTest {
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
     @Test
     public void equalsMethod() {
         assert(new ExportCommand().equals(new ExportCommand()));
@@ -26,12 +24,8 @@ public class ExportCommandTest {
 
     @Test
     public void execute_success() {
-        ExportCommand exportCommand = new ExportCommand();
-
-        String expectedMessage = String.format(ExportCommand.MESSAGE_SUCCESS);
-
-        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-        assertCommandSuccess(exportCommand, model, expectedMessage, expectedModel, false);
+        CommandResult expectedCommandResult = new CommandResult("Sucessfully Exported", false, false, false);
+        assertCommandSuccess(new ExportCommand(), model, expectedCommandResult, expectedModel);
     }
 
 }
