1ae862642ecc67e08409071c442c3dd01964110d
scarletblanks <irisu.nita@gmail.com>
2023-10-26 08:41:25 +0800

    Added test cases for AddressBook, Added more abstraction for ExportCommand

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -24,45 +24,50 @@ public class ExportCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Sucessfully Exported";
 
+    private String defaulltPath = "data/export.csv";
+
     /**
      * Empty constructor,
      */
     public ExportCommand() {}
 
-    public String treatAsString(String str) {
+    private String treatAsString(String str) {
         return "\"" + str + "\"";
+    }
+
+    private void appendPersons(FileWriter writer, Model model) throws IOException {
+        writer.append("Name,Phone,Email,Address,Tags,LinkedIn,Github,Remark,Status\n");
+
+        for (Person p : model.getAddressBook().getPersonList()) {
+            String name = treatAsString(p.getName().toString());
+            String phone = treatAsString(p.getPhone().toString());
+            String email = treatAsString(p.getEmail().toString());
+            String address = treatAsString(p.getAddress().toString());
+            String tags = treatAsString(p.getTags().toString());
+            String linkedIn = treatAsString(p.getLinkedIn().toString());
+            String github = treatAsString(p.getGithub().toString());
+            String remark = treatAsString(p.getRemark().toString());
+            String status = treatAsString(p.getStatus().toString());
+
+            writer.append(name)
+                    .append(",").append(phone)
+                    .append(",").append(email)
+                    .append(",").append(address)
+                    .append(",").append(tags)
+                    .append(",").append(linkedIn)
+                    .append(",").append(github)
+                    .append(",").append(remark)
+                    .append(",").append(status)
+                    .append("\n");
+        }
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        String fileName = "data/export.csv";
 
-        try (FileWriter writer = new FileWriter(fileName)) {
-            writer.append("Name,Phone,Email,Address,Tags,LinkedIn,Github,Remark,Status\n"); // Write the header
-
-            for (Person p : model.getAddressBook().getPersonList()) {
-                String name = treatAsString(p.getName().toString());
-                String phone = treatAsString(p.getPhone().toString());
-                String email = treatAsString(p.getEmail().toString());
-                String address = treatAsString(p.getAddress().toString());
-                String tags = treatAsString(p.getTags().toString());
-                String linkedIn = treatAsString(p.getLinkedIn().toString());
-                String github = treatAsString(p.getGithub().toString());
-                String remark = treatAsString(p.getRemark().toString());
-                String status = treatAsString(p.getStatus().toString());
-
-                writer.append(name)
-                        .append(",").append(phone)
-                        .append(",").append(email)
-                        .append(",").append(address)
-                        .append(",").append(tags)
-                        .append(",").append(linkedIn)
-                        .append(",").append(github)
-                        .append(",").append(remark)
-                        .append(",").append(status)
-                        .append("\n");
-            }
+        try (FileWriter writer = new FileWriter(defaulltPath)) {
+            appendPersons(writer, model);
             return new CommandResult(MESSAGE_SUCCESS);
         } catch (IOException e) {
             throw new CommandException("Error exporting data: " + e.getMessage());
