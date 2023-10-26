27064cfda5de031ca6d6d9e15fe09c99e6e8c1ef
Xenos F <git@xenosfio.com>
2023-10-26 17:09:57 +0800

    Improve hyperlink styling

--- src/main/java/networkbook/ui/FieldHyperlink.java
+++ src/main/java/networkbook/ui/FieldHyperlink.java
@@ -1,21 +1,17 @@
 package networkbook.ui;
 
-import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 
 /**
  * An UI component that displays text with an action performed on click.
  */
-public class FieldHyperlink extends Button {
-
-    private final Runnable action;
+public class FieldHyperlink extends Hyperlink {
 
     /**
      * Creates a {@code FieldHyperlink} with the given label and action.
      */
     public FieldHyperlink(String labelText, Runnable action) {
-        // Set button parameters
-        this.setText(labelText);
-        this.action = action;
+        super(labelText);
         this.setOnAction(e -> action.run());
     }
 
