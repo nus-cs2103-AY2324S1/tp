eb8fabad832cf8f441659fbf9d0e204925c73c3d
Janssen <janssenlau88@gmail.com>
2023-10-26 22:08:48 +0800

    Change method access modifier

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -132,7 +132,7 @@ public class EditCommand extends Command {
      * Creates and returns a {@code Person} with the details of {@code personToEdit}
      * edited with {@code editPersonDescriptor}.
      */
-    static Person createEditedPerson(Person personToEdit, EditPersonDescriptor editPersonDescriptor) {
+    public static Person createEditedPerson(Person personToEdit, EditPersonDescriptor editPersonDescriptor) {
         assert personToEdit != null;
 
         Name name = editPersonDescriptor.getName().orElse(personToEdit.getName());
