e95dedc208a5aa44d51c2c9aa8360052c5312270
Eola-Z <sxplayer123@gmail.com>
2023-10-26 22:16:07 +0800

    Fix checkstyle

--- src/test/java/networkbook/logic/commands/FilterCommandTest.java
+++ src/test/java/networkbook/logic/commands/FilterCommandTest.java
@@ -1,9 +1,7 @@
 package networkbook.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 import java.util.List;
