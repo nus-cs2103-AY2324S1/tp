7f6918c50bb39ba274828acfc21d3d0c2696c93e
Janssen <janssenlau88@gmail.com>
2023-10-26 11:55:34 +0800

    Increase code coverage

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -132,7 +132,7 @@ public class EditCommand extends Command {
      * Creates and returns a {@code Person} with the details of {@code personToEdit}
      * edited with {@code editPersonDescriptor}.
      */
-    private static Person createEditedPerson(Person personToEdit, EditPersonDescriptor editPersonDescriptor) {
+    static Person createEditedPerson(Person personToEdit, EditPersonDescriptor editPersonDescriptor) {
         assert personToEdit != null;
 
         Name name = editPersonDescriptor.getName().orElse(personToEdit.getName());
