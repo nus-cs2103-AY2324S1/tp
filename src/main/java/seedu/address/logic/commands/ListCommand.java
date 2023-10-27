5b6fc9fd92f9b337b6a0f728c222723817fad930
KamJiaYue <e0960550@u.nus.edu>
2023-10-27 15:43:15 +0800

    check style

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -3,22 +3,21 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-import seedu.address.model.Model;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import seedu.address.model.Model;
 
 /**
  * Lists all persons in the address book to the user.
  */
 public class ListCommand extends Command {
 
-    private static Logger logger = Logger.getLogger(ListCommand.class.getName());
-
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
+    private static Logger logger = Logger.getLogger(ListCommand.class.getName());
 
     @Override
     public CommandResult execute(Model model) {
