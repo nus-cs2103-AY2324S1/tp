feaddc18ff3afb58939422dcfbbf64727cc35f71
lihongguang00 <lihongguang00@gmail.com>
2023-10-27 16:15:27 +0800

    Change Authorship of DeleteEventCommandParser
    Changed Authorship of DeleteEventCommandParser

--- src/main/java/seedu/address/logic/parser/DeleteEventCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteEventCommandParser.java
@@ -1,23 +1,26 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.commands.DeleteEventCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
-
-import java.time.LocalDateTime;
-
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.model.event.EventPeriod.DATE_TIME_STRING_FORMATTER;
 
-public class DeleteEventCommandParser implements  Parser<DeleteEventCommand> {
+import java.time.LocalDateTime;
+
+import seedu.address.logic.commands.DeleteEventCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new DeleteEventCommand object.
+ */
+public class DeleteEventCommandParser implements Parser<DeleteEventCommand> {
     @Override
     public DeleteEventCommand parse(String userInput) throws ParseException {
         try {
-            String startString = userInput.trim();
-            LocalDateTime startTime = LocalDateTime.parse(startString, DATE_TIME_STRING_FORMATTER);
-            return new DeleteEventCommand(startTime);
+            String eventString = userInput.trim();
+            LocalDateTime eventTime = LocalDateTime.parse(eventString, DATE_TIME_STRING_FORMATTER);
+            return new DeleteEventCommand(eventTime);
         } catch (Exception pe) {
             throw new ParseException(String.format(
-                    MESSAGE_INVALID_COMMAND_FORMAT, DeleteEventCommand.MESSAGE_USAGE), pe);
+                    MESSAGE_INVALID_COMMAND_FORMAT, DeleteEventCommand.MESSAGE_USAGE));
         }
     }
 }
