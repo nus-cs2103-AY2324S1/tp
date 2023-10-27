60bcbb935f9a2b41e24d521f234f10d3cd745c8f
QianChangru <changru_qian@u.nus.edu>
2023-10-27 21:02:09 +0800

    Improve code quality

--- src/test/java/seedu/address/logic/parser/MarkAllCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/MarkAllCommandParserTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.MarkAllCommand;
-import seedu.address.logic.commands.MarkCommand;
 
 /**
  * Tests MarkAllCommandParser.
@@ -18,8 +17,10 @@ public class MarkAllCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                MarkAllCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                MarkAllCommand.MESSAGE_USAGE));
     }
 
     @Test
