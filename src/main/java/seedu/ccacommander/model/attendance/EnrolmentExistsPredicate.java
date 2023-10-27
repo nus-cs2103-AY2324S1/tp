18f64645a255dd9d82292b75a1dd66bf375c3246
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-10-28 00:32:49 +0800

    Create EnrolmentExistsPredicate
    This predicate was created so that it is possible to check if an
    Enrolment exists so that it can be edited.

---
+++ src/main/java/seedu/ccacommander/model/attendance/EnrolmentExistsPredicate.java
@@ -0,0 +1,43 @@
+package seedu.ccacommander.model.attendance;
+
+import seedu.ccacommander.commons.util.ToStringBuilder;
+
+import java.util.function.Predicate;
+
+/**
+ * Tests that a {@code Enrolment} is the same as the {@code Enrolment} given.
+ */
+public class EnrolmentExistsPredicate implements Predicate<Attendance> {
+    private final Attendance enrolmentToCheck;
+
+    public EnrolmentExistsPredicate(Attendance enrolmentToCheck) {
+        this.enrolmentToCheck = enrolmentToCheck;
+    }
+
+    @Override
+    public boolean test(Attendance enrolment) {
+        return enrolmentToCheck.isSameAttendance(enrolment);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EnrolmentExistsPredicate)) {
+            return false;
+        }
+
+        EnrolmentExistsPredicate otherEventNameContainsKeywordsPredicate =
+                (EnrolmentExistsPredicate) other;
+        return enrolmentToCheck.equals(otherEventNameContainsKeywordsPredicate.enrolmentToCheck);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("enrolmentToCheck", enrolmentToCheck).toString();
+    }
+}
