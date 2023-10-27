b5c48de6d99f8f441f80da53a3b26e21121377aa
yytan25 <tan.yang.yi.2001@gmail.com>
2023-10-27 15:01:28 +0800

    Fix bugs due to merge conflicts

--- src/test/java/seedu/address/logic/commands/AddMusiciantoBandCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddMusiciantoBandCommandTest.java
@@ -193,11 +193,6 @@ public class AddMusiciantoBandCommandTest {
         }
 
         @Override
-        public void removeMusicianFromBand(int bandIndex, int musicianIndex) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
         public void deleteBand(Band target) {
             throw new AssertionError("This method should not be called.");
         }
