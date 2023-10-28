18acd9864801235a4e3973a257f64f8026e24485
Joey Lee <joeylee@comp.nus.edu>
2023-10-29 05:05:01 +0800

    Remove leave status from Attendance, to be handles solely by leave

--- src/main/java/seedu/address/model/person/attendance/AttendanceType.java
+++ src/main/java/seedu/address/model/person/attendance/AttendanceType.java
@@ -6,13 +6,12 @@ package seedu.address.model.person.attendance;
 public enum AttendanceType {
     LATE,
     ABSENT,
-    PRESENT,
-    ON_LEAVE;
+    PRESENT;
     public static final String MESSAGE_CONSTRAINTS =
-            "Attendance type should only contain alphabets. It should either be 'present', 'late', 'absent', 'leave'";
+            "Attendance type should only contain alphabets. It should either be 'present', 'late', 'absent'";
     public static boolean isValidAttendanceType(String name) {
         name = name.toLowerCase();
-        return name.equals("present") || name.equals("absent") || name.equals("late") || name.equals("leave");
+        return name.equals("present") || name.equals("absent") || name.equals("late");
     }
 
 }
