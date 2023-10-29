4e55a28ed5eb6297763c23f3195d755ed4707864
ph-nathan <px5099@gmail.com>
2023-10-29 22:18:22 +0800

    Fix list command

--- src/main/java/seedu/ccacommander/logic/commands/ListCommand.java
+++ src/main/java/seedu/ccacommander/logic/commands/ListCommand.java
@@ -5,39 +5,32 @@ import static java.util.Objects.requireNonNull;
 import static seedu.ccacommander.model.Model.PREDICATE_SHOW_ALL_EVENTS;
 import static seedu.ccacommander.model.Model.PREDICATE_SHOW_ALL_MEMBERS;
 
-import java.util.List;
-
 import seedu.ccacommander.model.Model;
-import seedu.ccacommander.model.enrolment.Hours;
-import seedu.ccacommander.model.enrolment.Remark;
-import seedu.ccacommander.model.member.Member;
 import seedu.ccacommander.ui.EventListPanel;
 import seedu.ccacommander.ui.MemberListPanel;
 
 
 
 /**
- * Lists all persons in CcaCommander to the user.
+ * Lists all members and events in CcaCommander to the user.
  */
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all members";
+    public static final String MESSAGE_SUCCESS = "Listed all members and events";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        List<Member> lastShownMemberList = model.getFilteredMemberList();
-        for (Member member: lastShownMemberList) {
-            member.setHours(new Hours("0"));
-            member.setRemark(new Remark("None"));
-        }
+
         MemberListPanel.setIsViewEventCommand(false);
         EventListPanel.setIsViewMemberCommand(false);
+
         model.updateFilteredMemberList(PREDICATE_SHOW_ALL_MEMBERS);
         model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
+
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
