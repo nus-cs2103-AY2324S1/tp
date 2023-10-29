3842ac3253eec7d24615b2c329d9a8499684fef0
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-10-30 06:59:19 +0800

    feat(AddCommand): Help text optional note

--- src/main/java/swe/context/logic/commands/AddCommand.java
+++ src/main/java/swe/context/logic/commands/AddCommand.java
@@ -28,7 +28,7 @@ public class AddCommand extends Command {
             + PREFIX_NAME + "NAME "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
-            + PREFIX_NOTE + "NOTE "
+            + "[" + PREFIX_NOTE + "NOTE] "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
