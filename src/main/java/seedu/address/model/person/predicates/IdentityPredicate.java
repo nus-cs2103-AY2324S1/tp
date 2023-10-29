459cf441f7f092dd6693401f702dabfa1e3252f2
WZWren <115881359+WZWren@users.noreply.github.com>
2023-10-30 01:12:34 +0800

    Add well-defined Predicate classes

---
+++ src/main/java/seedu/address/model/person/predicates/IdentityPredicate.java
@@ -0,0 +1,34 @@
+package seedu.address.model.person.predicates;
+
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.person.Person;
+
+/**
+ * Default predicate for predicate chaining.
+ */
+public class IdentityPredicate implements Predicate<Person> {
+
+    public IdentityPredicate() {}
+
+    @Override
+    public boolean test(Person person) {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        return other instanceof IdContainsKeywordsPredicate;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("identity", true).toString();
+    }
+}
