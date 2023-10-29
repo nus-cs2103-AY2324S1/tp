6c3a286f8d7edbec7d14a80b7ea8ebd2ba36338b
Alicia Teng Hui Xin <teng.hxa@gmail.com>
2023-10-30 05:57:04 +0800

    add delete parser tests

---
+++ src/test/java/seedu/address/logic/parser/DeleteMemberCommandParserTest.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.DeleteMemberCommand;
+
+
+public class DeleteMemberCommandParserTest {
+
+    private DeleteMemberCommandParser parser = new DeleteMemberCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsDeleteCommand() {
+        assertParseSuccess(parser, "1", new DeleteMemberCommand(INDEX_FIRST_PERSON));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteMemberCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidNumber_throwsParseException() {
+        assertParseFailure(parser, "0", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteMemberCommand.MESSAGE_USAGE));
+    }
+}
