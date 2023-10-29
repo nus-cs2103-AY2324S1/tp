7252dbad6733bca579cac3e2e05dedecf126136e
itssisi <e0969060@u.nus.edu>
2023-10-30 00:27:58 +0800

    Refactor time in Lesson class into TimeInterval
    Currently, the start time and end time of lesson is handled directly by the Lesson class.
    
    For better OOP, the time class is refactor into TimeInterval class.

--- src/main/java/seedu/address/model/course/CourseData.java
+++ src/main/java/seedu/address/model/course/CourseData.java
@@ -1,7 +1,14 @@
 package seedu.address.model.course;
 
+import static java.time.DayOfWeek.MONDAY;
+import static java.time.DayOfWeek.TUESDAY;
+import static java.time.DayOfWeek.WEDNESDAY;
+
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Set;
+
+import seedu.address.model.availability.TimeInterval;
 
 /**
  * Contains a list of SOC courses.
@@ -10,37 +17,52 @@ public class CourseData {
     private static final List<Course> courseList = List.of(
             new Course("Discrete Structures", "CS1231S",
                     Set.of(
-                            new Lesson("Lecture", "CS1231S", "MONDAY", "10:00", "12:00"),
-                            new Lesson("Tutorial", "CS1231S", "TUESDAY", "10:00", "12:00"),
-                            new Lesson("Laboratory", "CS1231S", "WEDNESDAY", "10:00", "12:00")
+                            new Lesson("Lecture", "CS1231S", MONDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0))),
+                            new Lesson("Tutorial", "CS1231S", TUESDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0))),
+                            new Lesson("Laboratory", "CS1231S", WEDNESDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0)))
                     )
             ),
             new Course("Programming Methodology", "CS1101S",
                     Set.of(
-                            new Lesson("Lecture", "CS1101S", "MONDAY", "10:00", "12:00"),
-                            new Lesson("Tutorial", "CS1101S", "TUESDAY", "10:00", "12:00"),
-                            new Lesson("Laboratory", "CS1101S", "WEDNESDAY", "10:00", "12:00")
+                            new Lesson("Lecture", "CS1101S", MONDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0))),
+                            new Lesson("Tutorial", "CS1101S", TUESDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0))),
+                            new Lesson("Laboratory", "CS1101S", WEDNESDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0)))
                     )
             ),
             new Course("Data Structures and Algorithms", "CS2040S",
                     Set.of(
-                            new Lesson("Lecture", "CS2040S", "MONDAY", "10:00", "12:00"),
-                            new Lesson("Tutorial", "CS2040S", "TUESDAY", "10:00", "12:00"),
-                            new Lesson("Laboratory", "CS2040S", "WEDNESDAY", "10:00", "12:00")
+                            new Lesson("Lecture", "CS2040S", MONDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0))),
+                            new Lesson("Tutorial", "CS2040S", TUESDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0))),
+                            new Lesson("Laboratory", "CS2040S", WEDNESDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0)))
                     )
             ),
             new Course("Computer Organisation", "CS2100",
                     Set.of(
-                            new Lesson("Lecture", "CS2100", "MONDAY", "10:00", "12:00"),
-                            new Lesson("Tutorial", "CS2100", "TUESDAY", "10:00", "12:00"),
-                            new Lesson("Laboratory", "CS2100", "WEDNESDAY", "10:00", "12:00")
+                            new Lesson("Lecture", "CS2100", MONDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0))),
+                            new Lesson("Tutorial", "CS2100", TUESDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0))),
+                            new Lesson("Laboratory", "CS2100", WEDNESDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0)))
                     )
             ),
             new Course("Software Engineering", "CS2103T",
                     Set.of(
-                            new Lesson("Lecture", "CS2103T", "MONDAY", "10:00", "12:00"),
-                            new Lesson("Tutorial", "CS2103T", "TUESDAY", "10:00", "12:00"),
-                            new Lesson("Laboratory", "CS2103T", "WEDNESDAY", "10:00", "12:00")
+                            new Lesson("Lecture", "CS2103T", MONDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0))),
+                            new Lesson("Tutorial", "CS2103T", TUESDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0))),
+                            new Lesson("Laboratory", "CS2103T", WEDNESDAY,
+                                    new TimeInterval(LocalTime.of(10, 0), LocalTime.of(12, 0)))
                     )
             )
     );
