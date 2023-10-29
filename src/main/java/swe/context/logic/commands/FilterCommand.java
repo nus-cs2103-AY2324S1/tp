d8facd44397ae0cd282fa00a2dffbad9bcbd35c4
bwangpj <70694994+bwangpj@users.noreply.github.com>
2023-10-29 16:29:16 +0800

    Add filter by tag command
    Add filter command to allow users to list all contacts matching a
    certain Tag (case insensitive).
    
    Tests and documentation update to follow.

---
+++ src/main/java/swe/context/logic/commands/FilterCommand.java
@@ -0,0 +1,58 @@
+package swe.context.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import swe.context.commons.util.ToStringBuilder;
+import swe.context.logic.Messages;
+import swe.context.model.Model;
+import swe.context.model.contact.ContainsTagPredicate;
+
+/**
+ * Filters and lists {@link Contact}s whose tags match the specified
+ * tag in full. Case insensitive.
+ */
+public class FilterCommand extends Command {
+    public static final String COMMAND_WORD = "filter";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Filters and lists all contacts whose tags match the"
+            + " specified tag in full. Case insensitive."
+            + "\nParameters: TAG"
+            + "\nExample: " + COMMAND_WORD + " Friend";
+
+    private final ContainsTagPredicate predicate;
+
+    public FilterCommand(ContainsTagPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.setContactsFilter(predicate);
+        return new CommandResult(
+                String.format(Messages.CONTACTS_LISTED_OVERVIEW, model.getFilteredContactList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FilterCommand)) {
+            return false;
+        }
+
+        FilterCommand otherFilterCommand = (FilterCommand) other;
+        return predicate.equals(otherFilterCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+}
