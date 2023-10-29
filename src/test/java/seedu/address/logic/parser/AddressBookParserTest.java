9738d3d17c1ef0efab4899903470e90c277447af
Alicia Teng Hui Xin <teng.hxa@gmail.com>
2023-10-30 04:58:51 +0800

    add tests to addressbookparser

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -5,17 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.AddApplicantCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Applicant;
+import seedu.address.model.person.ApplicantContainsKeywordsPredicate;
+import seedu.address.model.person.MemberContainsKeywordsPredicate;
 import seedu.address.testutil.ApplicantBuilder;
 import seedu.address.testutil.ApplicantUtil;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AddressBookParserTest {
 
@@ -43,13 +47,25 @@ public class AddressBookParserTest {
         assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
     }
 
-    // TODO: adapt for deleteMember and deleteApplicant
-    //    @Test
-    //    public void parseCommand_delete() throws Exception {
-    //        DeleteCommand command = (DeleteCommand) parser.parseCommand(
-    //                DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
-    //        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
-    //    }
+    @Test
+    public void parseCommand__deleteApplicant() throws Exception {
+        DeleteApplicantCommand commandWord = (DeleteApplicantCommand) parser.parseCommand(
+                DeleteApplicantCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
+        DeleteApplicantCommand commandAlias = (DeleteApplicantCommand) parser.parseCommand(
+                DeleteApplicantCommand.COMMAND_ALIAS + " " + INDEX_FIRST_PERSON.getOneBased());
+            assertEquals(new DeleteApplicantCommand(INDEX_FIRST_PERSON), commandWord);
+            assertEquals(new DeleteApplicantCommand(INDEX_FIRST_PERSON), commandAlias);
+    }
+
+    @Test
+    public void parseCommand_commandWord_deleteMember() throws Exception {
+        DeleteMemberCommand commandWord = (DeleteMemberCommand) parser.parseCommand(
+                DeleteMemberCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
+        DeleteMemberCommand commandAlias = (DeleteMemberCommand) parser.parseCommand(
+                DeleteMemberCommand.COMMAND_ALIAS + " " + INDEX_FIRST_PERSON.getOneBased());
+        assertEquals(new DeleteMemberCommand(INDEX_FIRST_PERSON), commandWord);
+        assertEquals(new DeleteMemberCommand(INDEX_FIRST_PERSON), commandAlias);
+    }
 
     // TODO: adapt for editMember and editApplicant
     //    @Test
@@ -75,6 +91,32 @@ public class AddressBookParserTest {
     //                FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
     //        assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), command);
     //    }
+
+    @Test
+    public void parseCommand_findApplicant() throws Exception {
+        List<String> keywords = Arrays.asList("hello", "world", "wassup");
+        FindApplicantCommand commandWord = (FindApplicantCommand) parser.parseCommand(
+                FindApplicantCommand.COMMAND_WORD + " "
+                        + keywords.stream().collect(Collectors.joining(" ")));
+        FindApplicantCommand commandAlias = (FindApplicantCommand) parser.parseCommand(
+                FindApplicantCommand.COMMAND_ALIAS + " "
+                        + keywords.stream().collect(Collectors.joining(" ")));
+        assertEquals(new FindApplicantCommand(new ApplicantContainsKeywordsPredicate(keywords)), commandWord);
+        assertEquals(new FindApplicantCommand(new ApplicantContainsKeywordsPredicate(keywords)), commandAlias);
+    }
+
+    @Test
+    public void parseCommand_findMember() throws Exception {
+        List<String> keywords = Arrays.asList("hello", "world", "wassup");
+        FindMemberCommand commandWord = (FindMemberCommand) parser.parseCommand(
+                FindMemberCommand.COMMAND_WORD + " "
+                        + keywords.stream().collect(Collectors.joining(" ")));
+        FindMemberCommand commandAlias = (FindMemberCommand) parser.parseCommand(
+                FindMemberCommand.COMMAND_ALIAS + " "
+                        + keywords.stream().collect(Collectors.joining(" ")));
+        assertEquals(new FindMemberCommand(new MemberContainsKeywordsPredicate(keywords)), commandWord);
+        assertEquals(new FindMemberCommand(new MemberContainsKeywordsPredicate(keywords)), commandAlias);
+    }
 
     @Test
     public void parseCommand_help() throws Exception {
