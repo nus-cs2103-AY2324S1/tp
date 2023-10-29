07c7773a9645a528c968d290970e6bf9ced66020
Alicia Teng Hui Xin <teng.hxa@gmail.com>
2023-10-30 05:18:36 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -40,6 +40,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class AddressBookParserTest {
 
     private final AddressBookParser parser = new AddressBookParser();
