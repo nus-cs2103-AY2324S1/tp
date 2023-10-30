65a03e04408b277c1ec71d37561c308070acf95c
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2023-10-30 18:03:30 +0800

    Add styleClass for different status

--- src/main/java/seedu/application/ui/JobCard.java
+++ src/main/java/seedu/application/ui/JobCard.java
@@ -1,10 +1,16 @@
 package seedu.application.ui;
 
+import static seedu.application.model.job.Status.JobStatus.APPROVED;
+import static seedu.application.model.job.Status.JobStatus.PENDING;
+import static seedu.application.model.job.Status.JobStatus.REJECTED;
+import static seedu.application.model.job.Status.JobStatus.TO_BE_SUBMITTED;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.application.model.job.Job;
+import seedu.application.model.job.Status;
 
 /**
  * An UI component that displays information of a {@code Job}.
@@ -46,6 +52,23 @@ public class JobCard extends UiPart<Region> {
         role.setText(job.getRole().description);
         company.setText(job.getCompany().name);
         status.setText(job.getStatus().status);
+        setStatusLabelStyle();
         deadline.setText(job.getDeadline().deadline);
+    }
+
+    private void setStatusLabelStyle() {
+        Status jobStatus = job.getStatus();
+
+        if (jobStatus.equals(TO_BE_SUBMITTED)) {
+            status.getStyleClass().add("to_be_submitted");
+        } else if (jobStatus.equals(PENDING)) {
+            status.getStyleClass().add("pending");
+        } else if (jobStatus.equals(APPROVED)) {
+            status.getStyleClass().add("approved");
+        } else if (jobStatus.equals(REJECTED)) {
+            status.getStyleClass().add("rejected");
+        } else {
+            status.getStyleClass().add("");
+        }
     }
 }
