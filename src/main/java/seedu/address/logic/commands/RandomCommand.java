62ad35199f05ef7646786520fb15c34d3378955e
QianChangru <changru_qian@u.nus.edu>
2023-10-31 03:03:49 +0800

    Implement RandomCommand

--- src/main/java/seedu/address/logic/commands/RandomCommand.java
+++ src/main/java/seedu/address/logic/commands/RandomCommand.java
@@ -3,25 +3,32 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.student.Student;
 
 /**
  * Selects a specific number of students randomly from the students displayed.
  */
 public class RandomCommand extends Command {
     public static final String COMMAND_WORD = "random";
-    public static final String MESSAGE_MARK_SUCCESS = "The following students are selected.";
+    public static final String MESSAGE_MARK_SUCCESS = "The following students are selected.\n";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Selects a specific number of students randomly.\n"
             + "Parameters: INDEX\n"
             + "Example: " + COMMAND_WORD + " 2";
+    public static final String MESSAGE_TOO_MUCH_TO_BE_SELECTED =
+            "The number of students to be selected exceeds that of current students displayed";
     private final Index index;
 
     /**
      * Constructs a RandomCommand object.
      *
-     * @param index of the number of students.
+     * @param index of the number of students to be selected.
      */
     public RandomCommand(Index index) {
         requireAllNonNull(index);
@@ -33,8 +40,29 @@ public class RandomCommand extends Command {
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
+        List<Student> lastShownList = model.getFilteredStudentList();
+        if (this.index.getOneBased() > lastShownList.size()) {
+            throw new CommandException(MESSAGE_TOO_MUCH_TO_BE_SELECTED);
+        }
 
-        return new CommandResult(MESSAGE_MARK_SUCCESS);
+        Random random = new Random();
+        HashSet<Integer> distinctInt = new HashSet<>();
+
+        while (distinctInt.size() < index.getOneBased()) {
+            int i = random.nextInt(lastShownList.size());
+            distinctInt.add(i);
+        }
+
+        Integer[] randomInt = distinctInt.toArray(new Integer[0]);
+
+        String result = MESSAGE_MARK_SUCCESS;
+
+        for (Integer i : randomInt) {
+            Student s = lastShownList.get(i);
+            result += s.getName().toString() + " " + s.getStudentNumber().toString() + "\n";
+        }
+
+        return new CommandResult(result);
     }
 
     @Override
