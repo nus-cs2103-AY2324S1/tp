3fdef25f6c98c2ae27461b148543ccae88165ecc
starrylight99 <e0564873@u.nus.edu>
2023-10-30 15:26:39 +0800

    Added id back to card list

--- src/main/java/seedu/address/ui/EmployeeCard.java
+++ src/main/java/seedu/address/ui/EmployeeCard.java
@@ -31,6 +31,8 @@ public class EmployeeCard extends UiPart<Region> {
     @FXML
     private Label name;
     @FXML
+    private Label id;
+    @FXML
     private FlowPane departments;
 
     /**
@@ -39,6 +41,7 @@ public class EmployeeCard extends UiPart<Region> {
     public EmployeeCard(Employee employee, int displayedIndex) {
         super(FXML);
         this.employee = employee;
+        id.setText(displayedIndex + ". ");
         name.setText(employee.getName().fullName);
         employee.getDepartments().stream()
                 .sorted(Comparator.comparing(department -> department.departmentName))
