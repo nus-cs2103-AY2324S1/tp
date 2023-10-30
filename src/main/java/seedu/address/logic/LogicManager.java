9e48ba6e33f194db91f7059825f3985d95ee3fed
Yu Han Zhang <yuhan.zhang@u.nus.edu>
2023-10-30 22:11:00 +0800

    Expose `Dashboard` instance from `Logic`

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -14,6 +14,7 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.AddressBookParser;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Dashboard;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Person;
@@ -65,6 +66,11 @@ public class LogicManager implements Logic {
     @Override
     public ReadOnlyAddressBook getAddressBook() {
         return model.getAddressBook();
+    }
+
+    @Override
+    public Dashboard getDashboard() {
+        return model.getDashboard();
     }
 
     @Override
