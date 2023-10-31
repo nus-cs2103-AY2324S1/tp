7685e12e620a61e1ab19848d37e3e1428a449708
mamayuan <948665003my@gmail.com>
2023-10-31 19:21:25 +0800

    Refine Messages.java

--- src/main/java/swe/context/logic/Messages.java
+++ src/main/java/swe/context/logic/Messages.java
@@ -12,38 +12,21 @@ package swe.context.logic;
  * populated by calling their associated methods.
  */
 public final class Messages {
-    public static final String INVALID_CONTACT_DISPLAYED_INDEX =
-            "One or more of the contact indices provided are invalid";
-    public static final String CONTACTS_LISTED_OVERVIEW = "%d contacts listed!";
+    // Generic commands
+    public static final String COMMAND_UNKNOWN = "Unknown command.";
     public static final String DUPLICATE_FIELDS =
-            "Multiple values specified for the following single-valued field(s): ";
+        "Multiple values specified for the following single-valued field(s): ";
     public static final String EMPTY_WORD_PARAMETER = "Word parameter cannot be empty";
     public static final String SINGLE_WORD_EXPECTED = "Word parameter should be a single word";
-
-    // Messages associated with various Commands
-    public static final String ADD_COMMAND_SUCCESS = "New contact added: %1$s";
     public static final String COMMAND_DUPLICATE_CONTACT = "This contact is already in your contact list.";
-    public static final String DELETE_COMMAND_SUCCESS = "Deleted Contact(s): " + "\n" + "%1$s";
-    public static final String EDIT_COMMAND_SUCCESS = "Edited Contact: %1$s";
     public static final String EDIT_COMMAND_NOT_EDITED = "At least one field to edit must be provided.";
     public static final String HELP_COMMAND_SHOW_HELP = "Opened help window.";
     public static final String LIST_COMMAND_SUCCESS = "Listed all contacts";
-
-    // Exception message
-    public static final String FILE_OPS_ERROR_FORMAT = "Could not save data due to the following error: %s";
-    public static final String FILE_OPS_PERMISSION_ERROR_FORMAT =
-            "Could not save data to file %s due to insufficient permissions to write to the file or the folder.";
-    public static final String DUPLICATE_CONTACT_EXCEPTION = "Operation would result in duplicate contacts";
-
-    // Messages associated with Storage
-    public static final String FIELD_MISSING = "Contact's %s field is missing.";
-
-    //TODO refine the messages above this line
-
-    // Generic commands
-    public static final String COMMAND_UNKNOWN = "Unknown command.";
-    private static final String UNFORMATTED_COMMAND_INVALID_FORMAT =
-            "Invalid command format.\n%s";
+    private static final String UNFORMATTED_COMMAND_INVALID_FORMAT = "Invalid command format.\n%s";
+    private static final String UNFORMATTED_CONTACTS_LISTED_OVERVIEW = "%d contacts listed!";
+    private static final String UNFORMATTED_ADD_COMMAND_SUCCESS = "New contact added: %1$s";
+    private static final String UNFORMATTED_DELETE_COMMAND_SUCCESS = "Deleted Contact(s): \n%1$s";
+    private static final String UNFORMATTED_EDIT_COMMAND_SUCCESS = "Edited Contact: %1$s";
 
     // Specific commands
     public static final String COMMAND_CLEAR_SUCCESS = "All contacts have been removed!";
@@ -51,6 +34,8 @@ public final class Messages {
 
     // Contacts
     // Messages associated with Attributes constraints
+    public static final String INVALID_CONTACT_DISPLAYED_INDEX =
+        "One or more of the contact indices provided are invalid";
     public static final String NAME_INVALID =
             "Names must be alphanumeric (spaces allowed).";
     public static final String PHONE_INVALID =
@@ -62,6 +47,15 @@ public final class Messages {
 
     // JSON
     public static final String CONVERT_CONTACTS_DUPLICATE = "Encountered duplicate while converting contacts.";
+
+    // Exceptions
+    public static final String DUPLICATE_CONTACT_EXCEPTION = "Operation would result in duplicate contacts";
+    private static final String UNFORMATTED_FILE_OPS_ERROR_FORMAT =
+            "Could not save data due to the following error: %s";
+    private static final String UNFORMATTED_FILE_OPS_PERMISSION_ERROR_FORMAT =
+            "Could not save data to file %s due to insufficient permissions to write to the file or the folder.";
+
+    private static final String UNFORMATTED_FIELD_MISSING = "Contact's %s field is missing.";
 
     /**
      * Returns a formatted message about the command format being invalid, with
@@ -82,6 +76,55 @@ public final class Messages {
             Messages.UNFORMATTED_TAG_INVALID,
             invalidName
         );
+    }
+    
+    /**
+     * Returns a formatted message showing the number of contacts listed.
+     */
+    public static String contactsListedOverview(int count) {
+        return String.format(UNFORMATTED_CONTACTS_LISTED_OVERVIEW, count);
+    }
+
+    /**
+     * Returns a formatted message indicating a successful addition of a contact.
+     */
+    public static String addCommandSuccess(String contactDetails) {
+        return String.format(UNFORMATTED_ADD_COMMAND_SUCCESS, contactDetails);
+    }
+
+    /**
+     * Returns a formatted message indicating successful deletion of contact(s).
+     */
+    public static String deleteCommandSuccess(String contactDetails) {
+        return String.format(UNFORMATTED_DELETE_COMMAND_SUCCESS, contactDetails);
+    }
+
+    /**
+     * Returns a formatted message indicating successful editing of a contact.
+     */
+    public static String editCommandSuccess(String contactDetails) {
+        return String.format(UNFORMATTED_EDIT_COMMAND_SUCCESS, contactDetails);
+    }
+
+    /**
+     * Returns a formatted message indicating a file operation error.
+     */
+    public static String fileOpsErrorFormat(String errorDetails) {
+        return String.format(UNFORMATTED_FILE_OPS_ERROR_FORMAT, errorDetails);
+    }
+
+    /**
+     * Returns a formatted message indicating a file permission error.
+     */
+    public static String fileOpsPermissionErrorFormat(String filePath) {
+        return String.format(UNFORMATTED_FILE_OPS_PERMISSION_ERROR_FORMAT, filePath);
+    }
+
+    /**
+     * Returns a formatted message indicating a missing field in a contact.
+     */
+    public static String fieldMissing(String fieldName) {
+        return String.format(UNFORMATTED_FIELD_MISSING, fieldName);
     }
 
     private Messages() {
