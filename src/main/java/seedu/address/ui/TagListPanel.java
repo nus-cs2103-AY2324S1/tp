18ef97a025972b7f98ed4b62ef278e0bf48f617c
matochichap <ruijia.chan@gmail.com>
2023-10-31 23:05:17 +0800

    Make tags UI look nice

--- src/main/java/seedu/address/ui/TagListPanel.java
+++ src/main/java/seedu/address/ui/TagListPanel.java
@@ -1,12 +1,12 @@
 package seedu.address.ui;
 
-import java.util.Comparator;
 import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.tag.Tag;
@@ -19,29 +19,27 @@ public class TagListPanel extends UiPart<Region> {
     private final Logger logger = LogsCenter.getLogger(TagListPanel.class);
 
     @FXML
-    private FlowPane tags;
+    private ListView<Tag> tagListView;
 
     /**
      * Creates a {@code TagListPanel} with the given {@code ObservableList}.
      */
     public TagListPanel(ObservableList<Tag> tagList) {
         super(FXML);
-        updateTags(tagList);
+        tagListView.setItems(tagList);
+        tagListView.setCellFactory(listView -> new TagListViewCell());
     }
 
-    // TODO: find a way to update the tag list when the tag list changes
-    /**
-     * Updates the tag list.
-     */
-    public void updateTags(ObservableList<Tag> updatedTagList) {
-        tags.getChildren().clear();
-
-        updatedTagList.stream()
-                .sorted(Comparator.comparing(tag -> tag.tagName))
-                .forEach(tag -> {
-                    Label label = new Label(tag.tagName);
-                    label.setStyle("-fx-font-size: 14px;");
-                    tags.getChildren().add(label);
-                });
+    class TagListViewCell extends ListCell<Tag> {
+        @Override
+        protected void updateItem(Tag tag, boolean empty) {
+            super.updateItem(tag, empty);
+            if (empty || tag == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new Label(tag.tagName));
+            }
+        }
     }
 }
