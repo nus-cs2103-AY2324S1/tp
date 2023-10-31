049bf344c721c5c27e5897be76e211e570a662bd
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2023-10-31 15:00:34 +0800

    Change description of findCommand

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -5,7 +5,7 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
-import seedu.address.model.employee.NameContainsKeywordsPredicate;
+import seedu.address.model.employee.EmployeeContainsKeywordsPredicate;
 
 /**
  * Finds and lists all employees in address book whose name contains any of the argument keywords.
@@ -15,14 +15,14 @@ public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all employees whose names contain any of "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all employees where the given fields contain "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Example: " + COMMAND_WORD + " n/alice p/manager";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final EmployeeContainsKeywordsPredicate predicate;
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
+    public FindCommand(EmployeeContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
 
