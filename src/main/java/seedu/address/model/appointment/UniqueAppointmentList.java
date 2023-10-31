b1229b18005b8f077fc21853f97455f1ae1808a2
Sheeepen <chengxi1020@gmail.com>
2023-10-31 23:11:05 +0800

    Add overlap appointment methods

--- src/main/java/seedu/address/model/appointment/UniqueAppointmentList.java
+++ src/main/java/seedu/address/model/appointment/UniqueAppointmentList.java
@@ -136,4 +136,28 @@ public class UniqueAppointmentList implements Iterable<Appointment> {
         }
         return true;
     }
+
+    private boolean appointmentsDoNotOverlap(List<Appointment> appointments) {
+        for (int i = 0; i < appointments.size() - 1; i++) {
+            for (int j = i + 1; j < appointments.size(); j++) {
+                if (hasOverlap(appointments.get(i), appointments.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean hasOverlap(Appointment firstAppt, Appointment secondAppt) {
+        if (!firstAppt.getDate().equals(secondAppt.getDate())) {
+            return false;
+        }
+
+        if (firstAppt.getEndTime().getLocalTime().isAfter(secondAppt.getStartTime().getLocalTime())
+        && secondAppt.getEndTime().getLocalTime().isAfter(firstAppt.getStartTime().getLocalTime())) {
+            return true;
+        }
+
+        return false;
+    }
 }
