2526511691eb867b8b6107f6df6546998fddaa9e
hyc17003 <e0970589@u.nus.edu>
2023-10-31 22:13:22 +0800

    Add JavaDocs for log

--- src/main/java/seedu/address/logic/commands/LogCommand.java
+++ src/main/java/seedu/address/logic/commands/LogCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.LogBook;
@@ -16,7 +15,7 @@ public class LogCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD_ALIAS = "lo";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + "or" + COMMAND_WORD_ALIAS
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " or " + COMMAND_WORD_ALIAS
             + ": Saves the results of the most recent FindCommand to the logger tab.\n"
             + "Example 1: " + COMMAND_WORD + "\n"
             + "Example 2: " + COMMAND_WORD_ALIAS;
@@ -27,6 +26,13 @@ public class LogCommand extends UndoableCommand {
 
     private LogBook logBookBeforeUpdate;
 
+    /**
+     * Executes the LogCommand, saving the results of the most recent FindCommand to the logger tab.
+     *
+     * @param model The current model that contains the data.
+     * @return A CommandResult indicating the success of the operation.
+     * @throws CommandException If there are no results from the most recent FindCommand.
+     */
     @Override
     public CommandResult execute(Model model) throws CommandException {
         // Ensure that there are results from the most recent FindCommand
@@ -40,9 +46,14 @@ public class LogCommand extends UndoableCommand {
 
         model.getLogBook().setPersons(model.getFoundPersonsList());
         return new CommandResult(MESSAGE_SUCCESS);
-
     }
 
+    /**
+     * Undoes the LogCommand, restoring the logger tab to its state before saving the results.
+     *
+     * @param model The current model that contains the data.
+     * @return A CommandResult indicating the success of the undo operation.
+     */
     @Override
     public CommandResult undo(Model model) {
         requireNonNull(model);
