1354ca07ae5755eb4dcea1bc0c36394b06c10b05
Alicia Teng Hui Xin <teng.hxa@gmail.com>
2023-11-01 06:45:00 +0800

    fix checkstyle

--- src/main/java/seedu/address/model/person/fields/InterviewTime.java
+++ src/main/java/seedu/address/model/person/fields/InterviewTime.java
@@ -1,9 +1,9 @@
 package seedu.address.model.person.fields;
 
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
-import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Person's interview time in the address book.
