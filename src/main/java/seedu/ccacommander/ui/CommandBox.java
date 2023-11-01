26f42880a37fdca756863bbb2a2e7a122a0d41e2
ph-nathan <px5099@gmail.com>
2023-11-01 12:28:45 +0800

    Reorganize css file before refactoring to dark and light theme

--- src/main/java/seedu/ccacommander/ui/CommandBox.java
+++ src/main/java/seedu/ccacommander/ui/CommandBox.java
@@ -12,7 +12,7 @@ import seedu.ccacommander.logic.parser.exceptions.ParseException;
  * The UI component that is responsible for receiving user command inputs.
  */
 public class CommandBox extends UiPart<Region> {
-
+    // error style class in extensions.css
     public static final String ERROR_STYLE_CLASS = "error";
     private static final String FXML = "CommandBox.fxml";
 
