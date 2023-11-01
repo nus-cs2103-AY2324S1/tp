d3c108a21746d3aa45a10aaba1785abd8e6fd50d
chonguschonguschongus <chongzhenxi@gmail.com>
2023-11-01 16:43:37 +0800

    Fix test cases

--- src/test/java/seedu/address/model/person/GenderPredicateTest.java
+++ src/test/java/seedu/address/model/person/GenderPredicateTest.java
@@ -9,23 +9,6 @@ import org.junit.jupiter.api.Test;
 import seedu.address.testutil.PatientBuilder;
 
 public class GenderPredicateTest {
-
-    @Test
-    public void testGenderPredicate() {
-        GenderPredicate malePredicate = new GenderPredicate("M");
-        GenderPredicate femalePredicate = new GenderPredicate("F");
-
-        // Test with a person having MALE gender
-        assertTrue(malePredicate.test(new PatientBuilder().withGender("M").build()));
-
-        // Test with a person having FEMALE gender
-        assertTrue(femalePredicate.test(new PatientBuilder().withGender("F").build()));
-
-        // Test with a person having a different gender
-        assertFalse(malePredicate.test(new PatientBuilder().withGender("F").build()));
-        assertFalse(femalePredicate.test(new PatientBuilder().withGender("M").build()));
-    }
-
     @Test
     public void correctGenderPredicate_returnsTrue() {
         GenderPredicate malePredicate = new GenderPredicate("M");
