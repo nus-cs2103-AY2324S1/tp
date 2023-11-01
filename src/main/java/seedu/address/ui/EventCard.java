7b9bd65bebe655ee68c8576b8a42fe6d61201da6
Andre Sim Jing Xiang <Andresim8@gmail.com>
2023-11-01 18:15:52 +0800

    Fix documentation error in EventCard

--- src/main/java/seedu/address/ui/EventCard.java
+++ src/main/java/seedu/address/ui/EventCard.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.Region;
 import seedu.address.model.event.Event;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * An UI component that displays information of a {@code Event}.
  */
 public class EventCard extends UiPart<Region> {
 
