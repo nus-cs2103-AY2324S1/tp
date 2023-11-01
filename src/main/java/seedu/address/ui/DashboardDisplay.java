95c3484384d8facc38d7e80cbc2f3aecda771492
Yu Han Zhang <yuhan.zhang@u.nus.edu>
2023-11-01 16:56:13 +0800

    Add more stats to dashboard

--- src/main/java/seedu/address/ui/DashboardDisplay.java
+++ src/main/java/seedu/address/ui/DashboardDisplay.java
@@ -3,9 +3,15 @@ package seedu.address.ui;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import seedu.address.model.Dashboard;
+import seedu.address.model.person.lead.LeadType;
 
 /**
  * The Dashboard Display.
@@ -14,7 +20,21 @@ public class DashboardDisplay extends UiPart<Region> {
     private static final String FXML = "DashboardDisplay.fxml";
 
     @FXML
+    private Label interactionsPerClient;
+    @FXML
+    private Label uncontacted;
+    @FXML
+    private Label contacting;
+    @FXML
+    private Label closed;
+    @FXML
     private PieChart pieChart;
+    @FXML
+    private BarChart<String, Integer> barChart;
+    @FXML
+    private CategoryAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
 
     /**
      * Creates a {@code DashboardDisplay} using data from the given {@code dashboard}.
@@ -22,12 +42,46 @@ public class DashboardDisplay extends UiPart<Region> {
     public DashboardDisplay(Dashboard dashboard) {
         super(FXML);
 
+        // stats
+        interactionsPerClient.setText(String.valueOf(dashboard.averageInteractionsPerClient()));
+        uncontacted.setText(String.valueOf(dashboard.getNumUncontactedClients()));
+        contacting.setText(String.valueOf(dashboard.getNumContactingClients()));
+        closed.setText(String.valueOf(dashboard.getNumClosedClients()));
+
+        // pie chart
+        double closed = dashboard.closedPercentage();
         double interested = dashboard.interestedPercentage();
         double notInterested = dashboard.notInterestedPercentage();
+        double followUpRequired = dashboard.followUpRequiredPercentage();
+        double unknown = dashboard.unknownPercentage();
 
         ObservableList<PieChart.Data> data = FXCollections.observableArrayList(
-                new PieChart.Data("Interested", interested), new PieChart.Data("Not Interested", notInterested));
+                new PieChart.Data("Interested", interested), new PieChart.Data("Not Interested", notInterested),
+                new PieChart.Data("Follow Up Required", followUpRequired), new PieChart.Data("Closed", closed),
+                new PieChart.Data("Unknown", unknown));
 
         pieChart.setData(data);
+
+        // bar chart
+        int totalHotLeads = dashboard.getTotalHotLeads();
+        int totalWarmLeads = dashboard.getTotalWarmLeads();
+        int totalColdLeads = dashboard.getTotalColdLeads();
+
+        XYChart.Series<String, Integer> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>(LeadType.HOT.toString(), totalHotLeads));
+        series.getData().add(new XYChart.Data<>(LeadType.WARM.toString(), totalWarmLeads));
+        series.getData().add(new XYChart.Data<>(LeadType.COLD.toString(), totalColdLeads));
+
+        barChart.getData().add(series);
+
+        // set axis
+        double maxY = Math.ceil(1.25 * Math.max(totalHotLeads, Math.max(totalWarmLeads, totalColdLeads)));
+
+        xAxis.setLabel("Leads");
+        yAxis.setLabel("Number of clients");
+        yAxis.setMinorTickVisible(false);
+        yAxis.setTickUnit(1);
+        yAxis.setAutoRanging(false);
+        yAxis.setUpperBound(maxY);
     }
 }
