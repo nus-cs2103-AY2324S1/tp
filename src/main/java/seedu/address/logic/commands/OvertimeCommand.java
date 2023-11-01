dc79cb35fd7dfce715bca4f74a75d97fc699a1f2
s0ngyang <keesongyang4@gmail.com>
2023-11-01 14:30:54 +0800

    Update OvertimeCommand to take in operation

--- src/main/java/seedu/address/logic/commands/OvertimeCommand.java
+++ src/main/java/seedu/address/logic/commands/OvertimeCommand.java
@@ -1,17 +1,19 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_AMOUNT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_OPERATION;
 
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.commands.EditCommand.EditEmployeeDescriptor;
 import seedu.address.model.Model;
 import seedu.address.model.employee.Employee;
 import seedu.address.model.employee.Id;
 import seedu.address.model.employee.OvertimeHours;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Increases the overtime hours of an employee.
@@ -20,7 +22,7 @@ public class OvertimeCommand extends Command{
     public static final String COMMAND_WORD = "overtime";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Increases the overtime hours of an employee by the specified amount.\n"
+            + ": Increases/Decreases the overtime hours of an employee by the specified amount.\n"
             + "Parameters: " + PREFIX_ID + "ID (must be in the exact EID format) "
             + PREFIX_OPERATION + "OPERATION "
             + PREFIX_AMOUNT + "AMOUNT (must be a positive integer)\n"
@@ -30,21 +32,22 @@ public class OvertimeCommand extends Command{
             + PREFIX_AMOUNT + "2";
 
     public static final String MESSAGE_OVERTIME_EMPLOYEE_SUCCESS = "Overtime hours of employee %1$s increased by %2$s";
-
+    public static final String MISSING_OPERATION_AMOUNT = "Operation and amount must be specified";
     private final Id targetId;
+    private final int hoursToEdit;
+    private final String operation;
 
-    private final OvertimeHours overtimeHours;
-
-    public OvertimeCommand(Id targetId, OvertimeHours overtimeHours) {
+    public OvertimeCommand(Id targetId, int hoursToEdit, String operation) {
         this.targetId = targetId;
-        this.overtimeHours = overtimeHours;
+        this.hoursToEdit = hoursToEdit;
+        this.operation = operation;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        if (overtimeHours.value <= 0) {
+        if (hoursToEdit <= 0) {
             throw new CommandException(Messages.MESSAGE_INVALID_OVERTIME_HOURS);
         }
 
@@ -53,17 +56,22 @@ public class OvertimeCommand extends Command{
                 .findFirst().orElse(null);
         
         if (employeeToEdit != null) {
-            Employee editedEmployee = getEditedEmployee(employeeToEdit);
+            Employee editedEmployee = editEmployeeOvertime(employeeToEdit);
             model.setEmployee(employeeToEdit, editedEmployee);
             return new CommandResult(String.format(MESSAGE_OVERTIME_EMPLOYEE_SUCCESS, 
-                    Messages.format(editedEmployee), overtimeHours));
+                    Messages.format(editedEmployee), hoursToEdit));
         }
         throw new CommandException(Messages.MESSAGE_INVALID_EMPLOYEE_DISPLAYED_ID);
     }
 
-    private Employee getEditedEmployee(Employee employeeToEdit) {
-        OvertimeHours updatedOvertimeHours = new OvertimeHours(
-                employeeToEdit.getOvertimeHours().value - overtimeHours.value);
+    private Employee editEmployeeOvertime(Employee employeeToEdit) {
+        int updatedHours = 0;
+        if (Objects.equals(operation, "inc")) {
+            updatedHours = employeeToEdit.getOvertimeHours().value + hoursToEdit;
+        } else if (Objects.equals(operation, "dec")) {
+            updatedHours = employeeToEdit.getOvertimeHours().value - hoursToEdit;
+        }
+        OvertimeHours updatedOvertimeHours = new OvertimeHours(updatedHours);
         return new Employee(employeeToEdit.getName(), employeeToEdit.getPosition(),
                 employeeToEdit.getId(), employeeToEdit.getPhone(), employeeToEdit.getEmail(),
                 employeeToEdit.getSalary(), employeeToEdit.getDepartments(), updatedOvertimeHours,
--- src/main/java/seedu/address/logic/parser/OvertimeCommandParser.java
+++ src/main/java/seedu/address/logic/parser/OvertimeCommandParser.java
@@ -3,37 +3,39 @@ package seedu.address.logic.parser;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.OvertimeCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.employee.OvertimeHours;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_EMPLOYEE_DISPLAYED_INDEX;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 public class OvertimeCommandParser implements Parser<OvertimeCommand>{
     public OvertimeCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_ID, PREFIX_OVERTIME_HOURS);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_ID, PREFIX_OPERATION, PREFIX_AMOUNT);
 
         Index index;
 
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, OvertimeCommand.MESSAGE_USAGE), pe);
+            throw new ParseException(String.format(MESSAGE_INVALID_EMPLOYEE_DISPLAYED_INDEX, OvertimeCommand.MESSAGE_USAGE), pe);
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_ID, PREFIX_OVERTIME_HOURS);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_ID, PREFIX_OPERATION, PREFIX_AMOUNT);
 
-        OvertimeCommand.OvertimeCommandDescriptor overtimeCommandDescriptor = new OvertimeCommand.OvertimeCommandDescriptor();
+        if (argMultimap.getValue(PREFIX_OPERATION).isEmpty() || argMultimap.getValue(PREFIX_AMOUNT).isEmpty()) {
+            throw new ParseException(OvertimeCommand.MISSING_OPERATION_AMOUNT);
+        }
+
+
+
+        OvertimeHours overtimeHours = new OvertimeHours();
 
         if (argMultimap.getValue(PREFIX_ID).isPresent()) {
             overtimeCommandDescriptor.setId(ParserUtil.parseId(argMultimap.getValue(PREFIX_ID).get()));
         }
-        if (argMultimap.getValue(PREFIX_OVERTIME_HOURS).isPresent()) {
-            overtimeCommandDescriptor.setOvertimeHours(ParserUtil.parseOvertimeHours(argMultimap.getValue(PREFIX_OVERTIME_HOURS).get()));
-        }
 
-        if (!overtimeCommandDescriptor.isAnyFieldEdited()) {
-            throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
-        }
-
-        return new OvertimeCommand(index, overtimeCommandDescriptor);
+        return new OvertimeCommand(index, overtimeHours, order);
     }
 }
