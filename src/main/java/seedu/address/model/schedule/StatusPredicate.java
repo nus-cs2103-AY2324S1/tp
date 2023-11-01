cfbfe23300d97c5a6296e0b83353c27e80b9ef2e
Xuan Yi <saltedfishxx@gmail.com>
2023-11-01 17:19:17 +0800

    Rename javadoc for Status Predicate

--- src/main/java/seedu/address/model/schedule/StatusPredicate.java
+++ src/main/java/seedu/address/model/schedule/StatusPredicate.java
@@ -8,7 +8,7 @@ import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.person.Person;
 
 /**
- * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code Schedule}'s {@code Status} matches any of the keywords given.
  */
 public class StatusPredicate implements Predicate<Schedule> {
     private final List<String> status;
