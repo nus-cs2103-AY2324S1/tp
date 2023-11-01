62f11fad9ab5ab363776f6c18d3cafc328d9d42d
nicrandomlee <leejpnicholas@gmail.com>
2023-11-01 22:53:37 +0800

    Update Parser

--- src/main/java/seedu/address/logic/parser/UniMateParser.java
+++ src/main/java/seedu/address/logic/parser/UniMateParser.java
@@ -17,6 +17,7 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.DeleteEventCommand;
 import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.EditContactEventCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FilterCommand;
 import seedu.address.logic.commands.FindCommand;
@@ -100,6 +101,9 @@ public class UniMateParser {
 
         case AddContactEventCommand.COMMAND_WORD:
             return new AddContactEventCommandParser().parse(arguments);
+
+        case EditContactEventCommand.COMMAND_WORD:
+            return new EditContactEventCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
