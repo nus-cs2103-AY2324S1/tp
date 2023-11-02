cb2f8f7df0aefd7ba5e1e2d4dd6fabde22748457
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2023-11-02 20:23:30 +0800

    Adjust test case to include 1 in json file

--- src/test/java/seedu/address/logic/commands/SolveCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SolveCommandTest.java
@@ -88,7 +88,7 @@ public class SolveCommandTest {
 
         assertCommandSuccess(practiseCommand, model, expectedMessage, expectedModel);
 
-        assertTrue(targetCard.getSolveCount().equals(1));
+        assertTrue(targetCard.getSolveCount().equals(2));
     }
 
     @Test
