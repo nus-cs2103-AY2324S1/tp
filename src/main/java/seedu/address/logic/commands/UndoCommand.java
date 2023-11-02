5ec5349868a456dff698366af164b0e57118ff0b
TyrusLye <Lyetyrus@gmail.com>
2023-11-02 12:57:53 +0800

    Undo will now inform if there is no more undo history. Also fixed issue with reset text formatting

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -9,10 +9,14 @@ public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
     public static final String MESSAGE_SUCCESS = "Undo Completed";
     public static final String MESSAGE_USAGE = "This command will undo the last command";
+    public static final String MESSAGE_UNDO_DONE ="No more undo history found!";
 
 
     @Override
     public CommandResult execute(Model model) {
+        if (model == null) {
+            return new CommandResult(MESSAGE_UNDO_DONE);
+        }
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
