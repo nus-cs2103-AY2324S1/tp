f46c8489ab2e4b0a2bdffe023be2c5d7bc5329a5
kokseng1 <ngkokseng02@gmail.com>
2023-11-02 22:01:29 +0800

    Removed irrelevant comment

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -41,7 +41,6 @@ public class LogicManager implements Logic {
      * Constructs a {@code LogicManager} with the given {@code Model} and {@code Storage}.
      */
     public LogicManager(Model model, Storage storage) {
-        //this.commandHistory = new CommandHistory();
         this.model = model;
         this.storage = storage;
         addressBookParser = new AddressBookParser(model);
