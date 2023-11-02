f5972e3d3a4444893b12fc40ae5998f89e089cc6
D-Limiter <>
2023-11-03 04:17:34 +0800

    Fix EOF Checkstyle issues

--- src/test/java/seedu/flashlingo/logic/parser/CliSyntax.java
+++ src/test/java/seedu/flashlingo/logic/parser/CliSyntax.java
@@ -11,6 +11,4 @@ public class CliSyntax {
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
     public static final Prefix PREFIX_TRANSLATED_WORD = new Prefix("t/");
     public static final Prefix PREFIX_TRANSLATED_WORD_LANGUAGE = new Prefix("tl/");
-
-
 }
