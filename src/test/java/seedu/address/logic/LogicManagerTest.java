df14ce1cf23b222a853860d8533155163f0bb6df
matochichap <ruijia.chan@gmail.com>
2023-11-02 17:28:51 +0800

    Remove unnecessary comments

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -178,17 +178,11 @@ public class LogicManagerTest {
         logic = new LogicManager(model, storage);
 
         // Triggers the saveAddressBook method by executing an add command
-        //        String addCommand = AddCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY
-        //                + EMAIL_DESC_AMY + ADDRESS_DESC_AMY;
-        //        Person expectedPerson = new PersonBuilder(AMY).withTags().build();
 
         String addApplicantCommand = AddApplicantCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY;
         Applicant expectedApplicant = new ApplicantBuilder(AMY_APPLICANT).build();
 
         ModelManager expectedModel = new ModelManager();
-
-        //        expectedModel.addPerson(expectedPerson);
-        //        assertCommandFailure(addCommand, CommandException.class, expectedMessage, expectedModel);
 
         expectedModel.addApplicant(expectedApplicant);
         assertCommandFailure(addApplicantCommand, CommandException.class, expectedMessage, expectedModel);
