c06ea96a24b6e0e72fbb511b18707f986baa7c5d
kokrui <kokruiwong@gmail.com>
2023-11-03 00:31:57 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/parser/FindExpressionParser.java
+++ src/main/java/seedu/address/logic/parser/FindExpressionParser.java
@@ -12,7 +12,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SECONDARY_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM;
-import static seedu.address.logic.parser.FindFilterStringTokenizer.VALID_CONDITION;
 
 import java.util.List;
 import java.util.function.Predicate;
