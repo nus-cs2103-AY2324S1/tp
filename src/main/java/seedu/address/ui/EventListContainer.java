2c4d1b64735c89abada6198160e3bb003efa05cb
Andre Sim Jing Xiang <Andresim8@gmail.com>
2023-11-02 23:32:36 +0800

    Create UI elements for ContactEventList

---
+++ src/main/java/seedu/address/ui/EventListContainer.java
@@ -0,0 +1,32 @@
+package seedu.address.ui;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import seedu.address.model.event.Event;
+
+/**
+ * A pop-up window that shows an event list of a specific person in the addressbook.
+ */
+public class EventListContainer extends  UiPart<Region> {
+    private static final String FXML = "EventListContainer.fxml";
+
+    @FXML
+    private StackPane eventListPlaceholder;
+
+    private final EventListPanel eventListPanel;
+
+    public EventListContainer(ObservableList<Event> eventList) {
+        super(FXML);
+        this.eventListPanel = new EventListPanel(eventList);
+        fillInnerPart();
+    }
+
+    /**
+     * Fills up the placeholder of this window
+     */
+    void fillInnerPart() {
+        eventListPlaceholder.getChildren().add(eventListPanel.getRoot());
+    }
+}
