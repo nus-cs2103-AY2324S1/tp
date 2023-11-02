4826aa608608a666bcbc6220095b0fb17945e767
Lim Ngee Yong <e0940681@u.nus.edu>
2023-11-02 17:06:43 +0800

    Fix checkstyle

--- src/main/java/seedu/classmanager/model/student/StudentNumber.java
+++ src/main/java/seedu/classmanager/model/student/StudentNumber.java
@@ -9,8 +9,8 @@ import static seedu.classmanager.commons.util.AppUtil.checkArgument;
  */
 public class StudentNumber {
 
-    public static final String MESSAGE_CONSTRAINTS = "Student Number can take any value starting with capital 'A'," +
-            "followed by numbers, and ending with a single alphabet.";
+    public static final String MESSAGE_CONSTRAINTS = "Student Number can take any value starting with capital 'A',"
+            + "followed by numbers, and ending with a single alphabet.";
 
     /*
      * The first character of the student number needs to be "A".
