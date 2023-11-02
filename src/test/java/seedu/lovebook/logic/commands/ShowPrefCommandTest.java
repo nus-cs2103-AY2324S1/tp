a740d717ce172e31e03d322159c729bbf948b872
ryam <ryanngohenrui@gmail.com>
2023-11-03 01:51:20 +0800

    Add test for ShowPrefCommandTest

--- src/test/java/seedu/lovebook/logic/commands/ShowPrefCommandTest.java
+++ src/test/java/seedu/lovebook/logic/commands/ShowPrefCommandTest.java
@@ -1,18 +1,8 @@
 package seedu.lovebook.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static seedu.lovebook.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.lovebook.logic.commands.CommandTestUtil.showPersonAtIndex;
-import static seedu.lovebook.testutil.TypicalDatePrefs.getTypicalDatePrefs;
-import static seedu.lovebook.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.lovebook.testutil.TypicalPersons.getTypicalLoveBook;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.lovebook.model.Model;
-import seedu.lovebook.model.ModelManager;
-import seedu.lovebook.model.UserPrefs;
 
 class ShowPrefCommandTest {
     private Model model;
