a929f398b309b54ad7a87cac3ba97995b764dfcd
He Yifan <e0968838@u.nus.edu>
2023-11-02 16:22:03 +0800

    Update error message format

--- src/main/java/seedu/address/logic/commands/MarkCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkCommand.java
@@ -36,13 +36,13 @@ public class MarkCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Marks the employee's attendance identified by the index number used in the displayed employee list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Parameters: INDEX (must be a positive integer) /at ATTENDANCE_TYPE\n"
+            + "Example: " + COMMAND_WORD + " 1 /at present";
 
     public static final String MESSAGE_USAGE_FOR_NAME = COMMAND_WORD
             + ": Marks the employee's attendance identified by the name used in the displayed employee list.\n"
-            + "Parameters: n/NAME (must be present)\n"
-            + "Example: " + COMMAND_WORD + " n/John";
+            + "Parameters: /n NAME (must be present) /at ATTENDANCE_TYPE\n"
+            + "Example: " + COMMAND_WORD + " /n John /at absent";
     public static final String MESSAGE_MARK_PERSON_SUCCESS = "Marked Employee: %2$s as %1$s";
 
     public static final String MESSAGE_PERSON_ON_LEAVE = "The employee that you are trying to mark is on leave today!";
