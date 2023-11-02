c9c0ecf6934d1d6a137ffc1b84d126002942cf52
Andre Sim Jing Xiang <Andresim8@gmail.com>
2023-11-02 23:40:24 +0800

    Create new constructor for CommandResult

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -4,12 +4,14 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.Objects;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
  * Represents the result of a command execution.
  */
 public class CommandResult {
+    private static final int INVALID_INDEX = -1;
 
     private final String feedbackToUser;
 
@@ -22,6 +24,8 @@ public class CommandResult {
     /** The application should show the task list in the bottom list slot. */
     private final boolean switchBottomList;
 
+    private final Index eventViewIndex;
+
     /**
      * Constructs a {@code CommandResult} with the specified fields.
      */
@@ -30,6 +34,7 @@ public class CommandResult {
         this.showHelp = showHelp;
         this.exit = exit;
         this.switchBottomList = switchBottomList;
+        this.eventViewIndex = Index.fromOneBased(INVALID_INDEX);
     }
 
     /**
@@ -38,6 +43,18 @@ public class CommandResult {
      */
     public CommandResult(String feedbackToUser) {
         this(feedbackToUser, false, false, false);
+    }
+
+    /**
+     * Constructs a {@code CommandResult} with the specified {@code feedbackToUser},
+     * {@code eventViewIndex}, and other fields set to their default value
+     */
+    public CommandResult(String feedbackToUser, Index eventViewIndex) {
+        this.feedbackToUser = feedbackToUser;
+        this.showHelp = false;
+        this.exit = false;
+        this.switchBottomList = false;
+        this.eventViewIndex = eventViewIndex;
     }
 
     public String getFeedbackToUser() {
