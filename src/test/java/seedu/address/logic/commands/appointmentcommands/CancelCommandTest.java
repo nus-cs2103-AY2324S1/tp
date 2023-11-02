61ac4c059148c17db05e9afda3ea6e554ced1a35
trgao <112098096+trgao@users.noreply.github.com>
2023-11-02 15:05:28 +0800

    Add triage command tests

--- src/test/java/seedu/address/logic/commands/appointmentcommands/CancelCommandTest.java
+++ src/test/java/seedu/address/logic/commands/appointmentcommands/CancelCommandTest.java
@@ -20,7 +20,7 @@ import seedu.address.model.appointment.Appointment;
 
 public class CancelCommandTest {
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void execute_validIndexUnfilteredList_success() {
