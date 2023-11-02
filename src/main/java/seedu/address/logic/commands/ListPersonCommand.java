993b7806309c9323b6cb429ad8bab51fdd0ccc57
Andrew Janong <andrewjanong@gmail.com>
2023-11-02 17:02:28 +0800

    Changes for functions that require id
    Now, the functions that has an id argument will apply to the ids of the contacts with the current filter.

--- src/main/java/seedu/address/logic/commands/ListPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/ListPersonCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.Set;
@@ -12,11 +13,18 @@ import seedu.address.model.tag.Tag;
  * The command handler for {@code list contact} command
  */
 public class ListPersonCommand extends ListCommand {
-    public static final String MESSAGE_SUCCESS = "Listed all contacts";
+    public static final String MESSAGE_SUCCESS = "Listed contacts";
     public static final String SECONDARY_COMMAND_WORD = "contact";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + SECONDARY_COMMAND_WORD
-            + ": Lists all contacts in the address book";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " "
+            + SECONDARY_COMMAND_WORD
+            + ": Lists contacts in the address book."
+            + "Parameters: "
+            + PREFIX_TAG + " TAGNAME...\n"
+            + "Tag argument may be empty."
+            + "Example: \n"
+            + "- " + COMMAND_WORD + " " + SECONDARY_COMMAND_WORD + "\n"
+            + "- " + COMMAND_WORD + " " + SECONDARY_COMMAND_WORD + " " + PREFIX_TAG + " recruiter";
 
     private final Set<Tag> tags;
 
