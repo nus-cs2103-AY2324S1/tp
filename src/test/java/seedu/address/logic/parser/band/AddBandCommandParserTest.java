5fe79c1cd4baa2759d62bf15872ed97700361fff
Shu Yang <shuyangk@gmail.com>
2023-11-02 22:18:49 +0800

    Update test cases to include success cases

--- src/test/java/seedu/address/logic/parser/band/AddBandCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/band/AddBandCommandParserTest.java
@@ -1,26 +1,47 @@
 package seedu.address.logic.parser.band;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.GENRE_DESC_POP;
 import static seedu.address.logic.commands.CommandTestUtil.GENRE_DESC_ROCK;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_GENRE_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_ACE;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_BOOM;
 import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
+import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GENRE_POP;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GENRE_ROCK;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOOM;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.typicalentities.TypicalBands.ACE;
+import static seedu.address.testutil.typicalentities.TypicalBands.BOOM;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.band.AddBandCommand;
+import seedu.address.model.band.Band;
 import seedu.address.model.band.BandName;
 import seedu.address.model.tag.Genre;
+import seedu.address.testutil.BandBuilder;
 
 public class AddBandCommandParserTest {
     private AddBandCommandParser parser = new AddBandCommandParser();
 
+    @Test
+    public void parse_allFieldsPresent_success() {
+        Band expectedBand = new BandBuilder(BOOM).withGenres(VALID_GENRE_ROCK).build();
+
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOOM + GENRE_DESC_ROCK,
+                new AddBandCommand(expectedBand));
+
+        Band expectedBandMultipleGenres = new BandBuilder(BOOM).withGenres(VALID_GENRE_ROCK, VALID_GENRE_POP).build();
+
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOOM + GENRE_DESC_ROCK + GENRE_DESC_POP,
+                new AddBandCommand(expectedBandMultipleGenres));
+    }
     @Test
     public void parse_repeatedNonTagValue_failure() {
         String validExpectedBandString = NAME_DESC_BOOM + GENRE_DESC_ROCK;
@@ -50,6 +71,11 @@ public class AddBandCommandParserTest {
 
     }
 
+    @Test
+    public void parse_optionalFieldsMissing_success() {
+        Band expectedBand = new BandBuilder(ACE).withGenres().build();
+        assertParseSuccess(parser, NAME_DESC_ACE, new AddBandCommand(expectedBand));
+    }
     @Test
     public void parse_compulsoryFieldMissing_failure() {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddBandCommand.MESSAGE_USAGE);
