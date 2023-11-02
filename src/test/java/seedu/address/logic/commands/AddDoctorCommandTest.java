84864757d8042a8425f4eb3bb3561fadd8fb1aab
chonguschonguschongus <chongzhenxi@gmail.com>
2023-11-02 11:06:36 +0800

    Add DeleteAppointment command

--- src/test/java/seedu/address/logic/commands/AddDoctorCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddDoctorCommandTest.java
@@ -128,6 +128,11 @@ public class AddDoctorCommandTest {
         }
 
         @Override
+        public void deleteAppointment(Appointment appointment) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void setAddressBook(ReadOnlyAddressBook newData) {
             throw new AssertionError("This method should not be called.");
         }
