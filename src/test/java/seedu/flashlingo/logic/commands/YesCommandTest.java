29339307ae5ee1624d834fe03e4f9f3ed77c9e51
Cheng Wang <e1088136@u.nus.edu>
2023-11-02 19:11:07 +0800

    Fix checkstyle

--- src/test/java/seedu/flashlingo/logic/commands/YesCommandTest.java
+++ src/test/java/seedu/flashlingo/logic/commands/YesCommandTest.java
@@ -1,19 +1,18 @@
 package seedu.flashlingo.logic.commands;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
+import static seedu.flashlingo.testutil.TypicalFlashCards.getTypicalFlashlingoWithOneFlashCard;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import seedu.flashlingo.model.Model;
 import seedu.flashlingo.model.ModelManager;
 import seedu.flashlingo.model.UserPrefs;
 import seedu.flashlingo.model.flashcard.FlashCard;
 import seedu.flashlingo.model.flashcard.ProficiencyLevel;
-
-import static seedu.flashlingo.testutil.TypicalFlashCards.getTypicalFlashlingoWithOneFlashCard;
 
 public class YesCommandTest {
     private Model model = new ModelManager();
