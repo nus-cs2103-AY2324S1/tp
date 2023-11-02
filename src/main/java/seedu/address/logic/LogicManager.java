dd809d97a23b018a2ead65e4f0410f42ef33ffdf
Ken-Lai <weizhong9201@gmail.com>
2023-11-03 01:40:28 +0800

    Fix bugs
    Fixed when a group is assigned under an event, the person under that
    group is not shown in event when the person list does not show the
    person

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -72,7 +72,12 @@ public class LogicManager implements Logic {
     }
 
     @Override
-    public ObservableList<Event> getEventList() {
+    public ObservableList<Person> getFullPersonList() {
+        return model.getFullPersonList();
+    }
+
+    @Override
+    public ObservableList<Event> getFilteredEventList() {
         return model.getFilteredEventList();
     }
 
