74c578eeb79c41509a7ebae261529cd5186210b5
Cheng Wang <e1088136@u.nus.edu>
2023-11-03 15:04:59 +0800

    Upload CommandTestUtil

--- src/test/java/seedu/flashlingo/testutil/TypicalFlashCards.java
+++ src/test/java/seedu/flashlingo/testutil/TypicalFlashCards.java
@@ -11,6 +11,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import seedu.flashlingo.logic.commands.CommandTestUtil;
 import seedu.flashlingo.model.Flashlingo;
 import seedu.flashlingo.model.flashcard.FlashCard;
 
