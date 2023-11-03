e499e95cc08e66e09898ac112761b0bb949b01ff
He Yifan <e0968838@u.nus.edu>
2023-11-03 23:07:07 +0800

    Add testcases for payslip command parser

--- src/test/java/seedu/address/logic/parser/PayslipCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/PayslipCommandParserTest.java
@@ -3,21 +3,27 @@ package seedu.address.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.PayslipCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 class PayslipCommandParserTest {
 
-    private PayslipCommandParser parser = new PayslipCommandParser();
+    private final PayslipCommandParser parser = new PayslipCommandParser();
 
     @Test
-    public void parse() throws Exception {
+    public void parse_nullArgs_throwsNullPointerException() throws Exception {
         // null -> throws NullPointerException
         assertThrows(NullPointerException.class, () -> parser.parse(null));
+    }
 
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
         // invalid input -> throws ParseException
         assertThrows(ParseException.class, () -> parser.parse("a")); // non-numeric
         assertThrows(ParseException.class, () -> parser.parse("1.1")); // non-integer
@@ -28,7 +34,23 @@ class PayslipCommandParserTest {
         assertThrows(ParseException.class, () -> parser.parse("")); // empty string
         assertThrows(ParseException.class, () -> parser.parse(" ")); // whitespace
 
+        // index and name both not present
+        assertThrows(ParseException.class, () -> parser.parse(" /t 01/01/2020"));
+
+        // invalid date format
+        assertThrows(ParseException.class, () -> parser.parse(" 1 /t 01-01-2020"));
+    }
+
+    @Test
+    public void parse_validArgs_returnsPayslipCommand() throws Exception {
         // valid input -> returns PayslipCommand
         assertEquals(new PayslipCommand(Index.fromZeroBased(0)), parser.parse("1"));
+        assertEquals(new PayslipCommand(new NameContainsKeywordsPredicate(Collections.singletonList("Alex"))),
+            parser.parse(" /n Alex"));
+        assertEquals(new PayslipCommand(Index.fromZeroBased(0), ParserUtil.stringToDate("01/01/2020")),
+            parser.parse(" 1 /t 01/01/2020"));
+        assertEquals(new PayslipCommand(new NameContainsKeywordsPredicate(Collections.singletonList("Alex")),
+            ParserUtil.stringToDate("01/01/2020")),
+            parser.parse(" /n Alex /t 01/01/2020"));
     }
 }
