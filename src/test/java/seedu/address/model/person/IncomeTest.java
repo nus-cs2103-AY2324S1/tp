a5b26896bd7da14b37621a9efb79bf9af71b48ab
Dick Ong <dickongwd@gmail.com>
2023-11-04 18:30:06 +0800

    Add tests for isValidIncome

---
+++ src/test/java/seedu/address/model/person/IncomeTest.java
@@ -0,0 +1,39 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class IncomeTest {
+
+    // EP: valid income (integers)
+    @Test
+    public void isValidIncome_validIncome_returnsTrue() {
+        assertTrue(Income.isValidIncome("60000"));
+        assertTrue(Income.isValidIncome("100"));
+    }
+
+    // EP: null input
+    @Test
+    public void isValidIncome_nullInput_returnsFalse() {
+        assertFalse(Income.isValidIncome(null));
+    }
+
+    // EP: empty string
+    @Test
+    public void isValidIncome_emptyInput_returnsFalse() {
+        assertFalse(Income.isValidIncome(("")));
+        assertFalse(Income.isValidIncome((" ")));
+        assertFalse(Income.isValidIncome(("       ")));
+    }
+
+    // EP: invalid inputs (non-integers)
+    @Test
+    public void isValidIncome_invalidInput_returnsFalse() {
+        assertFalse(Income.isValidIncome(("100 USD")));
+        assertFalse(Income.isValidIncome(("123.50")));
+        assertFalse(Income.isValidIncome(("$60000")));
+    }
+}
