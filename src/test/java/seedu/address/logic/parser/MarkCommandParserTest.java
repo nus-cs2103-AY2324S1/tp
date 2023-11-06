b3f7bd0aaa77475a299f36ffb0115a487c5a38e9
Joey Lee <joeylee@comp.nus.edu>
2023-11-07 00:58:02 +0800

    Resolve bugs with testcases and with error messages

--- src/test/java/seedu/address/logic/parser/MarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/MarkCommandParserTest.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.ATTENDANCE_TYPE_DESC_ABSENT;
+import static seedu.address.logic.commands.CommandTestUtil.ATTENDANCE_TYPE_DESC_LATE;
+import static seedu.address.logic.commands.CommandTestUtil.ATTENDANCE_TYPE_DESC_PRESENT;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_BOB;
@@ -14,7 +17,6 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.Messages;
-import seedu.address.logic.commands.AttendanceCommand;
 import seedu.address.logic.commands.MarkCommand;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
@@ -28,19 +30,19 @@ public class MarkCommandParserTest {
     @Test
     public void parse_namePresent_validType_success() {
         // valid name, mark as absent
-        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB,
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB + ATTENDANCE_TYPE_DESC_ABSENT,
                 new MarkCommand(
                         new NameContainsKeywordsPredicate(Arrays.asList(VALID_NAME_BOB.split("\\s+"))),
                         AttendanceType.ABSENT));
 
         // valid name, mark as late
-        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB,
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB + ATTENDANCE_TYPE_DESC_LATE,
                 new MarkCommand(
                         new NameContainsKeywordsPredicate(Arrays.asList(VALID_NAME_BOB.split("\\s+"))),
                         AttendanceType.LATE));
 
         // valid name, mark as present
-        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB,
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB + ATTENDANCE_TYPE_DESC_PRESENT,
                 new MarkCommand(
                         new NameContainsKeywordsPredicate(Arrays.asList(VALID_NAME_BOB.split("\\s+"))),
                         AttendanceType.PRESENT));
@@ -49,19 +51,19 @@ public class MarkCommandParserTest {
     @Test
     public void parse_indexPresent_success() {
         // valid index, mark as absent
-        assertParseSuccess(parser, "1",
+        assertParseSuccess(parser, "1" + ATTENDANCE_TYPE_DESC_ABSENT,
                 new MarkCommand(
                         INDEX_FIRST_PERSON,
                         AttendanceType.ABSENT));
 
         // valid index, mark as late
-        assertParseSuccess(parser, "1",
+        assertParseSuccess(parser, "1"  + ATTENDANCE_TYPE_DESC_LATE,
                 new MarkCommand(
                         INDEX_FIRST_PERSON,
                         AttendanceType.LATE));
 
         // valid index, mark as present
-        assertParseSuccess(parser, "1",
+        assertParseSuccess(parser, "1" + ATTENDANCE_TYPE_DESC_PRESENT,
                 new MarkCommand(
                         INDEX_FIRST_PERSON,
                         AttendanceType.PRESENT));
@@ -72,34 +74,38 @@ public class MarkCommandParserTest {
         String validExpectedPersonString = NAME_DESC_BOB;
 
         // multiple names
-        assertParseFailure(parser, NAME_DESC_AMY + validExpectedPersonString,
+        assertParseFailure(parser, NAME_DESC_AMY + validExpectedPersonString + ATTENDANCE_TYPE_DESC_ABSENT,
                 Messages.getErrorMessageForDuplicatePrefixes(PREFIX_NAME));
 
         // multiple indexes
-        assertParseFailure(parser, " 1 2 " + NAME_DESC_BOB + validExpectedPersonString,
+        assertParseFailure(parser, " 1 2 " + NAME_DESC_BOB + validExpectedPersonString + ATTENDANCE_TYPE_DESC_ABSENT,
                 Messages.getErrorMessageForDuplicatePrefixes(PREFIX_NAME));
 
         // invalid value followed by valid value
 
         // invalid name
-        assertParseFailure(parser, INVALID_NAME_DESC + validExpectedPersonString,
+        assertParseFailure(parser, INVALID_NAME_DESC + validExpectedPersonString + ATTENDANCE_TYPE_DESC_ABSENT,
                 Messages.getErrorMessageForDuplicatePrefixes(PREFIX_NAME));
 
 
         // valid value followed by invalid value
 
         // invalid name
-        assertParseFailure(parser, validExpectedPersonString + INVALID_NAME_DESC,
+        assertParseFailure(parser, validExpectedPersonString + INVALID_NAME_DESC + ATTENDANCE_TYPE_DESC_ABSENT,
                 Messages.getErrorMessageForDuplicatePrefixes(PREFIX_NAME));
 
     }
 
     @Test
-    public void parse_compulsoryFieldMissing_failure() {
+    public void parse_attendanceFieldMissing_failure() {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE);
 
-        // missing name prefix
+        // missing attendance prefix
         assertParseFailure(parser, VALID_NAME_BOB,
+                expectedMessage);
+
+        // missing name prefix
+        assertParseFailure(parser, ATTENDANCE_TYPE_DESC_LATE,
                 expectedMessage);
 
 
@@ -111,7 +117,7 @@ public class MarkCommandParserTest {
     @Test
     public void parse_invalidName_failure() {
         // invalid name
-        assertParseFailure(parser, INVALID_NAME_DESC,
+        assertParseFailure(parser, INVALID_NAME_DESC + ATTENDANCE_TYPE_DESC_ABSENT,
                 Name.MESSAGE_CONSTRAINTS);
     }
 }
