daa390e60c93b7439dbebbe7813689a6bd88ef2c
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-11-06 22:11:53 +0800

    Adjust log permission handling
    It does not try to create log with similar name.
    The logger simply ignores the lack of permission,
    and continue.

--- src/main/java/networkbook/commons/core/LogsCenter.java
+++ src/main/java/networkbook/commons/core/LogsCenter.java
@@ -92,18 +92,13 @@ public class LogsCenter {
         baseLogger.addHandler(consoleHandler);
 
         // add a FileHandler to log to a file
-        String filename = LOG_FILE;
-        boolean isLogFileCreated = false;
-        while (!isLogFileCreated) {
-            try {
-                FileHandler fileHandler = new FileHandler(filename, MAX_FILE_SIZE_IN_BYTES, MAX_FILE_COUNT, true);
-                fileHandler.setFormatter(new SimpleFormatter());
-                fileHandler.setLevel(Level.ALL);
-                baseLogger.addHandler(fileHandler);
-                isLogFileCreated = true;
-            } catch (IOException e) {
-                filename += ".1";
-            }
+        try {
+            FileHandler fileHandler = new FileHandler(LOG_FILE, MAX_FILE_SIZE_IN_BYTES, MAX_FILE_COUNT, true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            fileHandler.setLevel(Level.ALL);
+            baseLogger.addHandler(fileHandler);
+        } catch (IOException e) {
+            baseLogger.warning("Cannot create file handler for logger.");
         }
     }
 
