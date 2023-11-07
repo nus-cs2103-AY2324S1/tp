cf12f0f4b1f94a89145248f781b06488225b0f45
JizhuoChen <88676760+JizhuoChen@users.noreply.github.com>
2023-11-07 17:29:01 +0800

    Undo the addition of help message shown, add test for TreeCommand

---
+++ src/test/java/seedu/address/logic/commands/TreeCommandTest.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+public class TreeCommandTest {
+
+    @Test
+    public void execute_treeCommand_success() {
+        Model model = new ModelManager();
+        CommandResult expectedCommandResult = new CommandResult(
+                TreeCommand.MESSAGE_SUCCESS, false, false,
+                false, false, true, false, false);
+        TreeCommand command = new TreeCommand();
+
+        CommandResult result = command.execute(model);
+
+        assertEquals(expectedCommandResult, result);
+    }
+}
