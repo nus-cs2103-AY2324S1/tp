4369f38e4de980ee5eaa295996f0b5cd51724231
brendanneojw <e0726880@u.nus.edu>
2023-11-07 19:16:37 +0800

    Fix bug where expired policies show up in remind

--- src/main/java/seedu/address/model/person/RemindPredicate.java
+++ src/main/java/seedu/address/model/person/RemindPredicate.java
@@ -11,6 +11,7 @@ import seedu.address.model.policy.PolicyDate;
  * Tests that a {@code Person}'s {@code Policy Expiry Date}is within a certain period from the current date.
  */
 public class RemindPredicate implements Predicate<Person> {
+    private static final int NON_EXPIRY_DAY = 0;
     private final int days;
 
     /**
@@ -25,8 +26,9 @@ public class RemindPredicate implements Predicate<Person> {
 
     @Override
     public boolean test(Person person) {
+        long dayDifference = ChronoUnit.DAYS.between(LocalDate.now(), person.getPolicy().getPolicyExpiryDate().date);
         return !person.getPolicy().getPolicyExpiryDate().toString().equals(PolicyDate.DEFAULT_VALUE)
-                && ChronoUnit.DAYS.between(LocalDate.now(), person.getPolicy().getPolicyExpiryDate().date) <= days;
+                && dayDifference >= 0 && dayDifference <= days;
     }
 
     @Override
