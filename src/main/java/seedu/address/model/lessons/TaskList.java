a3b8c3c0013f68b91825e3bb591ad1a8364e263c
elaineshijie <lohelaineshijie.183@gmail.com>
2023-11-07 21:23:34 +0800

    Remove unused commented code

--- src/main/java/seedu/address/model/lessons/TaskList.java
+++ src/main/java/seedu/address/model/lessons/TaskList.java
@@ -67,7 +67,6 @@ public class TaskList extends ListEntryField implements Iterable<Task> {
     public int getTaskClashWith(Task toCheck) {
         requireNonNull(toCheck);
         int index = 0;
-        //return internalTaskList.stream().filter(toCheck::isSameTask).findFirst().get();
         for (int i = 0; i < internalTaskList.size(); i++) {
             if (internalTaskList.get(i).isSameTask(toCheck)) {
                 index = i;
