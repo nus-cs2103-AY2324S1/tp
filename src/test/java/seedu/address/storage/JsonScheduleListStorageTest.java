2d7d17bd275961d2c97a91a953c88d2193b8b529
Marcus Soh <macussoh38@gmail.com>
2023-11-07 17:02:04 +0800

    Added tests for storing tasks

--- src/test/java/seedu/address/storage/JsonScheduleListStorageTest.java
+++ src/test/java/seedu/address/storage/JsonScheduleListStorageTest.java
@@ -1,20 +1,22 @@
 package seedu.address.storage;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
-import seedu.address.commons.exceptions.DataLoadingException;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.ReadOnlySchedule;
-import seedu.address.model.ScheduleList;
-import seedu.address.testutil.TypicalLessons;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalLessons.getTypicalScheduleList;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import seedu.address.commons.exceptions.DataLoadingException;
+import seedu.address.model.ReadOnlySchedule;
+import seedu.address.model.ScheduleList;
+import seedu.address.testutil.TypicalLessons;
+import seedu.address.testutil.TypicalTasks;
 
 public class JsonScheduleListStorageTest {
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonScheduleListStorageTest");
@@ -70,5 +72,43 @@ public class JsonScheduleListStorageTest {
         assertEquals(original, new ScheduleList(readBack));
     }
 
+    @Test
+    public void saveScheduleListWithTasks_allInOrder_success() throws Exception {
+        Path filePath = testFolder.resolve("TempScheduleList.json");
+        ScheduleList original = getTypicalScheduleList();
+        JsonScheduleListStorage jsonScheduleListStorage = new JsonScheduleListStorage(filePath);
+        // add a task to the first lesson
+        original.getLessonList().get(0).addToTaskList(TypicalTasks.TASK_5);
 
+        // add a task to the 3rd lesson
+        original.getLessonList().get(3).addToTaskList(TypicalTasks.TASK_4);
+        original.getLessonList().get(3).addToTaskList(TypicalTasks.TASK_6);
+
+        // save
+        jsonScheduleListStorage.saveScheduleList(original, filePath);
+        ReadOnlySchedule readBack = jsonScheduleListStorage.readScheduleList(filePath).get();
+        assertEquals(original, new ScheduleList(readBack));
+    }
+
+    @Test
+    public void saveScheduleList_nullScheduleList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveScheduleList(null, "SomeFile.json"));
+    }
+
+    /**
+     * Saves {@code schedule} at the specified {@code filePath}.
+     */
+    private void saveScheduleList(ReadOnlySchedule schedule, String filePath) {
+        try {
+            new JsonScheduleListStorage(Paths.get(filePath))
+                    .saveScheduleList(schedule, addToTestDataPathIfNotNull(filePath));
+        } catch (IOException ioe) {
+            throw new AssertionError("There should not be an error writing to the file.", ioe);
+        }
+    }
+
+    @Test
+    public void saveScheduleList_nullFilePath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveScheduleList(new ScheduleList(), null));
+    }
 }
