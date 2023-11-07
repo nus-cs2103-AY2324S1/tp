34feb8cf166e32c963ce45b6e787462eca2a568c
Lim Ngee Yong <e0940681@u.nus.edu>
2023-11-07 23:21:57 +0800

    Update Phone.java

--- src/main/java/seedu/classmanager/model/student/Phone.java
+++ src/main/java/seedu/classmanager/model/student/Phone.java
@@ -13,6 +13,7 @@ public class Phone {
     public static final String MESSAGE_CONSTRAINTS =
             "Phone numbers should only contain non negative numbers, and it should be at least 3 digits long";
     public static final String VALIDATION_REGEX = "\\d{3,}";
+
     public final String value;
 
     /**
