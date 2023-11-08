378e8d3a4808c5d84b854b89949e4ceb1a8a12fe
Nicholas Jimmy Alden <89759682+Nixx162@users.noreply.github.com>
2023-11-09 02:12:05 +0800

    chore: fix checkstyle

--- src/main/java/seedu/address/logic/commands/AddTagCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTagCommand.java
@@ -42,7 +42,7 @@ public class AddTagCommand extends AddCommand {
         if (person == null) {
             throw new CommandException(MESSAGE_PERSON_NOT_FOUND + this.contactId);
         }
-        
+
         Person toEdit = person;
         toEdit.addTags(this.toAdd);
         model.setPerson(person, toEdit);
