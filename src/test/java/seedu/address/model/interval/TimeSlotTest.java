99ec65eb84ff142dabdcaa1d366a09cf57d208ef
Winston <leonardwinstoncx9@gmail.com>
2023-11-09 01:35:30 +0800

    add test case for freeTime

--- src/test/java/seedu/address/model/interval/TimeSlotTest.java
+++ src/test/java/seedu/address/model/interval/TimeSlotTest.java
@@ -49,6 +49,8 @@ public class TimeSlotTest {
 
     @Test
     public void findAvailableTime() throws ParseException {
+        // check when interval end is bigger than last end in timeslot
+
         Interval interval = new Interval(new IntervalDay("Mon"), new Duration("30"), new IntervalBegin("0800"),
                 new IntervalEnd("2200"));
 
@@ -58,7 +60,6 @@ public class TimeSlotTest {
         timeslots.add(new TimeSlot(dateFormat.parse("15:00"), dateFormat.parse("18:00")));
 
         List<TimeSlot> result = TimeSlot.findAvailableTime(timeslots, interval);
-
         List<TimeSlot> expected = new ArrayList<>();
         expected.add(new TimeSlot(dateFormat.parse("08:00"), dateFormat.parse("12:00")));
         expected.add(new TimeSlot(dateFormat.parse("14:00"), dateFormat.parse("15:00")));
@@ -72,5 +73,30 @@ public class TimeSlotTest {
         List<TimeSlot> expected2 = new ArrayList<>();
         expected2.add(new TimeSlot(dateFormat.parse("08:00"), dateFormat.parse("12:00")));
         assertEquals(result2, expected2);
+
+        //check when interval end is smaller than last end in timeslots.
+
+        List<TimeSlot> timeslots3 = new ArrayList<>();
+        timeslots3.add(new TimeSlot(dateFormat.parse("20:00"), dateFormat.parse("21:00")));
+
+        Interval interval3 = new Interval(new IntervalDay("Mon"), new Duration("120"), new IntervalBegin("0800"),
+                new IntervalEnd("1000"));
+
+        List<TimeSlot> expected3 = new ArrayList<>();
+        expected3.add(new TimeSlot(dateFormat.parse("08:00"), dateFormat.parse("10:00")));
+        List<TimeSlot> result3 = TimeSlot.findAvailableTime(timeslots3, interval3);
+        assertEquals(result3, expected3);
+
+
+        //checks when duration is smaller than given the begin and end of intervals
+        List<TimeSlot> timeslots4 = new ArrayList<>();
+        timeslots4.add(new TimeSlot(dateFormat.parse("20:00"), dateFormat.parse("21:00")));
+
+        Interval interval4 = new Interval(new IntervalDay("Mon"), new Duration("180"), new IntervalBegin("0800"),
+                new IntervalEnd("1000"));
+        List<TimeSlot> expected4 = new ArrayList<>();
+        List<TimeSlot> result4 = TimeSlot.findAvailableTime(timeslots4, interval4);
+        assertEquals(result4, expected4);
     }
+
 }
