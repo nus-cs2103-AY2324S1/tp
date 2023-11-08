642ead2ecfaf5e6c625d6d44efa9090a3ee5205c
Yuheng <guoyuheng1year@gmail.com>
2023-11-08 22:46:26 +0800

    Fix CI issues

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -12,7 +12,8 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long and no longer than 17 digits.";
+            "Phone numbers should only contain numbers, and it should be at least 3"
+                    + " digits long and no longer than 17 digits.";
     public static final String VALIDATION_REGEX = "\\d{3,}";
     public static final Phone NULL_PHONE;
 
