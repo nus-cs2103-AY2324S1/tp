fedc0ce6771185df8e928b6fb56a1c9e46c79a02
anthonytamzil <anthony.tamzil@gmail.com>
2023-11-08 15:35:36 +0800

    Update MESSAGE_CONSTRAINT

--- src/test/java/seedu/address/model/person/NameTest.java
+++ src/test/java/seedu/address/model/person/NameTest.java
@@ -27,11 +27,11 @@ public class NameTest {
         // invalid name
         assertFalse(Name.isValidName("")); // empty string
         assertFalse(Name.isValidName(" ")); // spaces only
-        assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
-        assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric characters
+        assertFalse(Name.isValidName("^")); // only non-alphabetical characters
+        assertFalse(Name.isValidName("peter*")); // contains non-alphabetical characters
         assertFalse(Name.isValidName("12345")); // numbers only
-        assertFalse(Name.isValidName("peter the 2nd")); // alphanumeric characters
-        assertFalse(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
+        assertFalse(Name.isValidName("peter the 2nd")); // contains numerical characters
+        assertFalse(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long name with numbers
 
         // valid name
         assertTrue(Name.isValidName("peter jack")); // alphabets only
