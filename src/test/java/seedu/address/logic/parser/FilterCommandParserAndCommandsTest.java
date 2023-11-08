45222e4321b3c3069d996096be372d5ecbc23f72
yiwen101 <e0968880@u.nus.edu>
2023-11-08 22:06:09 +0800

    Fix style

--- src/test/java/seedu/address/logic/parser/FilterCommandParserAndCommandsTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserAndCommandsTest.java
@@ -1,10 +1,12 @@
 package seedu.address.logic.parser;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalLessons.getTypicalScheduleList;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
@@ -22,27 +24,27 @@ import seedu.address.model.person.Subjects;
 import seedu.address.model.person.Tags;
 import seedu.address.model.state.State;
 
-class FilterCommandParserTest {
+class FilterCommandParserAndCommandsTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(),
             getTypicalScheduleList());
-    private FilterCommandParser PersonFilter = new FilterCommandParser(State.STUDENT);
-    private FilterCommandParser LessonFilter = new FilterCommandParser(State.SCHEDULE);
+    private FilterCommandParser personFilter = new FilterCommandParser(State.STUDENT);
+    private FilterCommandParser lessonFilter = new FilterCommandParser(State.SCHEDULE);
     // phone, address, remark, tags, email
     @Test
-    void test_personFilter_Name() throws ParseException, CommandException {
+    void test_personFilter_name() throws ParseException, CommandException {
         Person person = Person.getDefaultPerson();
         person.setName(Name.of("test 123456"));
         Person person2 = Person.getDefaultPerson();
         person2.setName(Name.of("test 12345678"));
         model.addPerson(person);
         model.addPerson(person2);
-        PersonFilter.parse(" -name test 123456").execute(model);
+        personFilter.parse(" -name test 123456").execute(model);
         assertTrue(model.getFilteredPersonList().contains(person));
         assertFalse(model.getFilteredPersonList().contains(person2));
     }
 
     @Test
-    void test_personFilter_Phone() throws ParseException, CommandException {
+    void test_personFilter_phone() throws ParseException, CommandException {
         Person person = Person.getDefaultPerson();
         person.setPhone(Phone.of("12345678"));
         person.setName(Name.of("person 1"));
@@ -51,13 +53,13 @@ class FilterCommandParserTest {
         person2.setName(Name.of("person 2"));
         model.addPerson(person);
         model.addPerson(person2);
-        PersonFilter.parse(" -phone 12345678").execute(model);
+        personFilter.parse(" -phone 12345678").execute(model);
         assertTrue(model.getFilteredPersonList().contains(person));
         assertFalse(model.getFilteredPersonList().contains(person2));
     }
 
     @Test
-    void test_personFilter_Email() throws ParseException, CommandException {
+    void test_personFilter_email() throws ParseException, CommandException {
         Person person = Person.getDefaultPerson();
         person.setEmail(Email.of("fake123@domain.com"));
         person.setName(Name.of("person 1"));
@@ -66,13 +68,13 @@ class FilterCommandParserTest {
         person2.setName(Name.of("person 2"));
         model.addPerson(person);
         model.addPerson(person2);
-        PersonFilter.parse(" -email fake123@domain.com").execute(model);
+        personFilter.parse(" -email fake123@domain.com").execute(model);
         assertTrue(model.getFilteredPersonList().contains(person));
         assertFalse(model.getFilteredPersonList().contains(person2));
     }
 
     @Test
-    void test_personFilter_Tags() throws ParseException, CommandException {
+    void test_personFilter_tags() throws ParseException, CommandException {
         Person person = Person.getDefaultPerson();
         person.setTags(Tags.of("tag1,tag2"));
         person.setName(Name.of("person 1"));
@@ -81,13 +83,13 @@ class FilterCommandParserTest {
         person2.setName(Name.of("person 2"));
         model.addPerson(person);
         model.addPerson(person2);
-        PersonFilter.parse(" -tag tag1").execute(model);
+        personFilter.parse(" -tag tag1").execute(model);
         assertTrue(model.getFilteredPersonList().contains(person));
         assertTrue(model.getFilteredPersonList().contains(person2));
-        PersonFilter.parse(" -tag tag2").execute(model);
+        personFilter.parse(" -tag tag2").execute(model);
         assertTrue(model.getFilteredPersonList().contains(person));
         assertFalse(model.getFilteredPersonList().contains(person2));
-        PersonFilter.parse(" -tag tag3").execute(model);
+        personFilter.parse(" -tag tag3").execute(model);
         assertFalse(model.getFilteredPersonList().contains(person));
         assertTrue(model.getFilteredPersonList().contains(person2));
     }
@@ -102,19 +104,19 @@ void test_personFilter_subjects() throws ParseException, CommandException {
         person2.setName(Name.of("person 2"));
         model.addPerson(person);
         model.addPerson(person2);
-        PersonFilter.parse(" -subject physics").execute(model);
+        personFilter.parse(" -subject physics").execute(model);
         assertTrue(model.getFilteredPersonList().contains(person));
         assertTrue(model.getFilteredPersonList().contains(person2));
-        PersonFilter.parse(" -subject physics,chemistry").execute(model);
+        personFilter.parse(" -subject physics,chemistry").execute(model);
         assertTrue(model.getFilteredPersonList().contains(person));
         assertFalse(model.getFilteredPersonList().contains(person2));
-        PersonFilter.parse(" -subject English").execute(model);
+        personFilter.parse(" -subject English").execute(model);
         assertFalse(model.getFilteredPersonList().contains(person));
         assertTrue(model.getFilteredPersonList().contains(person2));
     }
 
     @Test
-    void test_personFilter_Remark() throws ParseException, CommandException {
+    void test_personFilter_remark() throws ParseException, CommandException {
         Person person = Person.getDefaultPerson();
         person.setRemark(Remark.of("remark1"));
         person.setName(Name.of("person 1"));
@@ -123,16 +125,16 @@ void test_personFilter_subjects() throws ParseException, CommandException {
         person2.setName(Name.of("person 2"));
         model.addPerson(person);
         model.addPerson(person2);
-        PersonFilter.parse(" -remark remark1").execute(model);
+        personFilter.parse(" -remark remark1").execute(model);
         assertTrue(model.getFilteredPersonList().contains(person));
         assertFalse(model.getFilteredPersonList().contains(person2));
-        PersonFilter.parse(" -remark remark").execute(model);
+        personFilter.parse(" -remark remark").execute(model);
         assertTrue(model.getFilteredPersonList().contains(person));
         assertTrue(model.getFilteredPersonList().contains(person2));
     }
 
     @Test
-    void test_personFilter_Multiple() throws ParseException, CommandException {
+    void test_personFilter_multiple() throws ParseException, CommandException {
         Person person = Person.getDefaultPerson();
         person.setTags(Tags.of("tag1,tag2"));
         person.setName(Name.of("person 1"));
@@ -143,23 +145,23 @@ void test_personFilter_subjects() throws ParseException, CommandException {
         person2.setPhone(Phone.of("123456789"));
         model.addPerson(person);
         model.addPerson(person2);
-        PersonFilter.parse(" -tag tag1 -phone 12345678").execute(model);
+        personFilter.parse(" -tag tag1 -phone 12345678").execute(model);
         assertTrue(model.getFilteredPersonList().contains(person));
         assertFalse(model.getFilteredPersonList().contains(person2));
-        PersonFilter.parse(" -tag tag1 -phone 123456789").execute(model);
+        personFilter.parse(" -tag tag1 -phone 123456789").execute(model);
         assertFalse(model.getFilteredPersonList().contains(person));
         assertTrue(model.getFilteredPersonList().contains(person2));
     }
 
     @Test
-    void test_lessonFilter_Name() throws ParseException, CommandException {
+    void test_lessonFilter_name() throws ParseException, CommandException {
         Lesson lesson = Lesson.getDefaultLesson();
         lesson.setName(Name.of("test lesson 1"));
         Lesson lesson2 = Lesson.getDefaultLesson();
         lesson2.setName(Name.of("test lesson 2"));
         model.addLesson(lesson);
         model.addLesson(lesson2);
-        LessonFilter.parse(" -name test lesson 1").execute(model);
+        lessonFilter.parse(" -name test lesson 1").execute(model);
         assertTrue(model.getFilteredScheduleList().contains(lesson));
         assertFalse(model.getFilteredScheduleList().contains(lesson2));
     }
@@ -172,13 +174,13 @@ void test_personFilter_subjects() throws ParseException, CommandException {
         lesson2.setDay(Day.of("3"));
         model.addLesson(lesson);
         model.addLesson(lesson2);
-        LessonFilter.parse(" -before 2").execute(model);
+        lessonFilter.parse(" -before 2").execute(model);
         assertTrue(model.getFilteredScheduleList().contains(lesson));
         assertFalse(model.getFilteredScheduleList().contains(lesson2));
-        LessonFilter.parse(" -after 2").execute(model);
+        lessonFilter.parse(" -after 2").execute(model);
         assertFalse(model.getFilteredScheduleList().contains(lesson));
         assertTrue(model.getFilteredScheduleList().contains(lesson2));
-        LessonFilter.parse(" -on 1").execute(model);
+        lessonFilter.parse(" -on 1").execute(model);
         assertTrue(model.getFilteredScheduleList().contains(lesson));
         assertFalse(model.getFilteredScheduleList().contains(lesson2));
     }
@@ -191,19 +193,19 @@ void test_personFilter_subjects() throws ParseException, CommandException {
         lesson2.setSubject(Subject.of("chemistry"));
         model.addLesson(lesson);
         model.addLesson(lesson2);
-        LessonFilter.parse(" -subject physics").execute(model);
+        lessonFilter.parse(" -subject physics").execute(model);
         assertTrue(model.getFilteredScheduleList().contains(lesson));
         assertFalse(model.getFilteredScheduleList().contains(lesson2));
-        LessonFilter.parse(" -subject physics,chemistry").execute(model);
+        lessonFilter.parse(" -subject physics,chemistry").execute(model);
         assertTrue(model.getFilteredScheduleList().contains(lesson));
         assertTrue(model.getFilteredScheduleList().contains(lesson2));
-        LessonFilter.parse(" -subject English").execute(model);
+        lessonFilter.parse(" -subject English").execute(model);
         assertFalse(model.getFilteredScheduleList().contains(lesson));
         assertFalse(model.getFilteredScheduleList().contains(lesson2));
     }
 
     @Test
-    void test_lessonFilter_Multiple() throws ParseException, CommandException {
+    void test_lessonFilter_multiple() throws ParseException, CommandException {
         Lesson lesson = Lesson.getDefaultLesson();
         lesson.setSubject(Subject.of("physics"));
         lesson.setDay(Day.of("1"));
@@ -212,15 +214,14 @@ void test_personFilter_subjects() throws ParseException, CommandException {
         lesson2.setDay(Day.of("3"));
         model.addLesson(lesson);
         model.addLesson(lesson2);
-        LessonFilter.parse(" -subject physics -before 2").execute(model);
+        lessonFilter.parse(" -subject physics -before 2").execute(model);
         assertTrue(model.getFilteredScheduleList().contains(lesson));
         assertFalse(model.getFilteredScheduleList().contains(lesson2));
-        LessonFilter.parse(" -subject physics -after 2").execute(model);
+        lessonFilter.parse(" -subject physics -after 2").execute(model);
         assertFalse(model.getFilteredScheduleList().contains(lesson));
         assertFalse(model.getFilteredScheduleList().contains(lesson2));
-        LessonFilter.parse(" -subject physics -on 1").execute(model);
+        lessonFilter.parse(" -subject physics -on 1").execute(model);
         assertTrue(model.getFilteredScheduleList().contains(lesson));
         assertFalse(model.getFilteredScheduleList().contains(lesson2));
     }
-
 }
