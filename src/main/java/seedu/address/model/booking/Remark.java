6db65d1e6edc60dbd9d30fb9f4889d8472570506
AryanG01 <rajeevganju0@gmail.com>
2023-11-08 22:57:23 +0800

    Fixed CheckStyle

--- src/main/java/seedu/address/model/booking/Remark.java
+++ src/main/java/seedu/address/model/booking/Remark.java
@@ -1,6 +1,5 @@
 package seedu.address.model.booking;
 
-import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import seedu.address.model.booking.exceptions.RemarkNotFoundException;
--- src/test/java/seedu/address/model/booking/BookingPeriodTest.java
+++ src/test/java/seedu/address/model/booking/BookingPeriodTest.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
-import java.time.DateTimeException;
-import java.time.format.DateTimeParseException;
-
 import org.junit.jupiter.api.Test;
 
 public class BookingPeriodTest {
