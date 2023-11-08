91b1146192b33c596087208f9b4dbfc699c2fb2b
He Yifan <e0968838@u.nus.edu>
2023-11-08 16:01:20 +0800

    Add testcases for salary and payslip command

--- src/test/java/seedu/address/logic/commands/PayslipCommandTest.java
+++ src/test/java/seedu/address/logic/commands/PayslipCommandTest.java
@@ -7,12 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.io.File;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.PaySlipGenerator;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.ParserUtil;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -22,20 +24,28 @@ import seedu.address.model.person.Person;
 
 class PayslipCommandTest {
 
-    private PayslipCommand payslipCommand = new PayslipCommand(Index.fromZeroBased(2));
-    private PayslipCommand payslipCommand2 = new PayslipCommand(Index.fromZeroBased(1000));
+    private final PayslipCommand payslipCommand = new PayslipCommand(Index.fromZeroBased(2));
+    private final PayslipCommand payslipCommand2 = new PayslipCommand(Index.fromZeroBased(1000));
+    private final PayslipCommand payslipCommand3 = new PayslipCommand(
+        new NameContainsKeywordsPredicate(Collections.singletonList("Alice")),
+        ParserUtil.stringToDate("06/10/2023"));
+    private final PayslipCommand payslipCommand4 = new PayslipCommand(
+        new NameContainsKeywordsPredicate(Collections.singletonList("Sponge")));
+    private final PayslipCommand payslipCommand5 = new PayslipCommand(
+        new NameContainsKeywordsPredicate(Collections.singletonList("Alice")),
+        ParserUtil.stringToDate("06/10/2024"));
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void constructor_nullIndex_throwsNullPointerException() {
+    public void constructor_nullArguments_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new PayslipCommand((Index) null));
         assertThrows(NullPointerException.class, () -> new PayslipCommand((NameContainsKeywordsPredicate) null));
     }
 
     @Test
-    public void execute() throws Exception {
-        // success
+    public void executeWithIndex() throws Exception {
+        // success with index
         Person employeeToGenerate = model.getFilteredPersonList().get(2);
         employeeToGenerate.addPayroll(new Payroll(employeeToGenerate.getSalary()));
         CommandResult commandResult = payslipCommand.execute(model);
@@ -44,9 +54,18 @@ class PayslipCommandTest {
         File payslip = new File("payslips/" + PaySlipGenerator.getFileName(employeeToGenerate));
         assertTrue(payslip.exists());
         payslip.delete();
+    }
 
-        // fail
+    @Test
+    public void execute_invalidArguments_throwsCommandException() {
+        // fail with invalid index
         assertThrows(CommandException.class, () -> payslipCommand2.execute(model)); // index out of bounds
+
+        // fail with non-existent name
+        assertThrows(CommandException.class, () -> payslipCommand4.execute(model)); // no such person
+
+        // fail with non-existent payroll
+        assertThrows(CommandException.class, () -> payslipCommand5.execute(model)); // no such payroll
     }
 
     @Test
