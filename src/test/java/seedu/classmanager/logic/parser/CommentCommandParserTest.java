e42106d096da058b1b407dcf5e27230df083dcbc
Lim Ngee Yong <e0940681@u.nus.edu>
2023-11-10 01:03:27 +0800

    Update CommentCommandParserTest.java

--- src/test/java/seedu/classmanager/logic/parser/CommentCommandParserTest.java
+++ src/test/java/seedu/classmanager/logic/parser/CommentCommandParserTest.java
@@ -34,7 +34,7 @@ public class CommentCommandParserTest {
     @Test
     public void parse_invalidStudentNumber_throwsParseException() {
         assertParseFailure(parser, " s/ " + "T1234567M" + " cm/ Good student",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, CommentCommand.MESSAGE_USAGE));
+                StudentNumber.MESSAGE_CONSTRAINTS + "\n" + CommentCommand.MESSAGE_USAGE);
     }
 
     @Test
