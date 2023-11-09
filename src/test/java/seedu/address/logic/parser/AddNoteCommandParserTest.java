8a670eacb9ed124590e3269739636d440219004d
limjunxian1 <limjunxian@live.com.sg>
2023-11-09 19:07:52 +0800

    Fix style issue

--- src/test/java/seedu/address/logic/parser/AddNoteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddNoteCommandParserTest.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSucces
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.AddNoteCommand;
 import seedu.address.model.person.Note;
 
