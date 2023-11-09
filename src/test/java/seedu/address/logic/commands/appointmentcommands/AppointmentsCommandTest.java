c5fa29f52c1ec91a92e25d96b6f1f11e26f689f3
licongshen12 <spectrashen@gmail.com>
2023-11-09 18:59:21 +0800

    Update tests for appointment commands

--- src/test/java/seedu/address/logic/commands/appointmentcommands/AppointmentsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/appointmentcommands/AppointmentsCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands.appointmentcommands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showAppointmentAtIndex;
 import static seedu.address.testutil.TypicalAppointments.getTypicalAddressBook;
@@ -32,5 +34,23 @@ public class AppointmentsCommandTest {
     public void execute_listIsFiltered_showsEverything() {
         showAppointmentAtIndex(model, INDEX_FIRST_APPOINTMENT);
         assertCommandSuccess(new AppointmentsCommand(), model, AppointmentsCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        AppointmentsCommand firstAppointmentsCommand = new AppointmentsCommand();
+        AppointmentsCommand secondAppointmentsCommand = new AppointmentsCommand();
+
+        // same object -> returns true
+        assertTrue(firstAppointmentsCommand.equals(firstAppointmentsCommand));
+
+        // same type -> returns true
+        assertTrue(firstAppointmentsCommand.equals(secondAppointmentsCommand));
+
+        // different types -> returns false
+        assertFalse(firstAppointmentsCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(firstAppointmentsCommand.equals(null));
     }
 }
--- src/test/java/seedu/address/logic/commands/appointmentcommands/TodayCommandTest.java
+++ src/test/java/seedu/address/logic/commands/appointmentcommands/TodayCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands.appointmentcommands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showAppointmentAtIndex;
 import static seedu.address.testutil.TypicalAppointments.TODAYAPPOINTMENT;
@@ -22,5 +24,23 @@ public class TodayCommandTest {
         expectedModel.addAppointment(TODAYAPPOINTMENT);
         showAppointmentAtIndex(expectedModel, Index.fromZeroBased(4));
         assertCommandSuccess(new TodayCommand(), model, TodayCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        TodayCommand firstTodayCommand = new TodayCommand();
+        TodayCommand secondTodayCommand = new TodayCommand();
+
+        // same object -> returns true
+        assertTrue(firstTodayCommand.equals(firstTodayCommand));
+
+        // same type -> returns true
+        assertTrue(firstTodayCommand.equals(secondTodayCommand));
+
+        // different types -> returns false
+        assertFalse(firstTodayCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(firstTodayCommand.equals(null));
     }
 }
