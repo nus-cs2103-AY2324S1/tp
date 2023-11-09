83fc5eb97b5b3969de28abd8983c495eeda80f4a
lordidiot <0xlordidiot@gmail.com>
2023-11-09 23:14:08 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/model/person/Avatar.java
+++ src/main/java/seedu/address/model/person/Avatar.java
@@ -7,7 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
