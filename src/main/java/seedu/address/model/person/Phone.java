bf440cf81e01cf2e86ceef4b8718adffd1164b49
MagnificentCreature <E0960127@u.nus.edu>
2023-11-09 21:01:15 +0800

    Add suggestions by @zhyuhan

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,8 +11,8 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-        "Phone numbers should only contain numbers (with an optional + sign at the front)"
-        + "and they should be between 3 and 15 characters long";
+        "Phone numbers should only contain numbers and they should be between 3 and 15 characters long"
+        + " (with an optional + sign at the front)";
     public static final String VALIDATION_REGEX = "^\\+?[0-9]{3,15}$";
     public final String value;
 
