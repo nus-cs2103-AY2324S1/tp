34adeda747648773ab017c54aeb5d35ffe245d55
pzl111 <phuazailian@gmail.com>
2023-11-10 10:31:51 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/state/State.java
+++ src/main/java/seedu/address/model/state/State.java
@@ -1,17 +1,11 @@
 package seedu.address.model.state;
 
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
-
 /**
  * Enum for different possible state of the app.
  */
 public enum State {
-
     STUDENT,
     SCHEDULE,
     TASK,
     NONE;
-
 }
