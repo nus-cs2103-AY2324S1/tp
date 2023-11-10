aad0c96c48d43c9dac6751c3a41fdbdb1f05a7f8
adhigop13 <adhigop13@gmail.com>
2023-11-11 02:58:03 +0800

    Add tests for AddTeamCommandParser

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -40,9 +40,9 @@ public class TypicalPersons {
             .withEmail("cornelia@example.com").withAddress("10th street")
             .withTags("friends").withIdentityCode("4").build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withAddress("michegan ave").build();
+            .withEmail("werner@example.com").withAddress("michegan ave").withIdentityCode("5").build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo").build();
+            .withEmail("lydia@example.com").withAddress("little tokyo").withIdentityCode("6").build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
             .withEmail("anna@example.com").withAddress("4th street").build();
 
