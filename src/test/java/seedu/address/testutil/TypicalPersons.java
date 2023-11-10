9f93c2f45cbc06585735da71fc95ce2fed280c9a
Kevin <e1053635@u.nus.edu>
2023-11-10 20:11:45 +0800

    Fix bug

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -56,7 +56,7 @@ public class TypicalPersons {
             .withAnnualLeave("2").withJoinDate("04/05/2021").withBankAccount("0123457")
             .withPayrollStorage(new ArrayList<>()).build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo").withSalary("1500.0" + "0")
+            .withEmail("lydia@example.com").withAddress("little tokyo").withSalary("1500.00")
             .withAnnualLeave("2").withJoinDate("04/05/2021").withBankAccount("0123457")
             .withPayrollStorage(new ArrayList<>()).build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
