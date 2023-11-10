19a124151fa79b4ee48bd33eb7f167332270d719
Leb14 <haocj98@gmail.com>
2023-11-11 00:33:11 +0800

    Add more testcases for ViewLeaveCommand

--- src/test/java/seedu/address/logic/commands/ViewLeaveCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewLeaveCommandTest.java
@@ -1,12 +1,16 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalFields.EMAIL_FIELD;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -55,5 +59,69 @@ class ViewLeaveCommandTest {
 
         DeleteLeaveCommand deleteLeaveCommand = new DeleteLeaveCommand(INDEX_FIRST_PERSON, validDate);
         deleteLeaveCommand.execute(model);
+    }
+
+    @Test
+    public void nameListToStringTest_success() throws CommandException {
+        LocalDate validDate = LocalDate.now().plusDays(1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = validDate.format(formatter);
+
+        AddLeaveCommand addLeaveCommand = new AddLeaveCommand(INDEX_FIRST_PERSON, validDate);
+        addLeaveCommand.execute(model);
+        ViewLeaveCommand viewLeaveCommand = new ViewLeaveCommand(validDate);
+
+        List<String> nameList = viewLeaveCommand.getNameList(model.getFilteredPersonList());
+        String nameListInString = viewLeaveCommand.nameListToString(nameList);
+
+        String expectedString = "Alice Pauline";
+
+        assertEquals(nameListInString, expectedString);
+
+        DeleteLeaveCommand deleteLeaveCommand = new DeleteLeaveCommand(INDEX_FIRST_PERSON, validDate);
+        deleteLeaveCommand.execute(model);
+    }
+
+    @Test
+    public void getNameListTest_success() throws CommandException {
+        LocalDate validDate = LocalDate.now().plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = validDate.format(formatter);
+
+        AddLeaveCommand addLeaveCommand = new AddLeaveCommand(INDEX_FIRST_PERSON, validDate);
+        addLeaveCommand.execute(model);
+        ViewLeaveCommand viewLeaveCommand = new ViewLeaveCommand(validDate);
+
+        List<String> nameList = viewLeaveCommand.getNameList(model.getFilteredPersonList());
+        List<String> expectedNameList = new ArrayList<>();
+        expectedNameList.add("Alice Pauline");
+
+        assertEquals(nameList, expectedNameList);
+
+        DeleteLeaveCommand deleteLeaveCommand = new DeleteLeaveCommand(INDEX_FIRST_PERSON, validDate);
+        deleteLeaveCommand.execute(model);
+    }
+
+    @Test
+    public void equals() {
+        ViewLeaveCommand viewLeaveFirstCommand = new ViewLeaveCommand(LocalDate.now());
+        ViewLeaveCommand viewLeaveSecondCommand = new ViewLeaveCommand(LocalDate.now().plusDays(1));
+
+
+        // same object -> returns true
+        assertTrue(viewLeaveFirstCommand.equals(viewLeaveFirstCommand));
+
+        // same values -> returns true
+        ViewLeaveCommand viewLeaveFirstCommandCopy = new ViewLeaveCommand(LocalDate.now());
+        assertTrue(viewLeaveFirstCommandCopy.equals(viewLeaveFirstCommand));
+
+        // different types -> returns false
+        assertFalse(viewLeaveFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(viewLeaveFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(viewLeaveFirstCommand.equals(viewLeaveSecondCommand));
     }
 }
