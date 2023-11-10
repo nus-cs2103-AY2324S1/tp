3a528f74a039e4ee6fc7537676339a146c3ada67
kenvynKwek <kenvynkwek@gmail.com>
2023-11-10 23:27:15 +0800

    Update email regex
    Email regex is incorrect.
    
    Able to add invalid emails with domain last part missing.
    
    Let's
    * update the email regex to accept only valid emails.
    * update relevant test cases in EmailTest.java.

--- src/main/java/seedu/address/model/employee/Email.java
+++ src/main/java/seedu/address/model/employee/Email.java
@@ -27,8 +27,8 @@ public class Email {
             + ALPHANUMERIC_NO_UNDERSCORE + ")*";
     private static final String DOMAIN_PART_REGEX = ALPHANUMERIC_NO_UNDERSCORE
             + "(-" + ALPHANUMERIC_NO_UNDERSCORE + ")*";
-    private static final String DOMAIN_LAST_PART_REGEX = "(" + DOMAIN_PART_REGEX + "){2,}$"; // At least two chars
-    private static final String DOMAIN_REGEX = "(" + DOMAIN_PART_REGEX + "\\.)*" + DOMAIN_LAST_PART_REGEX;
+    private static final String DOMAIN_LAST_PART_REGEX = "(" + DOMAIN_PART_REGEX + "){2,3}$"; // 2 to 3 characters
+    private static final String DOMAIN_REGEX = "(" + DOMAIN_PART_REGEX + "\\.)" + DOMAIN_LAST_PART_REGEX;
     public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@" + DOMAIN_REGEX;
 
     public final String value;
