8a33b5a136f3f05bb7da30bea1bd7fbd87890c58
A1WAYSD <yujl2004@163.com>
2023-11-11 17:32:09 +0800

    Update test

--- src/test/java/seedu/flashlingo/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/flashlingo/logic/commands/CommandResultTest.java
@@ -57,7 +57,7 @@ public class CommandResultTest {
         CommandResult commandResult = new CommandResult("feedback");
         String expected = CommandResult.class.getCanonicalName() + "{feedbackToUser="
                 + commandResult.getFeedbackToUser() + ", showHelp=" + commandResult.isShowHelp()
-                + ", exit=" + commandResult.isExit() + "}";
+                + ", exit=" + commandResult.isExit() + ", switchTheme=" + commandResult.isSwitchTheme() + "}";
         assertEquals(expected, commandResult.toString());
     }
 }
