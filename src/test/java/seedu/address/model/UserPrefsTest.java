2bd6e3b375474b5bade3bd7c032e2cbd8c1cebd3
Matthew Sung <SungMatt@SungMatt.com>
2023-11-12 00:26:03 +0800

    Add tests for UserPrefs

--- src/test/java/seedu/address/model/UserPrefsTest.java
+++ src/test/java/seedu/address/model/UserPrefsTest.java
@@ -1,5 +1,8 @@
 package seedu.address.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -18,4 +21,21 @@ public class UserPrefsTest {
         assertThrows(NullPointerException.class, () -> userPrefs.setManageHrFilePath(null));
     }
 
+    @Test
+    public void equals_sameObj_success() {
+        UserPrefs userPrefs = new UserPrefs();
+        assertTrue(userPrefs.equals(new UserPrefs()));
+    }
+
+    @Test
+    public void equals_null_failure() {
+        UserPrefs userPrefs = new UserPrefs();
+        assertFalse(userPrefs.equals(null));
+    }
+
+    @Test
+    public void getHash_sameObj_success() {
+        UserPrefs userPrefs = new UserPrefs();
+        assertEquals(userPrefs.hashCode(), new UserPrefs().hashCode());
+    }
 }
