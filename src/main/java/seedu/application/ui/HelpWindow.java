131ec68f5d7c3ec7a7b2b02685d3d3f7e1d021f6
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2023-11-11 19:22:27 +0800

    Correct checkstyle errors

--- src/main/java/seedu/application/ui/HelpWindow.java
+++ src/main/java/seedu/application/ui/HelpWindow.java
@@ -16,21 +16,23 @@ import seedu.application.commons.core.LogsCenter;
 public class HelpWindow extends UiPart<Stage> {
 
     public static final String USERGUIDE_URL = "https://ay2324s1-cs2103t-w12-3.github.io/tp/UserGuide.html";
-    public static final String HELP_MESSAGE_COMMANDS = "Here are the available commands and how to use them:\n\n"
-                                                           + "Words in UPPER_CASE are the parameters to be supplied by the user\n\n"
-                                                           + "1. To add an application: add c/COMPANY r/ROLE\n"
-                                                           + "2. To edit an application: edit INDEX\n"
-                                                           + "3. To delete an application: delete INDEX \n"
-                                                           + "4. To list all applications: list \n"
-                                                           + "5. To find an application: find [KEYWORDS]\n"
-                                                           + "6. To sort applications: sort PREFIX\n"
-                                                           + "7. To add an interview to an application: interview add INDEX t/TYPE d/DATETIME a/ADDRESS\n"
-                                                           + "8. To edit an interview of an application: interview edit INTERVIEW_INDEX from/JOB_INDEX\n"
-                                                           + "9. To delete an interview from an application: interview delete INTERVIEW_INDEX from/JOB_INDEX\n"
-                                                           + "10. To clear all applications: clear\n"
-                                                           + "11. To exit the app: exit\n";
-    public static final String HELP_MESSAGE = HELP_MESSAGE_COMMANDS + "\n"
-                                                  + "Refer to the user guide for additional optional inputs: " + USERGUIDE_URL;
+    public static final String HELP_MESSAGE_COMMANDS =
+        "Here are the available commands and how to use them:\n\n"
+            + "Words in UPPER_CASE are the parameters to be supplied by the user\n\n"
+            + "1. To add an application: add c/COMPANY r/ROLE\n"
+            + "2. To edit an application: edit INDEX\n"
+            + "3. To delete an application: delete INDEX \n"
+            + "4. To list all applications: list \n"
+            + "5. To find an application: find [KEYWORDS]\n"
+            + "6. To sort applications: sort PREFIX\n"
+            + "7. To add an interview to an application: interview add INDEX t/TYPE d/DATETIME a/ADDRESS\n"
+            + "8. To edit an interview of an application: interview edit INTERVIEW_INDEX from/JOB_INDEX\n"
+            + "9. To delete an interview from an application: interview delete INTERVIEW_INDEX from/JOB_INDEX\n"
+            + "10. To clear all applications: clear\n"
+            + "11. To exit the app: exit\n";
+    public static final String HELP_MESSAGE =
+        HELP_MESSAGE_COMMANDS + "\n"
+            + "Refer to the user guide for additional optional inputs: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
