5a3730a43383d0e0c7f3846805d866550eb508b8
nubnubyas <zhangyiheng0421@gmail.com>
2023-11-12 00:27:04 +0800

    Change format of comment to give credit

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -22,7 +22,7 @@ public class Email {
             + "    - have each domain label start and end with alphanumeric characters\n"
             + "    - have each domain label and tld consist of alphanumeric characters,"
             + " separated only by hyphens, if any.";
-    // Credit: OWASP Validation Regex Repository
+    // Code below is taken from https://owasp.org/www-community/OWASP_Validation_Regex_Repository
     private static final String VALIDATION_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@"
             + "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$";
 
