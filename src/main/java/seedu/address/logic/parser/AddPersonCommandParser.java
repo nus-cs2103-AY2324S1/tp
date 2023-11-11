32799dd2a8adc0ad8400d9f9f5e50cfd12e6ab55
yiwen101 <e0968880@u.nus.edu>
2023-11-11 13:56:56 +0800

    Update usage

--- src/main/java/seedu/address/logic/parser/AddPersonCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddPersonCommandParser.java
@@ -51,7 +51,7 @@ public class AddPersonCommandParser implements Parser<AddPersonCommand> {
     }
     private static String getUsageInfo() {
         return "\nUsage: addPerson -name NAME (any number of unique "
-                + "-[phone|email|address|subject|tag|remark VALUE]). "
+                + "[-phone|email|address|subject|tag|remark VALUE]). "
                 + "\nFor example, addPerson -name John -phone 91234567"
                 + "\nIf you are currently displaying student list, you could use 'add' inplace of 'addPerson'. "
                 + "\nNote you must provide a 'name' not already in the address book.";
