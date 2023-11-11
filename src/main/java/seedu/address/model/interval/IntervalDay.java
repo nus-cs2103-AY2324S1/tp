155150c34c699c3dc69f00d9c0f7fc433424906b
Winston <leonardwinstoncx9@gmail.com>
2023-11-12 01:07:35 +0800

    intervalDay inherits Day

--- src/main/java/seedu/address/model/interval/IntervalDay.java
+++ src/main/java/seedu/address/model/interval/IntervalDay.java
@@ -1,5 +1,7 @@
 package seedu.address.model.interval;
 
+import seedu.address.model.person.Day;
+
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
@@ -9,15 +11,7 @@ import java.time.DayOfWeek;
  * Represents the day in the interval
  * Guarantees: immutable; is valid as declared in {@link #isValidDay(String)}
  */
-public class IntervalDay {
-    public static final String MESSAGE_CONSTRAINTS =
-            "Days should be written using their full names or their first three letters, and it should not be blank";
-
-    public static final String VALIDATION_REGEX =
-            "(?i)^(Mon|Monday|Tue|Tuesday|Wed|Wednesday|Thu|Thursday|Fri|Friday|Sat|Saturday|Sun|Sunday)$";
-
-    public final DayOfWeek value;
-    public final String stringValue;
+public class IntervalDay extends Day {
 
     /**
      * Constructs a {@code Day}.
@@ -25,99 +19,7 @@ public class IntervalDay {
      * @param day A valid day.
      */
     public IntervalDay(String day) {
-        requireNonNull(day);
-        checkArgument(isValidDay(day), MESSAGE_CONSTRAINTS);
-        value = parse(day);
-        stringValue = parseDay(day);
-    }
-
-    /**
-     * Parses an input as a DayOfWeek object.
-     *
-     * @param test
-     * @return DayOfWeek object
-     */
-    public DayOfWeek parse(String test) {
-        String lowerCaseTest = test.toLowerCase();
-        switch (lowerCaseTest) {
-        case "mon":
-        case "monday":
-            return DayOfWeek.MONDAY;
-        case "tue":
-        case "tuesday":
-            return DayOfWeek.TUESDAY;
-        case "wed":
-        case "wednesday":
-            return DayOfWeek.WEDNESDAY;
-        case "thu":
-        case "thursday":
-            return DayOfWeek.THURSDAY;
-        case "fri":
-        case "friday":
-            return DayOfWeek.FRIDAY;
-        case "sat":
-        case "saturday":
-            return DayOfWeek.SATURDAY;
-        case "sun":
-        case "sunday":
-            return DayOfWeek.SUNDAY;
-        default:
-            throw new IllegalArgumentException(MESSAGE_CONSTRAINTS);
-        }
-        //checkArgument(isValidDay(day), MESSAGE_CONSTRAINTS);
-        //value = parseDay(day);
-    }
-
-    /**
-     * Parses the day input
-     * @param input string of Day
-     * @return parses the day into the complete day name
-     */
-    public String parseDay(String input) {
-        String day = input.toLowerCase();
-        String result = "";
-        switch (day) {
-        case "mon":
-        case "monday":
-            result = "Mon";
-            break;
-        case "tue":
-        case "tuesday":
-            result = "Tue";
-            break;
-        case "wed":
-        case "wednesday":
-            result = "Wed";
-            break;
-        case "thu":
-        case "thursday":
-            result = "Thu";
-            break;
-        case "fri":
-        case "friday":
-            result = "Fri";
-            break;
-        case "sat":
-        case "saturday":
-            result = "Sat";
-            break;
-        case "sun":
-        case "sunday":
-            result = "Sun";
-            break;
-        default:
-            throw new IllegalArgumentException(MESSAGE_CONSTRAINTS);
-        }
-        return result;
-    }
-
-    public static boolean isValidDay(String test) {
-        return test.matches(VALIDATION_REGEX);
-    }
-
-    @Override
-    public String toString() {
-        return stringValue;
+        super(day);
     }
 
     @Override
