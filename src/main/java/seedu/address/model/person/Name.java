4c79d98f42f6ff30f51ae006ab674de2f06477b5
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2023-11-12 11:20:43 +0800

    Revert "Update name validity check to check for numbers"
    This reverts commit c971271ba50078ce504d08c1c1c8e38db780df75.

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -35,10 +35,6 @@ public class Name {
      * Returns true if a given string is a valid name.
      */
     public static boolean isValidName(String test) {
-        if (test.matches(".*\\d.*")) {
-            return false;
-        }
-
         return test.matches(VALIDATION_REGEX);
     }
 
