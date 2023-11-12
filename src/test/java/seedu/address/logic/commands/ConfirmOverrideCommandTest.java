b68cc6338263606ac552c6036306854c34598e56
jylow <78674750+jylow@users.noreply.github.com>
2023-11-13 00:35:48 +0800

    Increase code coverage for ConfirmOverrideCommand

--- src/test/java/seedu/address/logic/commands/ConfirmOverrideCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ConfirmOverrideCommandTest.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
@@ -32,5 +35,50 @@ public class ConfirmOverrideCommandTest {
 
         assertCommandSuccess(new ConfirmOverrideCommand(newAppointment, personToEdit), model,
                 ConfirmOverrideCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void getMethodTest() {
+        Appointment newAppointment = Appointment.parseAppointmentDescription(APPOINTMENT_DESCRIPTION_STUB);
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Person personToEdit = model.getFilteredPersonList().get(1);
+        ConfirmOverrideCommand command = new ConfirmOverrideCommand(newAppointment, personToEdit);
+
+        assertEquals(command.getAppointment(), newAppointment);
+        assertEquals(command.getPersonToEdit(), personToEdit);
+    }
+
+    @Test
+    public void equals() {
+        Appointment newAppointment = Appointment.parseAppointmentDescription(APPOINTMENT_DESCRIPTION_STUB);
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Person personToEdit = model.getFilteredPersonList().get(1);
+        Person personToEdit2 = model.getFilteredPersonList().get(2);
+        ConfirmOverrideCommand confirmOverrideCommand = new ConfirmOverrideCommand(newAppointment, personToEdit);
+
+        // same values -> returns true
+        assertTrue(confirmOverrideCommand.equals(new ConfirmOverrideCommand(newAppointment, personToEdit)));
+
+        // same object -> returns true
+        assertTrue(confirmOverrideCommand.equals(confirmOverrideCommand));
+
+        // null -> returns false
+        assertFalse(confirmOverrideCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(confirmOverrideCommand.equals(0.5f));
+
+        // different feedbackToUser value -> returns false
+        assertFalse(confirmOverrideCommand.equals(new ConfirmOverrideCommand(newAppointment, personToEdit2)));
+
+    }
+
+    @Test
+    public void hashcode() {
+        CommandResult commandResult = new CommandResult("feedback");
+
+        // same values -> returns same hashcode
+        assertEquals(commandResult.hashCode(), new CommandResult("feedback").hashCode());
+
     }
 }
