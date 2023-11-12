29424d7b36c0ea7b8433ada53596608771044e9b
nubnubyas <zhangyiheng0421@gmail.com>
2023-11-13 00:36:02 +0800

    Revert changes to Email validation

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import seedu.address.logic.parser.Prefix;
-import seedu.address.model.person.Appointment;
 import seedu.address.model.person.Person;
 
 /**
