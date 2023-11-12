18a3c92e6789a0c47d9c1f363fad7bb4fe1c723a
Kailash201 <109354852+Kailash201@users.noreply.github.com>
2023-11-12 12:31:14 +0800

    update status bar

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -1,6 +1,7 @@
 package seedu.address.ui;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -21,7 +22,7 @@ public class StatusBarFooter extends UiPart<Region> {
      */
     public StatusBarFooter(Path saveLocation) {
         super(FXML);
-        saveLocationStatus.setText("./data/projectPRO");
+        saveLocationStatus.setText(Paths.get(".").resolve(saveLocation).toString());
     }
 
 }
