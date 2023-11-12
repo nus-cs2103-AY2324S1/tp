a342b84dc1f5f1e50c13299ab64d4ab385762d46
pzl111 <phuazailian@gmail.com>
2023-11-12 20:10:25 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -5,7 +5,6 @@ import static java.util.Objects.requireNonNull;
 import java.util.Objects;
 
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.model.state.State;
 
 /**
  * Represents the result of a command execution.
