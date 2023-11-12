f2271317c84c381e742cb221f5b158675538a407
brendanneojw <e0726880@u.nus.edu>
2023-11-12 22:46:58 +0800

    Update sample data to be reflective of real-life

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -31,55 +31,55 @@ public class SampleDataUtil {
             new Person(
                 new Name("Alex Yeoh"),
                 new Phone("87438807"),
-                new Email("alexyeoh@example.com"),
+                new Email("alexyeoh@gmail.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends"),
+                getTagSet("friend"),
                 new Nric("023A"),
                 new LicencePlate("SNB9538E"),
-                EMPTY_REMARK,
+                new Remark("Wants cheap policies!"),
                 new Policy(
-                    new Company("NTUC"),
-                    new PolicyNumber("A1231X"),
-                    new PolicyDate("01-01-2023"),
-                    new PolicyDate("01-01-2024")
+                    new Company("NTUC Income"),
+                    new PolicyNumber("NI11235"),
+                    new PolicyDate("11-11-2023"),
+                    new PolicyDate("11-11-2024")
                 )
             ),
             new Person(
                 new Name("Bernice Yu"),
                 new Phone("99272758"),
-                new Email("berniceyu@example.com"),
-                new Address("Blk 30 Serangoon North, #07-18"),
+                new Email("berniceyu@gmail.com"),
+                new Address("Blk 30 Serangoon North Avenue 4, #07-18"),
                 getTagSet("colleagues", "friends"),
                 new Nric("362D"),
-                new LicencePlate("SLR5E"),
-                EMPTY_REMARK,
+                new LicencePlate("SLR5694E"),
+                new Remark("Friends for 4 years! Likes to hang out"),
                 new Policy(
-                    new Company("InsureMe"),
-                    new PolicyNumber("B3425"),
-                    new PolicyDate("01-01-2020"),
-                    new PolicyDate("01-01-2030")
+                    new Company("AXA Insurance"),
+                    new PolicyNumber("AI38495"),
+                    new PolicyDate("11-10-2023"),
+                    new PolicyDate("11-10-2024")
                 )
             ),
             new Person(
-                new Name("Charlotte Oliveiro"),
-                new Phone("93210283"),
-                new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                new Name("Eva Ng"),
+                new Phone("89326549"),
+                new Email("evang@example.com"),
+                new Address("Blk 101 Bukit Timah Road, #15-20"),
                 getTagSet("neighbours"),
-                new Nric("743G"),
-                new LicencePlate("SBA1234A"),
-                EMPTY_REMARK,
+                new Nric("123A"),
+                new LicencePlate("SGP6535A"),
+                new Remark("Neighbours at current residence for 15 years already!"),
                 new Policy(
-                    new Company("NTUC"),
-                    new PolicyNumber("B3425"),
-                    new PolicyDate("20-12-2020"),
-                    new PolicyDate("01-01-2030")
+                    new Company("HSBC Insurance"),
+                    new PolicyNumber("HI23593"),
+                    new PolicyDate("30-12-2022"),
+                    new PolicyDate("30-12-2023")
                 )
             ),
             new Person(
                 new Name("David Li"),
                 new Phone("91031282"),
-                new Email("lidavid@example.com"),
+                new Email("lidavid@outlook.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                 getTagSet("family"),
                 new Nric("362D"),
@@ -95,28 +95,28 @@ public class SampleDataUtil {
             new Person(
                 new Name("Irfan Ibrahim"),
                 new Phone("92492021"),
-                new Email("irfan@example.com"),
+                new Email("irfan@gmail.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
                 getTagSet("classmates"),
                 new Nric("752X"),
-                new LicencePlate("SBP8888T"),
-                EMPTY_REMARK,
+                new LicencePlate("SBP5950T"),
+                new Remark("Classmate from high school"),
                 new Policy(
-                    new Company(Company.DEFAULT_VALUE),
-                    new PolicyNumber(PolicyNumber.DEFAULT_VALUE),
-                    new PolicyDate(PolicyDate.DEFAULT_VALUE),
-                    new PolicyDate(PolicyDate.DEFAULT_VALUE)
+                    new Company("NTUC Income"),
+                    new PolicyNumber("NI294759"),
+                    new PolicyDate("20-12-2022"),
+                    new PolicyDate("20-12-2023")
                 )
             ),
             new Person(
                 new Name("Roy Balakrishnan"),
                 new Phone("92624417"),
-                new Email("royb@example.com"),
+                new Email("royb@yahoo.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
                 getTagSet("colleagues"),
                 new Nric("764J"),
                 new LicencePlate("SJD6453Y"),
-                EMPTY_REMARK,
+                new Remark("Colleagues since 2 years ago"),
                 new Policy(
                     new Company(Company.DEFAULT_VALUE),
                     new PolicyNumber(PolicyNumber.DEFAULT_VALUE),
@@ -125,51 +125,51 @@ public class SampleDataUtil {
                 )
             ),
             new Person(
-                new Name("Bob White"),
-                new Phone("23456789"),
-                new Email("bob.white@example.com"),
+                new Name("Amirah Abdullah"),
+                new Phone("86532542"),
+                new Email("amirah@gmail.com.com"),
                 new Address("Blk 456 Serangoon Avenue 3, #05-12"),
                 getTagSet("colleagues"),
                 new Nric("456B"),
                 new LicencePlate("SGP5678Y"),
                 EMPTY_REMARK,
                 new Policy(
-                    new Company("ABC Insurance"),
-                    new PolicyNumber("RS67890"),
+                    new Company("DirectAsia Insurance"),
+                    new PolicyNumber("DI149503"),
                     new PolicyDate("15-06-2023"),
                     new PolicyDate("15-06-2024")
                 )
             ),
             new Person(
-                new Name("Alice Green"),
-                new Phone("34567890"),
-                new Email("alice.green@example.com"),
+                new Name("Raj Kumar"),
+                new Phone("93653498"),
+                new Email("rkumar@gmail.com"),
                 new Address("Blk 789 Orchard Road, #10-15"),
                 getTagSet("friends"),
                 new Nric("567C"),
                 new LicencePlate("SGP6789Z"),
                 EMPTY_REMARK,
                 new Policy(
-                    new Company("XYZ Insurance"),
-                    new PolicyNumber("TP12345"),
+                    new Company("AXA Insurance"),
+                    new PolicyNumber("AI948593"),
                     new PolicyDate("20-07-2023"),
                     new PolicyDate("20-07-2024")
                 )
             ),
             new Person(
-                new Name("John Doe"),
-                new Phone("12345678"),
-                new Email("john.doe@example.com"),
-                new Address("Blk 101 Bukit Timah Road, #15-20"),
-                getTagSet("neighbors"),
-                new Nric("123A"),
-                new LicencePlate("SGP1234A"),
-                EMPTY_REMARK,
-                new Policy(
-                    new Company("PQR Insurance"),
-                    new PolicyNumber("XY123456"),
-                    new PolicyDate("15-07-2023"),
-                    new PolicyDate("15-07-2024")
+            new Name("Noor Muhammad"),
+            new Phone("93210283"),
+            new Email("noormuhammad@yahoo.com"),
+            new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+            getTagSet("friends"),
+            new Nric("743G"),
+            new LicencePlate("SBA8473A"),
+            EMPTY_REMARK,
+            new Policy(
+                    new Company("NTUC Income"),
+                    new PolicyNumber("NI49383"),
+                    new PolicyDate("20-12-2022"),
+                    new PolicyDate("20-12-2023")
                 )
             )
         };
