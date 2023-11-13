4d95de54d886224ae0c45d05324fcd00f195ec4b
erohsikivar <ravikishore1809@gmail.com>
2023-11-14 01:33:21 +0800

    Delete test case

--- src/test/java/seedu/address/storage/JsonSerializableTeamBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableTeamBookTest.java
@@ -19,16 +19,6 @@ public class JsonSerializableTeamBookTest {
     private static final Path TYPICAL_TEAMS_FILE = TEST_DATA_FOLDER.resolve("typicalTeamsTeamBook.json");
     private static final Path INVALID_TEAM_FILE = TEST_DATA_FOLDER.resolve("invalidTeamTeamBook.json");
     private static final Path DUPLICATE_TEAM_FILE = TEST_DATA_FOLDER.resolve("duplicateTeamTeamBook.json");
-
-    //    @Test
-    //    public void toModelType_typicalTeamsFile_success() throws Exception {
-    //        JsonSerializableTeamBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_TEAMS_FILE,
-    //                JsonSerializableTeamBook.class).get();
-    //        TeamBook teamBookFromFile = dataFromFile.toModelType();
-    //        TeamBook typicalTeamsTeamBook = TypicalTeams.getTypicalTeamBook();
-    //        assertEquals(teamBookFromFile, typicalTeamsTeamBook);
-    // }
-
     @Test
     public void toModelType_invalidTeamFile_throwsIllegalValueException() throws Exception {
         JsonSerializableTeamBook dataFromFile = JsonUtil.readJsonFile(INVALID_TEAM_FILE,
