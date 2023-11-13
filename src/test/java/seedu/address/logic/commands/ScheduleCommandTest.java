188ba322cfff65253ada4728d79a42829c022783
Kb-tay <taykaiboon12@gmail.com>
2023-11-13 17:09:31 +0800

    Add test cases

--- src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.logic.commands.ScheduleCommand.CONFIRM_OVERRIDE_MESSAGE;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
