0c885c398dc04a152d5805a696beb85b9088b2a4
erohsikivar <ravikishore1809@gmail.com>
2023-11-14 01:34:12 +0800

    Fix minor bug

--- src/test/java/seedu/address/storage/JsonSerializableTeamBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableTeamBookTest.java
@@ -1,6 +1,5 @@
 package seedu.address.storage;
 
-//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
@@ -10,8 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
-//import seedu.address.model.TeamBook;
-//import seedu.address.testutil.TypicalTeams;
+
 
 public class JsonSerializableTeamBookTest {
 
