dc3050c1e4bbb9711a24145374b8c9b08248031e
Andrew Janong <andrewjanong@gmail.com>
2023-11-13 15:20:45 +0800

    Improve test cases in seedu.address.model.event

--- src/test/java/seedu/address/model/event/EventNameTest.java
+++ src/test/java/seedu/address/model/event/EventNameTest.java
@@ -7,16 +7,38 @@ import org.junit.jupiter.api.Test;
 
 public class EventNameTest {
     @Test
+    public void isValidEventName() {
+        // blank event name
+        assertFalse(EventName.isValidEventName(""));
+
+        // valid event name
+        assertTrue(EventName.isValidEventName("Interview"));
+        assertTrue(EventInformation.isValidEventInformation("Chatting with Recruiter"));
+        assertTrue(EventName.isValidEventName("Meetup with Friends"));
+    }
+
+    @Test
     public void test_eventLocationEquals_pass() {
         EventName name = EventName.fromString("SampleName0");
+
+        // same object --> returns true
         assertTrue(name.equals(name));
-        assertTrue(EventLocation.fromString("SampleName")
-                .equals(EventLocation.fromString("SampleName")));
+
+        // same values --> returns true
+        assertTrue(EventName.fromString("SampleName")
+                .equals(EventName.fromString("SampleName")));
+
+        // different types --> returns false
+        assertFalse(EventName.fromString("SampleName1").equals(50));
+
+        // different values --> returns false
         assertFalse(
-                EventLocation.fromString("SampleName1")
-                        .equals(EventLocation.fromString("SampleName2")));
+                EventName.fromString("SampleName1")
+                        .equals(EventName.fromString("SampleName2")));
+
+        // empty object --> returns false
         assertFalse(
-                EventLocation.fromString("SampleName1").equals(new Object()));
+                EventName.fromString("SampleName1").equals(new Object()));
     }
 
 }
