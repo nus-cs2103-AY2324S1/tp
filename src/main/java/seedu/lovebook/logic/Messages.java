c421fc0cf92d3a41635df1408b40bd06ea20e134
lynnlow175 <lynnlow175@gmail.com>
2023-11-14 03:41:06 +0800

    Update UG

--- src/main/java/seedu/lovebook/logic/Messages.java
+++ src/main/java/seedu/lovebook/logic/Messages.java
@@ -16,7 +16,6 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_FILTER_FORMAT = "Please only enter one metric to sort by! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The date index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d date(s) listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
