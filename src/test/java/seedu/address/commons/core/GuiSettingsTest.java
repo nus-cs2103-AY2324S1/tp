803e6d95d8a631966eecc3a7d3c3fd20ad24a53e
Matthew Sung <SungMatt@SungMatt.com>
2023-11-14 10:36:08 +0800

    Add tests for GUI Settings

--- src/test/java/seedu/address/commons/core/GuiSettingsTest.java
+++ src/test/java/seedu/address/commons/core/GuiSettingsTest.java
@@ -1,10 +1,28 @@
 package seedu.address.commons.core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class GuiSettingsTest {
+    @Test
+    public void hashCode_sameObject_equals() {
+        GuiSettings guiSettings = new GuiSettings();
+        assertEquals(guiSettings.hashCode(), guiSettings.hashCode());
+    }
+    @Test
+    public void equals_nullInput_failure() {
+        GuiSettings guiSettings = new GuiSettings();
+        assertFalse(guiSettings.equals(null));
+    }
+    @Test
+    public void equals_similarObjects_success() {
+        GuiSettings guiSettings1 = new GuiSettings();
+        GuiSettings guiSettings2 = new GuiSettings();
+        assertTrue(guiSettings1.equals(guiSettings2));
+    }
     @Test
     public void toStringMethod() {
         GuiSettings guiSettings = new GuiSettings();
